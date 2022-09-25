1b7eb44a17ff175308b6f2dfe2a614638270b77d
yunjichoi <cyjcode99@gmail.com>
2022-09-25 16:34:22 +0900

    [Silver I] Title: RGB거리, Time: 280 ms, Memory: 20660 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -4,24 +4,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[][] rgb = new int[n][3];
         int[][] dp = new int[n][3];
         for(int i = 0; i < n; i++) {
-            for(int j = 0; j < 3; j++) {
-                rgb[i][j] = sc.nextInt();
-				dp[i][j] = Integer.MAX_VALUE;
+            int r = sc.nextInt(), g = sc.nextInt(), b = sc.nextInt();
+            if(i > 0) {
+                dp[i][0] = r + Math.min(dp[i - 1][1], dp[i - 1][2]);
+                dp[i][1] = g + Math.min(dp[i - 1][0], dp[i - 1][2]);
+                dp[i][2] = b + Math.min(dp[i - 1][0], dp[i - 1][1]);
             }
-        }
-        dp[0][0] = rgb[0][0];
-        dp[0][1] = rgb[0][1];
-        dp[0][2] = rgb[0][2];
-        for(int i = 1; i < n; i++) {
-            for(int j = 0; j < 3; j++) {
-                for(int k = 0; k < 3; k++) {
-                    if(j != k) {
-                        dp[i][k] = Math.min(dp[i][k], dp[i - 1][j] + rgb[i][k]);
-                    }
-                }
+            else {
+                dp[i][0] = r;
+                dp[i][1] = g;
+                dp[i][2] = b;
             }
         }
         System.out.println(Math.min(Math.min(dp[n - 1][0], dp[n - 1][1]), dp[n - 1][2]));
