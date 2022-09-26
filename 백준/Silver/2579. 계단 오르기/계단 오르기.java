3f1b84b75ee985e1ac947820a9691ad0b721b568
yunjichoi <cyjcode99@gmail.com>
2022-09-27 00:03:36 +0900

    [Silver III] Title: 계단 오르기, Time: 232 ms, Memory: 18112 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] stairs = new int[n + 2];
+        int[] dp = new int[n + 2];
+        for(int i = 0; i < n; i++) {
+            stairs[i] = sc.nextInt();
+        }
+        dp[0] = stairs[0];
+        dp[1] = stairs[0] + stairs[1];
+        dp[2] = Math.max(stairs[0], stairs[1]) + stairs[2];
+        for(int i = 3; i < n; i++) {
+            dp[i] = Math.max(dp[i - 3] + stairs[i - 1] + stairs[i], dp[i - 2] + stairs[i]);
+        }
+        System.out.println(dp[n - 1]);
+        sc.close();
+    }
+}
