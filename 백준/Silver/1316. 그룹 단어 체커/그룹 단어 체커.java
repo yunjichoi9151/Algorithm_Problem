52d1ede3412f8ea9dc4c43fa2e9115f998910021
yunjichoi <cyjcode99@gmail.com>
2022-10-02 17:23:22 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 216 ms, Memory: 17764 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -5,26 +5,29 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int cnt = 0;
-        for(int tc = 1; tc <= n; tc++) {
+        for(int i = 0; i < n; i++) {
+            int[] check = new int[26];
             String s = sc.next();
-            boolean check[] = new boolean[26];
-            Boolean result = true;
-            for(int i = 0; i < s.length(); i++) {
-                int idx = s.charAt(i) - 'a';
-                if(check[idx]) {
-                    if(s.charAt(i) != s.charAt(i - 1)) {
-                        result = false;
+            char[] arr = s.toCharArray();
+            boolean res = true;
+            for(int j = 0; j < arr.length; j++) {
+                int tmp = (int)arr[j] - 97;
+                if(check[tmp] == 0) {
+                    check[tmp]++;
+                } else {
+                    if(arr[j - 1] == arr[j]) {
+                        continue;
+                    } else {
+                        res = false;
                         break;
                     }
                 }
-                else {
-                    check[idx] = true;
-                }
             }
-            if(result) {
+            if(res == true) {
                 cnt++;
             }
         }
         System.out.println(cnt);
+        sc.close();
     }
 }
