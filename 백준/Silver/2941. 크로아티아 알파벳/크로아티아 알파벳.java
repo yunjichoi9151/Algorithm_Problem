e5c3a56a395403ed52677151b773af2c103d8845
yunjichoi <cyjcode99@gmail.com>
2022-10-02 16:57:12 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 204 ms, Memory: 17604 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -4,22 +4,21 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
-        String[] arr = new String[s.length()];
-        int cnt = 0, l = s.length();
+        char[] arr = s.toCharArray();
+        int l = arr.length, cnt = 0;
         for(int i = 0; i < s.length(); i++) {
-            arr[i] = s.charAt(i) + "";
             if(i > 0) {
-                if(arr[i].equals("=")) {
-                    if(arr[i - 1].equals("c")) {
+                if(arr[i] == '=') {
+                    if(arr[i - 1] == 'c') {
                         cnt++;
                         l -= 2;
                     }
-                    else if(arr[i - 1].equals("s")) {
+                    else if(arr[i - 1] == 's') {
                         cnt++;
                         l -= 2;
                     }
-                    else if(arr[i - 1].equals("z")) {
-                        if(i > 1 && arr[i - 2].equals("d")) {
+                    else if(arr[i - 1] == 'z') {
+                        if(i > 1 && arr[i - 2] == 'd') {
                             cnt++;
                             l -= 3;
                         }
@@ -29,22 +28,22 @@ public class Main {
                         }
                     }
                 }
-                else if(arr[i].equals("-")) {
-                    if(arr[i - 1].equals("c")) {
+                else if(arr[i] == '-') {
+                    if(arr[i - 1] == 'c') {
                         cnt++;
                         l -= 2;
                     }
-                    else if(arr[i - 1].equals("d")) {
+                    else if(arr[i - 1] == 'd') {
                         cnt++;
                         l -= 2;
                     }
                 }
-                else if(arr[i].equals("j")) {
-                    if(arr[i - 1].equals("l")) {
+                else if(arr[i] == 'j') {
+                    if(arr[i - 1] == 'l') {
                         cnt++;
                         l -= 2;
                     }
-                    else if(arr[i - 1].equals("n")) {
+                    else if(arr[i - 1] == 'n') {
                         cnt++;
                         l -= 2;
                     }
@@ -52,5 +51,6 @@ public class Main {
             }
         }
         System.out.println(cnt + l);
+        sc.close();
     }
 }
