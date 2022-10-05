ebdb1fb65d4c4cd39d555f27fc9d72dfb31b773a
yunjichoi <cyjcode99@gmail.com>
2022-10-05 15:48:50 +0900

    [Gold IV] Title: 타임머신, Time: 628 ms, Memory: 36556 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.java
+++ 백준/Gold/11657. 타임머신/타임머신.java
@@ -1,28 +1,27 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(), m = sc.nextInt();
-        ArrayList<Node> adj = new ArrayList<Node>();
+        ArrayList<Node> arr = new ArrayList<Node>();
         long dist[] = new long[n + 1];
         boolean isCycle = false;
-        for(int i = 1; i <= n; i++) {
-            dist[i] = 987654321;
-        }
+        Arrays.fill(dist, Long.MAX_VALUE);
         for(int i = 0; i < m; i++) {
             int a = sc.nextInt(), b = sc.nextInt();
             long c = sc.nextLong();
-            adj.add(new Node(a, b, c));
+            arr.add(new Node(a, b, c));
         }
         dist[1] = 0;
         for(int i = 1; i <= n; i++) {
             for(int j = 0; j < m; j++) {
-                int a = adj.get(j).start;
-                int b = adj.get(j).dest;
-                long c = adj.get(j).cost;
-                if(dist[b] > dist[a] + c && dist[a] != 987654321) {
+                int a = arr.get(j).start;
+                int b = arr.get(j).end;
+                long c = arr.get(j).time;
+                if(dist[a] + c < dist[b] && dist[a] != Long.MAX_VALUE) {
                     if(i == n) {
                         isCycle = true;
                     }
@@ -34,7 +33,7 @@ public class Main {
             System.out.println(-1);
         } else {
             for(int i = 2; i <= n; i++) {
-                if(dist[i] == 987654321) {
+                if(dist[i] == Long.MAX_VALUE) {
                     System.out.println(-1);
                 } else {
                     System.out.println(dist[i]);
@@ -43,15 +42,14 @@ public class Main {
         }
         sc.close();
     }
-
     public static class Node {
         int start;
-        int dest;
-        long cost;
-        public Node(int c, int a, long b) {
-            start = c;
-            dest = a;
-            cost = b;
+        int end;
+        long time;
+        public Node(int a, int b, long c) {
+            start = a;
+            end = b;
+            time = c;
         }
     }
 }
