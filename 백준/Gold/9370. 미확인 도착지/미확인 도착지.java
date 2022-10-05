63c03dc18af2cfb1cde1cd19347c2cc8387ebcfe
yunjichoi <cyjcode99@gmail.com>
2022-10-05 19:31:08 +0900

    [Gold II] Title: 미확인 도착지, Time: 1488 ms, Memory: 301912 KB -BaekjoonHub

--- 백준/Gold/9370. 미확인 도착지/미확인 도착지.java
+++ 백준/Gold/9370. 미확인 도착지/미확인 도착지.java
@@ -3,17 +3,16 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.PriorityQueue;
-
 import java.util.Scanner;
 
-class Edge implements Comparable<Edge> {
+class Node implements Comparable<Node> {
     int e, weight;
-    public Edge(int e, int weight) {
+    public Node(int e, int weight) {
         this.e = e;
         this.weight = weight;
     }
     @Override
-    public int compareTo(Edge e) {
+    public int compareTo(Node e) {
         return weight - e.weight;
     }
 }
@@ -22,7 +21,7 @@ public class Main {
     static int n, m, t;
     static int[] dist;
     static int INF = 100000000;
-    static List<List<Edge>> list;
+    static List<List<Node>> list;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
@@ -40,11 +39,11 @@ public class Main {
             for(int i = 0; i < m; i++) {
                 int a = sc.nextInt(), b = sc.nextInt(), d = sc.nextInt();
                 if((a == g && b == h) || (a == h && b == g)) {
-                    list.get(a).add(new Edge(b, d * 2 - 1));
-                    list.get(b).add(new Edge(a, d * 2 - 1));
+                    list.get(a).add(new Node(b, d * 2 - 1));
+                    list.get(b).add(new Node(a, d * 2 - 1));
                 } else {
-                    list.get(a).add(new Edge(b, d * 2));
-                    list.get(b).add(new Edge(a, d * 2));
+                    list.get(a).add(new Node(b, d * 2));
+                    list.get(b).add(new Node(a, d * 2));
                 }
             }
             ArrayList<Integer> candidate = new ArrayList<>();
@@ -58,23 +57,24 @@ public class Main {
                     System.out.print(candidate.get(i) + " ");
                 }
             }
+            System.out.println();
         }
         sc.close();
     }
 
     public static void dijkstra(int s) {
         boolean[] visit = new boolean[n + 1];
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        PriorityQueue<Node> pq = new PriorityQueue<>();
         dist[s] = 0;
-        pq.add(new Edge(s, 0));
+        pq.add(new Node(s, 0));
         while(!pq.isEmpty()) {
-            Edge tmp = pq.poll();
+            Node tmp = pq.poll();
             if(!visit[tmp.e]) {
                 visit[tmp.e] = true;
-                for(Edge edge : list.get(tmp.e)) {
-                    if(!visit[edge.e] && dist[edge.e] > dist[tmp.e] + edge.weight) {
-                        dist[edge.e] = dist[tmp.e] + edge.weight;
-                        pq.add(new Edge(edge.e, dist[edge.e]));
+                for(Node node : list.get(tmp.e)) {
+                    if(!visit[node.e] && dist[tmp.e] + node.weight < dist[node.e]) {
+                        dist[node.e] = dist[tmp.e] + node.weight;
+                        pq.add(new Node(node.e, dist[node.e]));
                     }
                 }
             }
