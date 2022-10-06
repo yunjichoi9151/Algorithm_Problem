6507f5b6b610f90a3e6510a0637a30097c635f9e
yunjichoi <cyjcode99@gmail.com>
2022-10-06 14:20:29 +0900

    [Silver II] Title: 연속합, Time: 816 ms, Memory: 109364 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        dp[0] = arr[0];
+        int max = arr[0];
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1] + arr[i], arr[i]);
+            max = Math.max(max, dp[i]);
+        }
+        System.out.println(max);
+        sc.close();
+    }
+}
