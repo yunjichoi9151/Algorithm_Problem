37c912fb74dc8d6877bdea466add66537cd46054
yunjichoi <cyjcode99@gmail.com>
2022-10-11 16:23:22 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 392 ms, Memory: 19820 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++) { 
+            for(int j = 0; j < n; j++) {
+                System.out.print(j < n - i - 1 ? ' ' : '*');
+            }
+            for(int j = 0; j < i; j++) {
+                System.out.print('*');
+            }
+            if(i != n - 1) {
+                System.out.println();
+            }
+        }
+        sc.close();
+    }
+}
