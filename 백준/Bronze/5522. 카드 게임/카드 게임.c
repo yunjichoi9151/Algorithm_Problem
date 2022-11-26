a8e40361ef1e7ddeafcdb63e07d390b85403fbdd
yunjichoi <cyjcode99@gmail.com>
2022-11-26 21:08:07 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, sum = 0;
+
+    for (int i = 0; i < 5; i++) {
+        scanf("%d", &a);
+        sum += a;
+    }
+
+    printf("%d\n", sum);
+
+    return 0;
+}
