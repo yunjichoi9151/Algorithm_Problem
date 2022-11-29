730539b58246da5b2fda8f46eaa093e6cbc279a0
yunjichoi <cyjcode99@gmail.com>
2022-11-29 23:58:31 +0900

    [Bronze III] Title: 직각삼각형, Time: 216 ms, Memory: 17724 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.java
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -1,19 +1,19 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arr = new int[3];
-        arr[0] = sc.nextInt();
-        arr[1] = sc.nextInt();
-        arr[2] = sc.nextInt();
-        while(!(arr[0] == 0 && arr[1] == 0 && arr[2] == 0)) {
-            Arrays.sort(arr);
-            System.out.println((Math.pow(arr[2], 2) == Math.pow(arr[0], 2) + Math.pow(arr[1], 2)) ? "right": "wrong");
-            arr[0] = sc.nextInt();
-            arr[1] = sc.nextInt();
-            arr[2] = sc.nextInt();
+        int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+        while(a != 0 && b != 0 && c != 0) {
+            if((Math.pow(a, 2) == Math.pow(b, 2) + Math.pow(c, 2)) || (Math.pow(b, 2) == Math.pow(a, 2) + Math.pow(c, 2)) || (Math.pow(c, 2) == Math.pow(a, 2) + Math.pow(b, 2))) {
+                System.out.println("right");
+            }
+            else {
+                System.out.println("wrong");
+            }
+            a = sc.nextInt();
+            b = sc.nextInt();
+            c = sc.nextInt();
         }
         sc.close();
     }
