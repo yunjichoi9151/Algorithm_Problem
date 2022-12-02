39d685e44711d84eaacc0a1812a69a550e64829c
yunjichoi <cyjcode99@gmail.com>
2022-12-03 02:12:48 +0900

    [level 3] Title: 숫자 게임, Time: 58.26 ms, Memory: 65.4 MB -BaekjoonHub

--- 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.java
+++ 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.java
@@ -4,10 +4,11 @@ class Solution {
     public int solution(int[] A, int[] B) {
         int answer = 0;
         int tmp = 0;
+        int len = A.length;
         Arrays.sort(A);
         Arrays.sort(B);
-        for(int i = 0; i < A.length; i++) {
-            for(int j = tmp; j < B.length; j++) {
+        for(int i = 0; i < len; i++) {
+            for(int j = tmp; j < len; j++) {
                 if(A[i] < B[j]) {
                     answer++;
                     tmp = j + 1;
