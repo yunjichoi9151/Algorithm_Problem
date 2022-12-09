97c82ae07d04bd66b00625f4fd4116d6e1925ee0
yunjichoi <cyjcode99@gmail.com>
2022-12-09 23:50:57 +0900

    [Bronze IV] Title: 특별한 날, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10768. 특별한 날/특별한 날.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int m, d;
+
+    scanf("%d\n%d", &m, &d);
+
+    if (m < 2)
+        printf("Before");
+    else if (m == 2) {
+        if (d < 18)
+            printf("Before");
+        else if (d == 18)
+            printf("Special");
+        else
+            printf("After");
+    }
+    else
+        printf("After");
+
+    return 0;
+}
