8f283295ef7d8f6cd1055327e15adb4f7949a737
yunjichoi <cyjcode99@gmail.com>
2022-12-13 23:37:38 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 208 ms, Memory: 17664 KB -BaekjoonHub

--- 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
@@ -1,17 +1,11 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt(), y = sc.nextInt(), w = sc.nextInt(), h = sc.nextInt();
-        int[] arr = new int[4];
-        arr[0] = Math.abs(y - h);
-        arr[1] = Math.abs(x - w);
-        arr[2] = x;
-        arr[3] = y;
-        Arrays.sort(arr);
-        System.out.println(arr[0]);
-        sc.close();
+        int n1 = (Math.abs(w - x) < Math.abs(h - y)) ? Math.abs(w - x) : Math.abs(h - y);
+        int n2 = x < y ? x : y;
+        System.out.println(n1 < n2 ? n1 : n2);
     }
 }
