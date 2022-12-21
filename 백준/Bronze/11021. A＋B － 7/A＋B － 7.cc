b4f91cb31b027478887f6f37c88520ba155d3d29
yunjichoi <cyjcode99@gmail.com>
2022-12-21 23:25:38 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+
+    scanf("%d", &a);
+
+    for (int i = 1; i <= a; i++) {
+        scanf("%d %d", &b, &c);
+        printf("Case #%d: %d\n", i, b + c);
+    }
+
+    return 0;
+}
