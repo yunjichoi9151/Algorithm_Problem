f812c2384cc77e2d2164a193acfaae91f8183794
yunjichoi <cyjcode99@gmail.com>
2022-12-28 23:48:52 +0900

    [Bronze IV] Title: 와글와글 숭고한, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/17388. 와글와글 숭고한/와글와글 숭고한.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int s, k, h, min;
+
+    scanf("%d %d %d", &s, &k, &h);
+
+    if (s + k + h >= 100)
+        printf("OK");
+    else if (s < k && s < h)
+        printf("Soongsil");
+    else if(k < s && k < h)
+        printf("Korea");
+    else if(h < s && h < k)
+        printf("Hanyang");
+
+    return 0;
+}
