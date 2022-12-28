a2dd0a996fb3d4aefffdc3a9ce3bc5032fc64bc0
yunjichoi <cyjcode99@gmail.com>
2022-12-28 23:48:02 +0900

    [Bronze V] Title: 달달함이 넘쳐흘러, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/17256. 달달함이 넘쳐흘러/달달함이 넘쳐흘러.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int ax, ay, az, cx, cy, cz;
+
+    scanf("%d %d %d", &ax, &ay, &az);
+    scanf("%d %d %d", &cx, &cy, &cz);
+
+    printf("%d %d %d", cx - az, cy / ay, cz - ax);
+
+    return 0;
+}
