af70b7538dcb15b67af93495ac4f51810e63d9d3
yunjichoi <cyjcode99@gmail.com>
2023-01-09 23:29:13 +0900

    [Bronze IV] Title: Archivist, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/20232. Archivist/Archivist.py
@@ -0,0 +1,7 @@
+y = int(input())
+if y == 1996 or y == 1997 or y == 2000 or y == 2007 or y == 2008 or y == 2013 or y == 2018:
+    print("SPbSU")
+elif y == 2006:
+    print("PetrSU, ITMO")
+else:
+    print("ITMO")
