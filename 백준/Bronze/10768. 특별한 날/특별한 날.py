fe8d0c171b9ba704f2f3d7a1857dacc0deac9285
yunjichoi <cyjcode99@gmail.com>
2023-02-08 23:26:46 +0900

    [Bronze IV] Title: 특별한 날, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/10768. 특별한 날/특별한 날.py
@@ -0,0 +1,13 @@
+m = int(input())
+d = int(input())
+if m < 2:
+    print("Before")
+elif m == 2:
+    if d < 18:
+        print("Before")
+    elif d == 18:
+        print("Special")
+    else:
+        print("After")
+else:
+    print("After")
