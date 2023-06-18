65bdf5be66d47482b1f19c783ad4d63ca6327b32
yunjichoi <cyjcode99@gmail.com>
2023-06-18 18:16:26 +0900

    [Bronze V] Title: A+B - 3, Time: 128 ms, Memory: 9580 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.js
@@ -0,0 +1,8 @@
+var fs = require('fs');
+var input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+var t = parseInt(input[0]);
+for (var i = 1; i <= t; i++) {
+  console.log(
+    parseInt(input[i].split(' ')[0]) + parseInt(input[i].split(' ')[1])
+  );
+}
