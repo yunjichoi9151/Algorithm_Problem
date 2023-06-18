62fd1190d822a021ace897ed729182cffcaecf52
yunjichoi <cyjcode99@gmail.com>
2023-06-18 16:33:04 +0900

    [Bronze V] Title: 시험 성적, Time: 120 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+var input = fs.readFileSync('/dev/stdin').toString().trim();
+if (input >= 90) {
+  console.log('A');
+} else if (input >= 80) {
+  console.log('B');
+} else if (input >= 70) {
+  console.log('C');
+} else if (input >= 60) {
+  console.log('D');
+} else {
+  console.log('F');
+}
