1e447df59fe411bca5570e34d205004ab2cb962d
yunjichoi <cyjcode99@gmail.com>
2023-06-18 16:38:37 +0900

    [Bronze V] Title: 윤년, Time: 128 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.js
@@ -0,0 +1,7 @@
+var fs = require('fs');
+var input = fs.readFileSync('/dev/stdin').toString().trim();
+if ((input % 4 == 0 && input % 100 != 0) || input % 400 == 0) {
+  console.log(1);
+} else {
+  console.log(0);
+}
