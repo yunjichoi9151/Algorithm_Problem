d59d6b6aa386a030591956f9e4c7c37ea6ecbef7
yunjichoi <cyjcode99@gmail.com>
2023-07-16 13:12:43 +0900

    [Bronze IV] Title: 줄번호, Time: 240 ms, Memory: 18448 KB -BaekjoonHub

--- 백준/Bronze/4470. 줄번호/줄번호.java
+++ 백준/Bronze/4470. 줄번호/줄번호.java
@@ -4,9 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        String s = sc.nextLine();
-        for(int tc = 1; tc <= n; tc++) {
-            System.out.println(tc + ". " + sc.nextLine());
+        String tmp1 = sc.nextLine();
+        for(int i = 1; i <= n; i++) {
+            String s = sc.nextLine();
+            System.out.println(i + ". " + s);
         }
         sc.close();
     }
