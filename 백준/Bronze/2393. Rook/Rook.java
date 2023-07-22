cb849a45252ca52b23ca9dd3459507d27a591b94
yunjichoi <cyjcode99@gmail.com>
2023-07-23 00:55:00 +0900

    [Bronze V] Title: Rook, Time: 112 ms, Memory: 13872 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("  ___  ___  ___\n  | |__| |__| |\n  |           |\n   \\_________/\n    \\_______/\n     |     |\n     |     |\n     |     |\n     |     |\n     |_____|\n  __/       \\__\n /             \\\n/_______________\\");
+    }
+}
