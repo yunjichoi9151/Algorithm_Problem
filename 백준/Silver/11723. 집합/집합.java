e68444cefda52b53e3b4925992a084bd6103d6c6
yunjichoi <cyjcode99@gmail.com>
2023-07-24 19:49:05 +0900

    [Silver V] Title: 집합, Time: 1124 ms, Memory: 313496 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -6,42 +6,44 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int m = Integer.parseInt(br.readLine());
+        boolean[] arr = new boolean[21];
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st = null;
 
-        int n = Integer.parseInt(br.readLine());
-        int S = 0;
-        while(n-- > 0) {
-            st = new StringTokenizer(br.readLine());
-            String str = st.nextToken();
-            int num = 0;
-
-            switch(str) {
-                case "add" :
-                    num = Integer.parseInt(st.nextToken()) - 1;
-                    S = S | (1 << num);
+        for (int tc = 0; tc < m; tc++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String s = st.nextToken();
+            int x = 0;
+            if (!s.equals("all") && !s.equals("empty")) {
+                x = Integer.parseInt(st.nextToken());
+            }
+            switch (s) {
+                case "add":
+                    arr[x] = true;
                     break;
-                case "remove" :
-                    num = Integer.parseInt(st.nextToken()) - 1;
-                    S = S & ~(1 << num);
+                case "remove":
+                    arr[x] = false;
                     break;
-                case "check" :
-                    num = Integer.parseInt(st.nextToken()) - 1;
-                    int temp = S & (1 << num);
-                    sb.append(((temp == 0) ? 0 : 1) + "\n");
+                case "check":
+                    sb.append(arr[x] ? 1 : 0).append('\n');
                     break;
-                case "toggle" :
-                    num = Integer.parseInt(st.nextToken()) - 1 ;
-                    S = S ^ (1 << num);
+                case "toggle":
+                    arr[x] = !arr[x];
                     break;
-                case "all" :
-                    S = (1 << 21) - 1;
+                case "all":
+                    for (int i = 1; i <= 20; i++) {
+                        arr[i] = true;
+                    }
                     break;
-                case "empty" :
-                    S = 0;
+                case "empty":
+                    for (int i = 1; i <= 20; i++) {
+                        arr[i] = false;
+                    }
                     break;
             }
         }
-        System.out.println(sb.toString());
+
+        System.out.print(sb);
+        br.close();
     }
 }
