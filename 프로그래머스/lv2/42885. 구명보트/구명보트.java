9c4c94b177c7582b2042248a472c5cdddc314d70
yunjichoi <cyjcode99@gmail.com>
2023-08-14 02:46:28 +0900

    [level 2] Title: 구명보트, Time: 8.68 ms, Memory: 53.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.java
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -2,15 +2,14 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int[] people, int limit) {
-        int answer = 0, idx = 0;
+        int answer = 0;
+        int idx = 0;
         Arrays.sort(people);
         for(int i = people.length - 1; i >= idx; i--) {
-            if(people[i] + people[idx] > limit) {
-                answer++;
-            } else {
+            if(people[i] + people[idx] <= limit) {
                 idx++;
-                answer++;
             }
+            answer++;
         }
         return answer;
     }
