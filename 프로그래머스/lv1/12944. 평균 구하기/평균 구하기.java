c792c7368cb7f666631ffc47810d9f131f663765
yunjichoi <cyjcode99@gmail.com>
2023-08-24 16:24:26 +0900

    [level 1] Title: 평균 구하기, Time: 0.02 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.java
@@ -1,8 +1,8 @@
 class Solution {
     public double solution(int[] arr) {
         double answer = 0;
-        for(int i = 0; i < arr.length; i++) {
-            answer += arr[i];
+        for(int n : arr) {
+            answer += n;
         }
         return answer / arr.length;
     }
