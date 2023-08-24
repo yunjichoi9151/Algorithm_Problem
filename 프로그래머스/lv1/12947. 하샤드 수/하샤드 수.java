f6e8b88c78f852852f88d59a8cb425499b602837
yunjichoi <cyjcode99@gmail.com>
2023-08-24 17:44:27 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -1,10 +1,10 @@
 class Solution {
     public boolean solution(int x) {
-        String str = Integer.toString(x);
-        int num = 0;
-        for(int i = 0; i < str.length(); i++) {
-            num += str.charAt(i) - '0';
+        String s = Integer.toString(x);
+        int sum = 0;
+        for(int i = 0; i < s.length(); i++) {
+            sum += s.charAt(i) - '0';
         }
-        return x % num == 0 ? true : false;
+        return x % sum == 0 ? true : false;
     }
 }
