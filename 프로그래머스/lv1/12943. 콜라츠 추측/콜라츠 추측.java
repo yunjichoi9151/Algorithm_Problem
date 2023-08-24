b64efb0c88ebd898d03002583716757c66865752
yunjichoi <cyjcode99@gmail.com>
2023-08-24 18:14:21 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 67.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,14 +1,18 @@
 class Solution {
     public int solution(int num) {
         int answer = 0;
-        while(num != 1) {
-            if(num % 2 == 0) {
-                num /= 2;
-            } else if(num % 2 == 1) {
-                num = num * 3 + 1;
+        long n = (long) num;
+        if(n == 1) {
+            return 0;
+        }
+        while(n != 1) {
+            if(n % 2 == 0) {
+                n /= 2;
+            } else {
+                n = n * 3 + 1;
             }
             answer++;
-            if(answer == 500) {
+            if(answer >= 500) {
                 return -1;
             }
         }
