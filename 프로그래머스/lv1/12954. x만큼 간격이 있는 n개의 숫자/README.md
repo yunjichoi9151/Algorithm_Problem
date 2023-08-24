f95ac492e339489b28eb440b83e06ff56bc836ff
yunjichoi <cyjcode99@gmail.com>
2023-08-24 16:32:32 +0900

    [level 1] Title: x만큼 간격이 있는 n개의 숫자, Time: 0.05 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12954. x만큼 간격이 있는 n개의 숫자/README.md
+++ 프로그래머스/lv1/12954. x만큼 간격이 있는 n개의 숫자/README.md
@@ -1,10 +1,10 @@
 # [level 1] x만큼 간격이 있는 n개의 숫자 - 12954 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12954) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12954#) 
 
 ### 성능 요약
 
-메모리: 77.1 MB, 시간: 0.06 ms
+메모리: 71.3 MB, 시간: 0.05 ms
 
 ### 구분
 
--- 프로그래머스/lv1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.java
+++ 프로그래머스/lv1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.java
@@ -1,11 +1,8 @@
-import java.util.*;
-
 class Solution {
     public long[] solution(int x, int n) {
         long[] answer = new long[n];
-        answer[0] = x;
-        for(int i = 1; i < n; i++) {
-            answer[i] = answer[i - 1] + x;
+        for(int i = 0; i < n; i++) {
+            answer[i] = x * ((long)i + 1);
         }
         return answer;
     }
