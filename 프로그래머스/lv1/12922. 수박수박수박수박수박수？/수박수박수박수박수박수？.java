e931d1edc855a7f6abdb4b0c740f0c22f1b174b0
yunjichoi <cyjcode99@gmail.com>
2023-08-27 07:21:33 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.84 ms, Memory: 67.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,11 +1,11 @@
 class Solution {
     public String solution(int n) {
         String answer = "";
-        for(int i = 1; i <= n; i++) {
+        for(int i = 0; i < n; i++) {
             if(i % 2 == 0) {
-                answer += "박";
-            } else {
                 answer += "수";
+            } else {
+                answer += "박";
             }
         }
         return answer;
