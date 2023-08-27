5f7da97259654c799a2c25baa9cf35b096709db3
yunjichoi <cyjcode99@gmail.com>
2023-08-27 19:45:39 +0900

    [level 1] Title: 시저 암호, Time: 44.03 ms, Memory: 90.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
@@ -2,20 +2,14 @@ class Solution {
     public String solution(String s, int n) {
         String answer = "";
         for(int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            char tmp = c;
-            for(int j = 0; j < n; j++) {
-                if(tmp == 'z') {
-                    tmp = 'a';
-                } else if(tmp == 'Z') {
-                    tmp = 'A';
-                } else if(tmp == ' ') {
-                    tmp = ' ';
-                } else {
-                    tmp = (char)(tmp + 1);
-                }
+            char tmp = s.charAt(i);
+            if(tmp >= 'a' && tmp <= 'z') {
+                answer += Character.toString(((tmp - 'a' + n) % 26) + 'a');
+            } else if(tmp >= 'A' && tmp <= 'Z') {
+                answer += Character.toString(((tmp - 'A' + n) % 26) + 'A');
+            } else {
+                answer += tmp;
             }
-            answer += tmp;
         }
         return answer;
     }
