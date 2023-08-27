776fc2ac487f323f18b901fb5a01195ad5db7918
yunjichoi <cyjcode99@gmail.com>
2023-08-27 23:00:10 +0900

    [level 1] Title: K번째수, Time: 0.35 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.java
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -4,16 +4,13 @@ class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         for(int i = 0; i < commands.length; i++) {
-            int from = commands[i][0];
-            int to = commands[i][1];
-            int where = commands[i][2];
-            int[] new_arr = new int[to - from + 1];
+            int[] arr = new int[commands[i][1] - commands[i][0] + 1];
             int idx = 0;
-            for(int j = from - 1; j < to; j++) {
-                new_arr[idx++] = array[j];
+            for(int j = commands[i][0] - 1; j < commands[i][1]; j++) {
+                arr[idx++] = array[j];
             }
-            Arrays.sort(new_arr);
-            answer[i] = new_arr[where - 1];
+            Arrays.sort(arr);
+            answer[i] = arr[commands[i][2] - 1];
         }
         return answer;
     }
