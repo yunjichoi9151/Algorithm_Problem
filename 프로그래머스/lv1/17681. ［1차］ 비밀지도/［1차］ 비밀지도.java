7091697602cb0ca1206d420d06e18149c752e8eb
yunjichoi <cyjcode99@gmail.com>
2023-08-27 22:08:28 +0900

    [level 1] Title: [1차] 비밀지도, Time: 6.88 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,17 +1,12 @@
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
+        String form = "%0" + n + "d";
         for(int i = 0; i < n; i++) {
-            String s = Integer.toBinaryString(arr1[i] | arr2[i]);
-            if(s.length() < n) {
-                int k = n - s.length();
-                for(int j = 0; j < k; j++) {
-                    s = "0" + s;
-                }
-            }
-            s = s.replaceAll("1", "#");
-            s = s.replaceAll("0", " ");
-            answer[i] = s;
+            String tmp = Integer.toBinaryString(arr1[i] | arr2[i]);
+            tmp = "0".repeat(n - tmp.length()) + tmp;
+            answer[i] = tmp.replaceAll("0", " ");
+            answer[i] = answer[i].replaceAll("1", "#");
         }
         return answer;
     }
