1d54b583784537a622cb0458d7bfebf2251abf8e
yunjichoi <cyjcode99@gmail.com>
2023-08-29 20:45:29 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 2.69 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,32 +1,12 @@
 class Solution {
     public String solution(int[] food) {
         String answer = "";
-        int len = 0;
-        for(int i = 0; i < food.length; i++) {
-            if(food[i] % 2 == 0) {
-                len += food[i];
-            } else {
-                len += food[i] - 1;
-                food[i]--;
-            }
+        for(int i = 1; i < food.length; i++) {
+            answer += Integer.toString(i).repeat(food[i] / 2);
         }
-        char[] arr = new char[len + 1];
-        arr[len / 2] = '0';
-        int ans_idx = 0;
-        int food_idx = 1;
-        while(true) {
-            for(int i = 0; i < food[food_idx] / 2; i++) {
-                arr[ans_idx] = (char)(food_idx + '0');
-                arr[len - ans_idx] = (char)(food_idx + '0');
-                ans_idx++;
-            }
-            food_idx++;
-            if(food_idx == food.length) {
-                break;
-            }
-        }
-        for(int num : arr) {
-            answer += num - '0';
+        answer += "0";
+        for(int i = food.length - 1; i >= 1; i--) {
+            answer += Integer.toString(i).repeat(food[i] / 2);
         }
         return answer;
     }
