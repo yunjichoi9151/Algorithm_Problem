90bbe1ffa0ed788b61cfae0c7b9f813c8cf66a01
yunjichoi <cyjcode99@gmail.com>
2023-08-30 00:43:25 +0900

    [level 1] Title: 소수 찾기, Time: 329.45 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
@@ -2,14 +2,14 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         for(int i = 2; i <= n; i++) {
-            int now = i;
-            answer++;
+            boolean check = true;
             for(int j = 2; j <= i / j; j++) {
-                if(i % j == 0 && j != 1) {
-                    answer--;
+                if(i % j == 0) {
+                    check = false;
                     break;
                 }
             }
+            if(check) answer++;
         }
         return answer;
     }
