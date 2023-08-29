7ad322276711dda0772c9591d7775e84cd984bf9
yunjichoi <cyjcode99@gmail.com>
2023-08-30 00:21:57 +0900

    [level 1] Title: 모의고사, Time: 0.94 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.java
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -2,23 +2,23 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
+        int[] cnt = new int[3];
         int[] num1 = {1, 2, 3, 4, 5};
         int[] num2 = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] num3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        int[] score = {0, 0, 0};
         for(int i = 0; i < answers.length; i++) {
-            if(answers[i] == num1[i % 5]) score[0]++;
-            if(answers[i] == num2[i % 8]) score[1]++;
-            if(answers[i] == num3[i % 10]) score[2]++;
+            if(answers[i] == num1[i % 5]) cnt[0]++;
+            if(answers[i] == num2[i % 8]) cnt[1]++;
+            if(answers[i] == num3[i % 10]) cnt[2]++;
         }
-        int max = Math.max(score[0], Math.max(score[1], score[2]));
+        int max = Math.max(cnt[0], Math.max(cnt[1], cnt[2]));
         ArrayList<Integer> list = new ArrayList<>();
-        if(max == score[0]) list.add(1);
-        if(max == score[1]) list.add(2);
-        if(max == score[2]) list.add(3);
+        for(int i = 0; i < 3; i++) {
+            if(max == cnt[i]) list.add(i);
+        }
         int[] answer = new int[list.size()];
-        for(int i = 0; i < answer.length; i++) {
-            answer[i] = list.get(i);
+        for(int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i) + 1;
         }
         return answer;
     }
