2fa80a0c0f14e39a21e1b165f185d336fafc5809
yunjichoi <cyjcode99@gmail.com>
2023-08-29 23:15:00 +0900

    [level 1] Title: 2016년, Time: 0.03 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.java
+++ 프로그래머스/lv1/12901. 2016년/2016년.java
@@ -1,13 +1,11 @@
 class Solution {
     public String solution(int a, int b) {
-        int num = b + 4;
-        int[] date = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        String[] day = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
-        if(a > 1) {
-            for(int i = 0; i < a - 1; i++) {
-                num += date[i];
-            }
+        int sum = 0;
+        String[] day = {"THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"};
+        int[] month = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        for(int i = 0; i < a - 1; i++) {
+            sum += month[i];
         }
-        return day[num % 7];
+        return day[(sum + b) % 7];
     }
 }
