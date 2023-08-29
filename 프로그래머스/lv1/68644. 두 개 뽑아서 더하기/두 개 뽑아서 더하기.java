44baab8aa9a3d5083bdf44b0cb5baaf3364d5868
yunjichoi <cyjcode99@gmail.com>
2023-08-29 13:44:23 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 5.88 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -1,31 +1,14 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] numbers) {
-        boolean[] is_include = new boolean[201];
-        int cnt = 0;
-        int max = 0;
-        int min = Integer.MAX_VALUE;
+        TreeSet<Integer> tree = new TreeSet<>();
         for(int i = 0; i < numbers.length - 1; i++) {
             for(int j = i + 1; j < numbers.length; j++) {
-                int tmp = numbers[i] + numbers[j];
-                if(!is_include[tmp]) {
-                    is_include[tmp] = true;
-                    cnt++;
-                }
-                if(max < tmp) {
-                    max = tmp;
-                }
-                if(min > tmp) {
-                    min = tmp;
-                }
+                tree.add(numbers[i] + numbers[j]);
             }
         }
-        int[] answer = new int[cnt];
-        int idx = 0;
-        for(int i = min; i <= max; i++) {
-            if(is_include[i]) {
-                answer[idx++] = i;
-            }
-        }
+        int[] answer = tree.stream().mapToInt(Integer::intValue).toArray();
         return answer;
     }
 }
