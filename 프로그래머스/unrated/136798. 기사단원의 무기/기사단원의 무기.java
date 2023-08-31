37ce8f0a3f21b0ced6373707811196d2a34f3acd
yunjichoi <cyjcode99@gmail.com>
2023-09-01 00:24:50 +0900

    [unrated] Title: 기사단원의 무기, Time: 0.18 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,18 +1,18 @@
 class Solution {
     public int solution(int number, int limit, int power) {
-        int answer = 0;
-        for(int i = 1; i <= number; i++) {
-            int cnt = 0;
-            int num = (int)Math.sqrt(i);
-            for(int j = 1; j <= num; j++) {
-                if(i % j == 0) {
-                    cnt += 2;
-                    if(j == i / j) {
-                        cnt--;
-                    }
-                }
+        int[] count = new int[number + 1];    
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= number / i; j++) {
+                count[i * j]++;
             }
-            answer += cnt > limit ? power : cnt;
+        }
+        int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            if (count[i] > limit) {
+                answer += power;
+            } else {
+                answer += count[i];
+            }
         }
         return answer;
     }
