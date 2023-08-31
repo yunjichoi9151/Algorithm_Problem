8352421dff723f8e621cd603fcc9ea3960b4853d
yunjichoi <cyjcode99@gmail.com>
2023-09-01 01:15:15 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.02 ms, Memory: 69.3 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/README.md
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/README.md
@@ -1,10 +1,10 @@
 # [level 1] 로또의 최고 순위와 최저 순위 - 77484 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77484?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77484) 
 
 ### 성능 요약
 
-메모리: 72.8 MB, 시간: 0.02 ms
+메모리: 69.3 MB, 시간: 0.02 ms
 
 ### 구분
 
--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,8 +1,8 @@
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
-        int[] answer = {0, 0};
+        int[] answer = new int[2];
         int zero_cnt = 0;
-        int same_cnt = 0;
+        int num = 0;
         for(int i = 0; i < 6; i++) {
             if(lottos[i] == 0) {
                 zero_cnt++;
@@ -10,21 +10,12 @@ class Solution {
             }
             for(int j = 0; j < 6; j++) {
                 if(lottos[i] == win_nums[j]) {
-                    same_cnt++;
+                    num++;
                 }
             }
         }
-        if(same_cnt == 0) {
-            answer[1] = 6;
-        } else {
-            answer[1] = 7 - same_cnt;
-        }
-        if(same_cnt != 0 || zero_cnt != 0) {
-            answer[0] = 7 - (same_cnt + zero_cnt);
-        } else {
-            answer[0] = 6;
-        }
-        
+        answer[0] = zero_cnt + num < 2 ? 6 : 7 - (zero_cnt + num);
+        answer[1] = num < 2 ? 6 : 7 - num;
         return answer;
     }
 }
