cc6a787273ed0ce428a295caa8dac34f6a47d784
yunjichoi <cyjcode99@gmail.com>
2023-09-01 00:09:07 +0900

    [level 1] Title: 실패율, Time: 0.87 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.java
+++ 프로그래머스/lv1/42889. 실패율/실패율.java
@@ -2,27 +2,26 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int N, int[] stages) {
-        int[] cnt = new int[N];
-        for(int i = 0; i < stages.length; i++) {
-            if(stages[i] != N + 1) {
-                cnt[stages[i] - 1]++;
-            }
-        }
-        Map<Integer, Double> map = new HashMap<>();
-        int num = stages.length;
-        for(int i = 0; i < N; i++) {
-            if(cnt[i] == 0 || num == 0) {
-                map.put(i + 1, 0.0);
-            } else {
-                map.put(i + 1, (double)cnt[i] / num);
-            }
-            num -= cnt[i];
-        }
-        List<Integer> keySet = new ArrayList<>(map.keySet());
-        keySet.sort((o1, o2) -> map.get(o2).compareTo(map.get(o1)));
         int[] answer = new int[N];
+        int[] cnt = new int[N + 1];
+        double user = stages.length;
+        for(int num : stages) {
+            cnt[num - 1]++;
+        }
+        Map<Integer, Double> fail = new HashMap<>();
         for(int i = 0; i < N; i++) {
-            answer[i] = keySet.get(i);
+            if(cnt[i] == 0 || user == 0.0) {
+                fail.put(i + 1, 0.0);
+            } else {
+                fail.put(i + 1, cnt[i] / user);
+            }
+            user -= (double)cnt[i];
+        }
+        List<Integer> keySet = new ArrayList<>(fail.keySet());
+        keySet.sort((o1, o2) -> fail.get(o2).compareTo(fail.get(o1)));
+        int idx = 0;
+        for(Integer key : keySet) {
+            answer[idx++] = key;
         }
         return answer;
     }
