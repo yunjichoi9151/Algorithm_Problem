6c893ec430280d88691f657485279e27de21919b
yunjichoi <cyjcode99@gmail.com>
2023-09-01 13:54:29 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 10.54 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -1,6 +1,24 @@
+import java.util.*;
+
 class Solution {
     public String solution(String[] survey, int[] choices) {
         String answer = "";
+        char[] arr = {'R', 'T', 'C', 'F', 'J', 'M', 'A', 'N'};
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char c : arr) {
+            map.put(c, 0);
+        }
+        for(int i = 0; i < survey.length; i++) {
+            if(choices[i] >= 5) {
+                map.put(survey[i].charAt(1), map.get(survey[i].charAt(1)) + (choices[i] - 4));
+            } else if(choices[i] <= 3) {
+                map.put(survey[i].charAt(0), map.get(survey[i].charAt(0)) + (4 - choices[i]));
+            }
+        }
+        answer += map.get('R') >= map.get('T') ? 'R' : 'T';
+        answer += map.get('C') >= map.get('F') ? 'C' : 'F';
+        answer += map.get('J') >= map.get('M') ? 'J' : 'M';
+        answer += map.get('A') >= map.get('N') ? 'A' : 'N';
         return answer;
     }
 }
