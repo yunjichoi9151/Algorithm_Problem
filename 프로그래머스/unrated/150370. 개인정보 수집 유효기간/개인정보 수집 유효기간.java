27fce693ed4acc850e18d55ef83612d8d0073764
yunjichoi <cyjcode99@gmail.com>
2023-09-01 14:28:30 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 1.15 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -2,35 +2,31 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
-        ArrayList<Integer> list = new ArrayList<>();
-        int[] now_date = convert(today);
-        int now_num = now_date[0] * 12 * 28 + now_date[1] * 28 + now_date[2];
-        int[] char_arr = new int[26];
-        for(int i = 0; i < terms.length; i++) {
-            String[] tmp = terms[i].split(" ");
-            char_arr[tmp[0].charAt(0) - 'A'] = Integer.parseInt(tmp[1]);
+        ArrayList<Integer> ans = new ArrayList<>();
+        int now = convert(today);
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String s : terms) {
+            String[] arr = s.split(" ");
+            map.put(arr[0], Integer.parseInt(arr[1]));
         }
         for(int i = 0; i < privacies.length; i++) {
-            int[] start_date = convert(privacies[i].substring(0, 10));
-            start_date[1] = start_date[1] + char_arr[privacies[i].charAt(11) - 'A'];
-            if(start_date[1] > 12) {
-                start_date[0] += start_date[1] / 12;
-                start_date[1] = start_date[1] % 12;
-            }
-            int start_num = start_date[0] * 12 * 28 + start_date[1] * 28 + start_date[2];
-            if(now_num >= start_num) {
-                list.add(i + 1);
+            String[] arr = privacies[i].split(" ");
+            int num = convert(arr[0]) + map.get(arr[1]) * 28;
+            if(now >= num) {
+                ans.add(i + 1);
             }
         }
-        int[] answer = list.stream().mapToInt(Integer::intValue).toArray();
+        int[] answer = new int[ans.size()];
+        for(int i = 0; i < ans.size(); i++) {
+            answer[i] = ans.get(i);
+        }
         return answer;
     }
-    static int[] convert(String s) {
-        int[] arr = new int[3];
-        String[] str_arr = s.split("\\.");
-        for(int i = 0; i < 3; i++) {
-            arr[i] = Integer.parseInt(str_arr[i]);
-        }
-        return arr;
+    
+    public int convert(String s) {
+        String[] arr = s.split("\\.");
+        return (Integer.parseInt(arr[0]) - 1) * 12 * 28 + 
+               (Integer.parseInt(arr[1]) - 1) * 28 +
+                Integer.parseInt(arr[2]);
     }
 }
