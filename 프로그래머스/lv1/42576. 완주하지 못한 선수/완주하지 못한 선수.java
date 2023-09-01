0990b68c301d8a2ebebb480ad62c9d80b84d5cb2
yunjichoi <cyjcode99@gmail.com>
2023-09-01 09:50:12 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 78.38 ms, Memory: 95.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,15 +3,19 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
-        Map<String, Integer> map = new HashMap<>();
-        for(String player : completion) {
-            map.put(player, map.getOrDefault(player, 0) + 1);
-        }
-        for(String player : participant) {
-            if(!map.containsKey(player) || map.get(player) == 0) {
-                return player;
+        HashMap<String, Integer> map = new HashMap<>();
+        for(int i = 0; i < completion.length; i++) {
+            if(map.containsKey(completion[i])) {
+                map.put(completion[i], map.get(completion[i]) + 1);
             } else {
-                map.put(player, map.get(player) - 1);
+                map.put(completion[i], 1);
+            }
+        }
+        for(int i = 0; i < participant.length; i++) {
+            if(!map.containsKey(participant[i]) || map.get(participant[i]) == 0) {
+                return participant[i];
+            } else {
+                map.put(participant[i], map.get(participant[i]) - 1);
             }
         }
         return answer;
