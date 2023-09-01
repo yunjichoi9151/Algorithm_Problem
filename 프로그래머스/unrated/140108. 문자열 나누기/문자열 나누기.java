2119e5eeda4c8f4b1dfec0cde70d510734cca928
yunjichoi <cyjcode99@gmail.com>
2023-09-01 10:10:52 +0900

    [unrated] Title: 문자열 나누기, Time: 0.40 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.java
@@ -1,43 +1,25 @@
 class Solution {
     public int solution(String s) {
-        int answer = 0;
+        int answer = 1;
+        int start = s.charAt(0);
+        int same_cnt = 1;
+        int diff_cnt = 0;
+        int len = s.length();
         int idx = 1;
-        int cnt1 = 0;
-        int cnt2 = 0;
-        char before = s.charAt(0);
-        boolean isClear = false;
-        while(idx < s.length()) {
-            if(before == s.charAt(idx)) {
-                cnt1++;
-                isClear = false;
+        while(idx < len) {
+            if(s.charAt(idx) == start) {
+                same_cnt++;
             } else {
-                cnt2++;
-                if(cnt1 == 0 || cnt1 == cnt2 - 1) {
+                diff_cnt++;
+            }
+            if(same_cnt == diff_cnt) {
+                if(idx + 1 < len) {
+                    start = s.charAt(idx + 1);
+                    same_cnt = diff_cnt = 0;
                     answer++;
-                    cnt1 = 0;
-                    cnt2 = 0;
-                    if(idx + 1 >= s.length()) {
-                        continue;
-                    }
-                    before = s.charAt(idx + 1);
-                    idx++;
-                    isClear = true;
-                } else {
-                    isClear = false;
                 }
             }
             idx++;
-            if(idx >= s.length() - 1) {
-                if(cnt1 == 0 && cnt2 == 0) {
-                    answer++;
-                } else if(!isClear) {
-                    answer++;
-                }
-                break;
-            }
-        }
-        if(s.length() == 1) {
-            answer = 1;
         }
         return answer;
     }
