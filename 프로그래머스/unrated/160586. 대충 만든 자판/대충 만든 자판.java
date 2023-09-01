25715dd37ad6f3fce32773872c12476a5d1ad673
yunjichoi <cyjcode99@gmail.com>
2023-09-01 10:29:39 +0900

    [unrated] Title: 대충 만든 자판, Time: 5.70 ms, Memory: 68.8 MB -BaekjoonHub

--- 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.java
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.java
@@ -1,24 +1,22 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(String[] keymap, String[] targets) {
         int[] answer = new int[targets.length];
-        int[] min_arr = new int[26];
-        Arrays.fill(min_arr, -1);
-        for(int i = 0; i < keymap.length; i++) {
-            for(int j = 0; j < keymap[i].length(); j++) {
-                int num = keymap[i].charAt(j) - 'A';
-                min_arr[num] = min_arr[num] == -1 ? j + 1 : Math.min(min_arr[num], j + 1);
-            }
-        }
         for(int i = 0; i < targets.length; i++) {
             for(int j = 0; j < targets[i].length(); j++) {
-                int num = targets[i].charAt(j) - 'A';
-                if(min_arr[num] == -1) {
+                char c = targets[i].charAt(j);
+                int min = Integer.MAX_VALUE;
+                for(int k = 0; k < keymap.length; k++) {
+                    int tmp = keymap[k].indexOf(c);
+                    if(tmp != -1 && min > tmp) {
+                        min = tmp;
+                    }
+                }
+                if(min == Integer.MAX_VALUE) {
                     answer[i] = -1;
                     break;
+                } else {
+                    answer[i] += min + 1;
                 }
-                answer[i] += min_arr[num];
             }
         }
         return answer;
