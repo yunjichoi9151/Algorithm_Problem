d72b86990034345bb1b916aec4af35a72339ae0c
yunjichoi <cyjcode99@gmail.com>
2023-09-01 12:56:37 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.72 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,28 +1,20 @@
 class Solution {
     public String solution(String new_id) {
         String answer = "";
-        new_id = new_id.toLowerCase().replaceAll("[^a-z0-9-_.]", "").replaceAll("\\.{2,}", ".");
-        if(new_id.charAt(0) == '.') {
-            new_id = new_id.substring(1);
-        }
-        if(new_id.equals(".")) {
-            new_id = "";
-        } else if(new_id.length() != 0 && new_id.charAt(new_id.length() - 1) == '.') {
-            new_id = new_id.substring(0, new_id.length() - 1);
-        }
-        if(new_id.equals("")) {
-            new_id = "a";
-        }
+        new_id = new_id.toLowerCase();
+        new_id = new_id.replaceAll("[^a-z0-9\\-\\_\\.]", "");
+        new_id = new_id.replaceAll("\\.{2,}", ".");
+        if(new_id.length() > 0 && new_id.charAt(0) == '.') new_id = new_id.substring(1);
+        if(new_id.length() > 0 && new_id.charAt(new_id.length() - 1) == '.') new_id = new_id.substring(0, new_id.length() - 1);
+        if(new_id.length() == 0) new_id = "a";
         if(new_id.length() >= 16) {
             new_id = new_id.substring(0, 15);
-            if(new_id.charAt(14) == '.') {
-                new_id = new_id.substring(0, 14);
-            }
+            if(new_id.charAt(14) == '.') new_id = new_id.substring(0, 14);
         }
-        if(new_id.length() <= 2) {
-            while(new_id.length() < 3) {
-                new_id = new_id + new_id.charAt(new_id.length() - 1);
-            }
+        if(new_id.length() == 1) {
+            new_id = new_id.repeat(3);
+        } else if(new_id.length() == 2) {
+            new_id += new_id.charAt(1);
         }
         return new_id;
     }
