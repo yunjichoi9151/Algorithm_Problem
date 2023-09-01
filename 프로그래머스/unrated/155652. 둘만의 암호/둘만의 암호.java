e109651558c3de5cbe3eb0c7fda93c2cc8de728c
yunjichoi <cyjcode99@gmail.com>
2023-09-01 10:56:18 +0900

    [unrated] Title: 둘만의 암호, Time: 13.66 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
@@ -1,21 +1,26 @@
 class Solution {
     public String solution(String s, String skip, int index) {
         String answer = "";
-        boolean[] arr = new boolean[26];
-        for(int i = 0; i < skip.length(); i++) {
-            arr[skip.charAt(i) - 'a'] = true;
-        }
         for(int i = 0; i < s.length(); i++) {
-            int idx = s.charAt(i) - 'a';
-            int num = index;
-            int start = idx;
-            while (num > 0) {
-                start = (start + 1) % 26;
-                if (!arr[start]) {
-                    num--;
+            int tmp = 0;
+            int idx = 0;
+            int now = (s.charAt(i) - 'a' + 1) % 26;
+            while(tmp < index) {
+                boolean isTrue = false;
+                for(int j = 0; j < skip.length(); j++) {
+                    if(skip.charAt(j) - 'a' == now) {
+                        now = (now + 1) % 26;
+                        isTrue = true;
+                        break;
+                    }
+                }
+                if(!isTrue) {
+                    tmp++;
+                    now = (now + 1) % 26;
                 }
             }
-            answer += (char)(start + 'a') + "";
+            answer += (char)((now == 0 ? 25 : now - 1) + 'a');
+            
         }
         return answer;
     }
