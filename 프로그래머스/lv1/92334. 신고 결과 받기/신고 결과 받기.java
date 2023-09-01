06b81ce4033ba2807dcdd7549aa728d8c8ee451e
yunjichoi <cyjcode99@gmail.com>
2023-09-01 15:34:03 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.16 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -2,34 +2,29 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        int num = id_list.length;
-        int[] ans = new int[num];
-        Map<String, Set<String>> reported = new HashMap<>();
-        Map<String, Integer> cnt = new HashMap<>();
-
-        for(int i = 0; i < num; i++) {
-            reported.put(id_list[i], new HashSet<>());
-            cnt.put(id_list[i], 0);
+        int[] answer = new int[id_list.length];
+        HashMap<String, Set<String>> reported = new HashMap<>();
+        HashMap<String, Integer> cnt = new HashMap<>();
+        for(String s : id_list) {
+            reported.put(s, new HashSet<>());
+            cnt.put(s, 0);
         }
-
-        for(int i = 0; i < report.length; i++) {
-            String[] rs = report[i].split(" ");
-            Set<String> tmp1 = reported.get(rs[1]);
-            tmp1.add(rs[0]);
+        for(String s : report) {
+            String[] arr = s.split(" ");
+            Set<String> tmp1 = reported.get(arr[1]);
+            tmp1.add(arr[0]);
         }
-
-        for(int i = 0; i < num; i++) {
+        for(int i = 0; i < id_list.length; i++) {
             Set<String> tmp2 = reported.get(id_list[i]);
             if(tmp2.size() >= k) {
                 for(String s : tmp2) {
-                    Integer val = cnt.get(s);
-                    cnt.replace(s, val + 1);
+                    cnt.put(s, cnt.get(s) + 1);
                 }
             }
         }
-        for(int i = 0; i < num; i++) {
-            ans[i] = cnt.get(id_list[i]);
+        for(int i = 0; i < answer.length; i++) {
+            answer[i] = cnt.get(id_list[i]);
         }
-        return ans;
+        return answer;
     }
 }
