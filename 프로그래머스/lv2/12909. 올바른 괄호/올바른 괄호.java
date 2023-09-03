b75bbfe55ff0e777f41cf3d2cbb19ba536872247
yunjichoi <cyjcode99@gmail.com>
2023-09-03 13:42:42 +0900

    [level 2] Title: 올바른 괄호, Time: 19.26 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,15 +2,30 @@ import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        char[] arr = s.toCharArray();
+        boolean answer = true;
         Stack<Character> stack = new Stack<>();
-        for(char c : arr) {
-            if(c == '(') stack.push('(');
-            else {
-                if(stack.size() > 0 && stack.peek() == '(') stack.pop();
-                else return false;
+        int cnt = 0;
+        for(int i = 0; i < s.length(); i++) {
+            switch(s.charAt(i)) {
+                case '(':
+                    stack.push('(');
+                    break;
+                case ')':
+                    if(stack.isEmpty()) {
+                        answer = false;
+                        break;
+                    } else if(stack.peek() == '(') {
+                        stack.pop();
+                    } else {
+                        answer = false;
+                        break;
+                    }
+                    break;
             }
         }
-        return stack.size() == 0 ? true : false;
+        if(stack.size() != 0) {
+            answer = false;
+        }
+        return answer;
     }
 }
