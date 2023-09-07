4937fb6ec53dc54ceb3ece5e470ba5888ddfc051
yunjichoi <cyjcode99@gmail.com>
2023-09-07 23:49:36 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.java
@@ -1,18 +1,21 @@
-class Solution
-{
-    public int solution(int n, int a, int b)
-    {
+class Solution {
+    public int solution(int n, int a, int b) {
         int answer = 1;
         while(true) {
-            if(a % 2 == 0 && b == a - 1) {
-                break;
-            } else if(b % 2 == 0 && a == b - 1) {
-                break;
+            if(a % 2 == 0 && a - b == 1 || b % 2 == 0 && b - a == 1) {
+                return answer;
+            } else {
+                a = cal(a);
+                b = cal(b);
+                answer++;
             }
-            a = a % 2 == 0 ? a / 2 : (a + 1) / 2;
-            b = b % 2 == 0 ? b / 2 : (b + 1) / 2;
-            answer++;
         }
-        return answer;
+    }
+    public int cal(int x) {
+        if(x % 2 == 0) {
+            return x / 2;
+        } else {
+            return (x + 1) / 2;
+        }
     }
 }
