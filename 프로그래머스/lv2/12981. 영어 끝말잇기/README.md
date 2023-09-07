e05e4d34408df9a8eac5527b18bb374199ec36bd
yunjichoi <cyjcode99@gmail.com>
2023-09-07 22:27:24 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.19 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/README.md
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 영어 끝말잇기 - 12981 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981) 
 
 ### 성능 요약
 
-메모리: 77.1 MB, 시간: 0.21 ms
+메모리: 81.4 MB, 시간: 0.19 ms
 
 ### 구분
 
--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,20 +1,17 @@
 class Solution {
     public int[] solution(int n, String[] words) {
-        char before = words[0].charAt(words[0].length() - 1);
+        char c = words[0].charAt(words[0].length() - 1);
         for(int i = 1; i < words.length; i++) {
-            if(words[i].length() == 1) {
-                return new int[]{i % n + 1, ((i + 1) % n == 0) ? (i + 1) / n : (i + 1) / n + 1};
-            }
-            char now = words[i].charAt(0);
-            if(before != now) {
-                return new int[]{i % n + 1, ((i + 1) % n == 0) ? (i + 1) / n : (i + 1) / n + 1};
-            }
-            for(int j = 0; j < i; j++) {
-                if(words[j].equals(words[i])) {
-                    return new int[]{i % n + 1, ((i + 1) % n == 0) ? (i + 1) / n : (i + 1) / n + 1};
+            if(words[i].length() == 1 || words[i].charAt(0) != c) {
+                return new int[]{i % n + 1, i / n + 1};
+            } else {
+                for(int j = 0; j < i; j++) {
+                    if(words[j].equals(words[i])) {
+                        return new int[]{i % n + 1, i / n + 1};
+                    }
                 }
+                c = words[i].charAt(words[i].length() - 1);
             }
-            before = words[i].charAt(words[i].length() - 1);
         }
         return new int[]{0, 0};
     }
