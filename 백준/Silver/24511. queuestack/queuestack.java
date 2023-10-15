5a3464e73eac130d93e7fe77103c8ea02d8d5b99
yunjichoi <cyjcode99@gmail.com>
2023-10-15 17:12:59 +0900

    [Silver III] Title: queuestack, Time: 672 ms, Memory: 69944 KB -BaekjoonHub

--- 백준/Silver/24511. queuestack/queuestack.java
+++ 백준/Silver/24511. queuestack/queuestack.java
@@ -1,33 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Deque;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
-        List<int[]> list = new ArrayList<>();
         Deque<Integer> q = new LinkedList<>();
-        int[] tmp = new int[n];
+        String[] tmp1 = br.readLine().split(" ");
+        String[] tmp2 = br.readLine().split(" ");
         for(int i = 0; i < n; i++) {
-            tmp[i] = Integer.parseInt(st.nextToken());
+            if(tmp1[i].equals("0")) q.add(Integer.parseInt(tmp2[i]));
         }
-        st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < n; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            list.add(new int[]{tmp[i], num});
-            if(tmp[i] == 0) q.add(num);
-        }
-        st = new StringTokenizer(br.readLine());
-        int m = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 0; i < m; i++) {
             int in = Integer.parseInt(st.nextToken());
             if(q.size() == 0) {
