a5ef2ffa51ed4ae0a61201c5ade5d6b6c48e81b9
yunjichoi <cyjcode99@gmail.com>
2023-12-23 11:18:42 +0900

    [Bronze V] Title: 개, Time: 116 ms, Memory: 13956 KB -BaekjoonHub

--- 백준/Bronze/10172. 개/개.java
+++ 백준/Bronze/10172. 개/개.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("|\\_/|");
-        System.out.println("|q p|   /}");
-        System.out.println("( 0 )\"\"\"\\");
-        System.out.println("|\"^\"`    |");
-        System.out.println("||_/=\\\\__|");
+        System.out.println("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|");
     }
 }
