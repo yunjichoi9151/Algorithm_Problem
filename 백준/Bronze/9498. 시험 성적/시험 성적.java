7fe743a033cffbe96d0c7d806dc7edd6472066c4
yunjichoi <cyjcode99@gmail.com>
2023-12-23 11:23:11 +0900

    [Bronze V] Title: 시험 성적, Time: 204 ms, Memory: 17740 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        if(n >= 90) {
+        int score = sc.nextInt();
+        if(score >= 90) {
             System.out.println("A");
-        } else if(n >= 80) {
+        } else if(score >= 80) {
             System.out.println("B");
-        } else if(n >= 70) {
+        } else if(score >= 70) {
             System.out.println("C");
-        } else if(n >= 60) {
+        } else if(score >= 60) {
             System.out.println("D");
         } else {
             System.out.println("F");
