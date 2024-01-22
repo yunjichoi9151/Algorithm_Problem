0c3cbebd153c33390279e8af1f79718b2aa06075
yunjichoi <cyjcode99@gmail.com>
2024-01-22 23:25:23 +0900

    [Silver IV] Title: 큐 2, Time: 1368 ms, Memory: 407492 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.java
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -1,40 +1,40 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Deque;
-import java.util.LinkedList;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Deque<Integer> que = new LinkedList<>();
         StringBuilder sb = new StringBuilder();
-        int n = Integer.parseInt(br.readLine());
-        int last = 0;
-        for(int i = 0; i < n; i++) {
-            String[] s = br.readLine().split(" ");
-            switch(s[0]) {
-                case "push":
-                    last = Integer.parseInt(s[1]);
-                    que.add(last);
-                    break;
-                case "pop":
-                    sb.append(que.isEmpty() ? -1 : que.poll());
-                    break;
-                case "size":
-                    sb.append(que.size());
-                    break;
-                case "empty":
-                    sb.append(que.isEmpty() ? 1 : 0);
-                    break;
-                case "front":
-                    sb.append(que.isEmpty() ? -1 : que.peek());
-                    break;
-                case "back":
-                    sb.append(que.isEmpty() ? -1 : last);
-                    break;
+        Queue<Integer> que = new LinkedList<>();
+        int N = Integer.parseInt(br.readLine());
+        int last = -1;
+        for(int i = 0; i < N; i++) {
+            String s = br.readLine();
+            if(s.length() < 6) {
+                switch(s) {
+                    case "pop":
+                        sb.append(!que.isEmpty() ? que.poll() : -1);
+                        break;
+                    case "size":
+                        sb.append(que.size());
+                        break;
+                    case "empty":
+                        sb.append(que.isEmpty() ? 1 : 0);
+                        break;
+                    case "front":
+                        sb.append(!que.isEmpty() ? que.peek() : -1);
+                        break;
+                    case "back":
+                        sb.append(!que.isEmpty() ? last : -1);
+                        break;
+                }
+                sb.append('\n');
+            } else {
+                String[] tmp = s.split(" ");
+                int num = Integer.parseInt(tmp[1]);
+                que.add(num);
+                last = num;
             }
-            if(!s[0].equals("push")) sb.append("\n");
         }
         System.out.print(sb.toString());
     }
