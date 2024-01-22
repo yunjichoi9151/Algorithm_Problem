d3131d3bf7ce765e7a8ff405201c987357f6e355
yunjichoi <cyjcode99@gmail.com>
2024-01-22 21:57:08 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 232 ms, Memory: 18940 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -1,46 +1,50 @@
-import java.util.Scanner;
-import java.util.Stack;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         while(true) {
-            s = sc.nextLine();
-            if(s.equals(".")) {
-                break;
+            String s = br.readLine();
+            if(s.equals(".")) break;
+            int idx = 0;
+            Stack<Character> stack = new Stack<>();
+            boolean isTrue = true;
+            outerLoop:
+            while(s.charAt(idx) != '.') {
+                char c = s.charAt(idx);
+                switch(c) {
+                    case '(' :
+                    case '[' :
+                        stack.push(c);
+                        break;
+                    case ')' :
+                        if(!stack.isEmpty() && stack.peek() == '(') {
+                            stack.pop();
+                            break;
+                        } else {
+                            isTrue = false;
+                            break outerLoop;
+                        }
+                    case ']' :
+                        if(!stack.isEmpty() && stack.peek() == '[') {
+                            stack.pop();
+                            break;
+                        } else {
+                            isTrue = false;
+                            break outerLoop;
+                        }
+                }
+                idx++;
             }
-            System.out.println(result(s));
+            if(stack.isEmpty() && isTrue && idx == s.length() - 1) {
+                sb.append("yes");
+            } else {
+                sb.append("no");
+            }
+            sb.append('\n');
         }
-    }
-    public static String result(String s) {
-        Stack<Character> stack = new Stack<>();
-        String res = "yes";
-        for(int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if(c == '(' || c == '[') {
-                stack.push(c);
-            }
-            else if(c == ')') {
-                if(stack.empty() || stack.peek() != '(') {
-                    res = "no";
-                }
-                else {
-                    stack.pop();
-                }
-            }
-            else if(c == ']') {
-                if(stack.empty() || stack.peek() != '[') {
-                    res = "no";
-                }
-                else {
-                    stack.pop();
-                }
-            }
-        }
-        if(!stack.empty()) {
-            res = "no";
-        }
-        return res;
+        System.out.print(sb.toString());
     }
 }
