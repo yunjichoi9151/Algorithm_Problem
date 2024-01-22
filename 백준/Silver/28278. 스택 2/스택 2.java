6ca66f27c7f7582e2b2e3bdaa59f492979c4d0da
yunjichoi <cyjcode99@gmail.com>
2024-01-22 21:08:56 +0900

    [Silver IV] Title: 스택 2, Time: 972 ms, Memory: 308540 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.java
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -1,42 +1,35 @@
-import java.util.Scanner;
-import java.util.Stack;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		Stack<Integer> stack = new Stack<>();
-		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < n; i++) {
-			int k = sc.nextInt();
-			switch(k) {
-				case 1:
-					stack.push(sc.nextInt());
-					break;
-				case 2:
-					if(!stack.isEmpty()) {
-						sb.append(stack.pop());
-					} else {
-						sb.append(-1);
-					}
-					break;
-				case 3:
-					sb.append(stack.size());
-					break;
-				case 4:
-					sb.append(stack.isEmpty() ? 1 : 0);
-					break;
-				case 5:
-					if(!stack.isEmpty()) {
-						sb.append(stack.peek());
-					} else {
-						sb.append(-1);
-					}
-					break;
-			}
-			if(k != 1) sb.append("\n");
-		}
-		System.out.print(sb.toString());
-		sc.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0; i < N; i++) {
+            String s = br.readLine();
+            if(s.length() == 1) {
+                switch(Integer.parseInt(s)) {
+                    case 2:
+                        sb.append(!stack.isEmpty() ? stack.pop() : -1);
+                        break;
+                    case 3:
+                        sb.append(stack.size());
+                        break;
+                    case 4:
+                        sb.append(stack.isEmpty() ? 1 : 0);
+                        break;
+                    case 5:
+                        sb.append(!stack.isEmpty() ? stack.peek() : -1);
+                        break;
+                }
+                sb.append('\n');
+            } else {
+                String[] tmp = s.split(" ");
+                stack.push(Integer.parseInt(tmp[1]));
+            }
+        }
+        System.out.print(sb.toString());
+    }
 }
