d7a7eafa53d674397d6aedbb039891216f586576
yunjichoi <cyjcode99@gmail.com>
2024-01-22 23:13:45 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 128 ms, Memory: 14408 KB -BaekjoonHub

--- 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.java
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.java
@@ -1,26 +1,36 @@
-import java.util.Scanner;
-import java.util.Stack;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int now = 1;
-		Stack<Integer> stack = new Stack<>();
-		for(int i = 0; i < n; i++) {
-			int tmp = sc.nextInt();
-			stack.push(tmp);
-			while(!stack.isEmpty()) {
-				if(stack.peek() == now) {
-					now++;
-					stack.pop();
-				} else {
-					break;
-				}
-			}
-		}
-
-		System.out.println(stack.isEmpty() ? "Nice" : "Sad");
-		sc.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+        int now = 1;
+        boolean isPossible = true;
+        for(int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            while(!stack.isEmpty() && stack.peek() == now) {
+                now++;
+                stack.pop();
+            }
+            if(num == now) {
+                now++;
+            } else {
+                stack.push(num);
+            }
+        }
+        int size = stack.size();
+        for(int i = 0; i < size; i++) {
+            if(stack.peek() == now) {
+                now++;
+                stack.pop();
+            } else {
+                isPossible = false;
+                break;
+            }
+        }
+        System.out.println(stack.isEmpty() && isPossible ? "Nice" : "Sad");
+    }
 }
