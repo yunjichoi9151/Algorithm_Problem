8dc6d407be30a103564128f07e7fc727d4ddbead
yunjichoi <cyjcode99@gmail.com>
2024-01-23 11:18:21 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 288 ms, Memory: 33388 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -1,36 +1,25 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(), k = sc.nextInt();
-        Queue<Integer> queue = new LinkedList<>();
-        int[] arr = new int[n];
-        int j = 0;
-        for(int i = 1; i <= n; i++) {
-            queue.add(i);
+        StringBuilder sb = new StringBuilder();
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        Queue<Integer> que = new LinkedList<>();
+        sb.append("<");
+        for(int i = 1; i <= N; i++) {
+            que.add(i);
         }
-        while(queue.size() != 0) {
-            for(int i = 1; i < k; i++) {
-                queue.add(queue.peek());
-                queue.remove();
+        while(!que.isEmpty()) {
+            for(int i = 0; i < K - 1; i++) {
+                int x = que.poll();
+                que.add(x);
             }
-            if(j < n) {
-                arr[j] = queue.peek();
-                j++;
-                queue.remove();
-            }
+            sb.append(que.poll() + (!que.isEmpty() ? ", " : ""));
         }
-        System.out.print("<");
-        for(int i = 0; i < n; i++) {
-            System.out.print(arr[i]);
-            if(i < n - 1) {
-                System.out.print(", ");
-            }
-        }
-        System.out.print(">");
+        sb.append('>');
+        System.out.println(sb.toString());
         sc.close();
     }
 }
