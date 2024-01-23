d40e22659f455955a37c1e37ca6d4f57b5eb10f7
yunjichoi <cyjcode99@gmail.com>
2024-01-23 15:11:35 +0900

    [Silver III] Title: queuestack, Time: 692 ms, Memory: 66984 KB -BaekjoonHub

--- 백준/Silver/24511. queuestack/queuestack.java
+++ 백준/Silver/24511. queuestack/queuestack.java
@@ -1,31 +1,27 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-        Deque<Integer> q = new LinkedList<>();
-        String[] tmp1 = br.readLine().split(" ");
-        String[] tmp2 = br.readLine().split(" ");
-        for(int i = 0; i < n; i++) {
-            if(tmp1[i].equals("0")) q.add(Integer.parseInt(tmp2[i]));
-        }
-        int m = Integer.parseInt(br.readLine());
+        Deque<Integer> deq = new ArrayDeque<>();
+        int N = Integer.parseInt(br.readLine());
+        boolean[] check = new boolean[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < m; i++) {
-            int in = Integer.parseInt(st.nextToken());
-            if(q.size() == 0) {
-                sb.append(in + " ");
-            } else {
-                q.addFirst(in);
-                sb.append(q.pollLast() + " ");
-            }
+        for(int i = 0; i < N; i++) {
+            check[i] = Integer.parseInt(st.nextToken()) == 0;
+        }
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if(check[i]) deq.addFirst(num);
+        }
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < M; i++) {
+            deq.addLast(Integer.parseInt(st.nextToken()));
+            sb.append(deq.pollFirst() + " ");
         }
         System.out.println(sb.toString());
     }
