12948d775e14cfb2c2fbaaaada49427b385bf223
yunjichoi <cyjcode99@gmail.com>
2024-01-25 21:58:29 +0900

    [Silver V] Title: 숫자 카드, Time: 944 ms, Memory: 136156 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,33 +1,21 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
- 
+import java.io.*;
+import java.util.*;
+
 public class Main {
- 
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	
-		int[] counting = new int[20000001];
-		
-		
-		int N = Integer.parseInt(br.readLine());
-		
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
-		for(int i = 0 ; i < N; i++) {
-			counting[Integer.parseInt(st.nextToken()) + 10000000]++;
-		}
-		
-		int M = Integer.parseInt(br.readLine());
-		
-		st = new StringTokenizer(br.readLine(), " ");
-		StringBuilder sb = new StringBuilder();
-		
-		for(int i = 0; i < M; i++) {
-			sb.append(counting[Integer.parseInt(st.nextToken()) + 10000000]).append(' ');
-		}
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        HashMap<Integer, Integer> map = new HashMap<>();
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < N; i++) {
+            map.put(Integer.parseInt(st.nextToken()), 1);
+        }
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < M; i++) {
+            sb.append(map.containsKey(Integer.parseInt(st.nextToken())) ? "1 " : "0 ");
+        }
+        System.out.println(sb.toString().trim());
+    }
 }
