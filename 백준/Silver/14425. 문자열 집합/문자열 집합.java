e48c45cd536ad5ed13531173bc07fd58c10cc11c
yunjichoi <cyjcode99@gmail.com>
2024-01-25 22:02:15 +0900

    [Silver IV] Title: 문자열 집합, Time: 416 ms, Memory: 31480 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,22 +1,20 @@
-import java.util.HashSet;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int m = sc.nextInt();
-		int cnt = 0;
-		HashSet<String> set = new HashSet<>();
-		for(int i = 0; i < n; i++) {
-			set.add(sc.next());
-		}
-		for(int i = 0; i < m; i++) {
-			if(set.contains(sc.next())) {
-				cnt++;
-			}
-		}
-		System.out.println(cnt);
-		sc.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        HashSet<String> set = new HashSet<>();
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int ans = 0;
+        for(int i = 0; i < N; i++) {
+            set.add(br.readLine());
+        }
+        for(int i = 0; i < M; i++) {
+            if(set.contains(br.readLine())) ans++;
+        }
+        System.out.println(ans);
+    }
 }
