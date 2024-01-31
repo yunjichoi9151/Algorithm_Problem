5217840e25abf40d249809f47aae6f839a8b8566
yunjichoi <cyjcode99@gmail.com>
2024-01-31 11:11:47 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 268 ms, Memory: 18704 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -1,23 +1,22 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		int c = sc.nextInt();
-		int d = sc.nextInt();
-		int e = sc.nextInt();
-		int f = sc.nextInt();
-        outerLoop:
-		for (int i = -999; i < 1000; i++) {
-			for (int j = -999; j < 1000; j++) {
-				if(a * i + b * j == c && d * i + e * j == f) {
-					System.out.println(i + " " + j);
-					break outerLoop;
-				}
-			}
-		}
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int e = sc.nextInt();
+        int f = sc.nextInt();
+        for(int i = -999; i <= 999; i++) {
+            for(int j = -999; j <= 999; j++) {
+                if(a * i + b * j == c && d * i + e * j == f) {
+                    System.out.println(i + " " + j);
+                    break;
+                }
+            }
+        }
         sc.close();
     }
 }
