bb7ba39609c84aae2236a525dd049a58762d8c9b
yunjichoi <cyjcode99@gmail.com>
2024-02-07 11:09:24 +0900

    [Silver I] Title: 숨바꼭질, Time: 156 ms, Memory: 18896 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static class Node {
+        int x;
+        int time;
+        public Node(int x, int time) {
+            this.x = x;
+            this.time = time;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        Queue<Node> que = new LinkedList<Node>();
+        boolean[] visited = new boolean[100001];
+        que.add(new Node(N, 0));
+        while(!que.isEmpty()) {
+            Node node = que.poll();
+            if(node.x == K) {
+                System.out.println(node.time);
+                break;
+            }
+            if(node.x + 1 <= 100000 && !visited[node.x + 1]) {
+                que.add(new Node(node.x + 1, node.time + 1));
+                visited[node.x + 1] = true;
+            }
+            if(node.x - 1 >= 0 && !visited[node.x - 1]) {
+                que.add(new Node(node.x - 1, node.time + 1));
+                visited[node.x - 1] = true;
+            }
+            if(node.x * 2 <= 100000 && !visited[node.x * 2]) {
+                que.add(new Node(node.x * 2, node.time + 1));
+                visited[node.x * 2] = true;
+            }
+        }
+    }
+}
