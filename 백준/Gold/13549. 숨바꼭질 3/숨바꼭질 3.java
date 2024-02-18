8d57f20717a78262aee043f584cbef07b854bdc9
yunjichoi <cyjcode99@gmail.com>
2024-02-18 19:11:43 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 232 ms, Memory: 22352 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,40 +1,41 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-    static int[] time = new int[100001];
-    static int[][] dx = {{2, 0}, {1, -1}, {1, 1}};
-    static boolean visit[] = new boolean[100001];
+    static class Node {
+        int x;
+        int time;
+        public Node(int x, int time) {
+            this.x = x;
+            this.time = time;
+        }
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(), k = sc.nextInt();
-        if(k <= n) {
-            System.out.println(n - k);
-        } else {
-            bfs(n, k);
-            System.out.println(time[k]);
-        }
-        sc.close();
-    }
-
-    public static void bfs(int start, int end) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(start);
-        time[start] = 0;
-        while(!queue.isEmpty()) {
-            int tmp = queue.poll();
-            for(int i = 0; i < 3; i++) {
-                int x = dx[i][0] * tmp + dx[i][1] * 1;
-                if(x < 0 || 100000 < x) {
-                    continue;
-                }
-                if(!visit[x]) {
-                    time[x] = time[tmp] + Math.abs(dx[i][1]);
-                    queue.add(x);
-                    visit[x] = true;
-                }
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        boolean[] visited = new boolean[100001];
+        Queue<Node> que = new LinkedList<Node>();
+        que.add(new Node(N, 0));
+        visited[N] = true;
+        while(!que.isEmpty()) {
+            Node node = que.poll();
+            if(node.x == K) {
+                System.out.println(node.time);
+                break;
+            }
+            if(node.x * 2 <= 100000 && !visited[node.x * 2]) {
+                que.add(new Node(node.x * 2, node.time));
+                visited[node.x * 2] = true;
+            }
+            if(node.x - 1 >= 0 && !visited[node.x - 1]) {
+                que.add(new Node(node.x - 1, node.time + 1));
+                visited[node.x - 1] = true;
+            }
+            if(node.x + 1 <= 100000 && !visited[node.x + 1]) {
+                que.add(new Node(node.x + 1, node.time + 1));
+                visited[node.x + 1] = true;
             }
         }
+        sc.close();
     }
 }
