2e0b26f1ac52e3c89236a19d5ffbfd7a1b03d5d2
yunjichoi <cyjcode99@gmail.com>
2024-05-17 23:43:06 +0900

    [Silver III] Title: 퇴사, Time: 128 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] T = new int[N];
+        int[] P = new int[N];
+        int[] dp = new int[N + 1];
+        for(int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            T[i] = Integer.parseInt(st.nextToken());
+            P[i] = Integer.parseInt(st.nextToken());
+        }
+        for(int i = 0; i < N; i++) {
+            if(i + T[i] <= N) {
+                dp[i + T[i]] = Math.max(dp[i + T[i]], dp[i] + P[i]);
+            }
+            dp[i + 1] = Math.max(dp[i + 1], dp[i]);
+        }
+        System.out.println(dp[N]);
+    }
+}
