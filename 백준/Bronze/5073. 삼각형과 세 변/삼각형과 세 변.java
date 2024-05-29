350d3d80f67cd09511de11a4e98757cbc37dfc10
yunjichoi <cyjcode99@gmail.com>
2024-05-29 16:55:21 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 120 ms, Memory: 14028 KB -BaekjoonHub

--- 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.java
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.java
@@ -1,27 +1,21 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        while(a != 0 && b != 0 && c != 0) {
-            if(2 * Math.max(Math.max(a, b), c) < (a + b + c)) {
-                if(a == b && b == c) {
-                    System.out.println("Equilateral");
-                } else if(a == b || b == c || c == a) {
-                    System.out.println("Isosceles");
-                } else {
-                    System.out.println("Scalene");
-                }
-            } else {
-                System.out.println("Invalid");
-            }
-            a = sc.nextInt();
-            b = sc.nextInt();
-            c = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        while(true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            if(a == 0 && b == 0 && c == 0) break;
+            else if(a == b && b == c && c == a) sb.append("Equilateral\n");
+            else if(a >= b + c || b >= a + c || c >= a + b) sb.append("Invalid\n");
+            else if(a != b && b != c && c != a) sb.append("Scalene\n");
+            else sb.append("Isosceles\n");
         }
-        sc.close();
+        System.out.println(sb.toString().trim());
     }
 }
