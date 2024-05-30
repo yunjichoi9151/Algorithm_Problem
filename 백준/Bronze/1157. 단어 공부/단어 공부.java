4964cb8719b26b0d550c26e9562d239f9485447f
yunjichoi <cyjcode99@gmail.com>
2024-05-30 15:31:57 +0900

    [Bronze I] Title: 단어 공부, Time: 476 ms, Memory: 31328 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -3,25 +3,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s = sc.next().toLowerCase();
-        int[] arr = new int[26];
+        String s = sc.next().toUpperCase();
+        char[] arr = s.toCharArray();
+        int[] cnt = new int[26];
         int max = 0;
-        char ans = '?';
-        for(int i = 0; i < s.length(); i++) {
-            if('a' <= s.charAt(i) && s.charAt(i) <= 'z') {
-                arr[s.charAt(i) - 'a']++;
-            }
+        char answer = '?';
+        boolean already = false;
+        for(char c : arr) {
+            cnt[c - 'A']++;
+            if(max < cnt[c - 'A']) max = cnt[c - 'A'];
         }
         for(int i = 0; i < 26; i++) {
-            if(max < arr[i]) {
-                max = arr[i];
-                ans = (char)(i + 65);
-            }
-            else if(arr[i] == max) {
-                ans = '?';
+            if(max == cnt[i]) {
+                if(already) {
+                    answer = '?';
+                    break;
+                } else {
+                    answer = (char)(i + 'A');
+                    already = true;
+                }
             }
         }
-        System.out.println(ans);
+        System.out.println(answer);
         sc.close();
     }
 }
