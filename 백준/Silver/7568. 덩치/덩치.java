d3d9226272c5f68ecaf2a58095112fc86fbf4c6d
yunjichoi <cyjcode99@gmail.com>
2024-05-31 18:41:32 +0900

    [Silver V] Title: 덩치, Time: 144 ms, Memory: 15764 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.java
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -1,27 +1,32 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[][] arr = new int[n][n];
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < 2; j++) {
-                arr[i][j] = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = stoi(br.readLine());
+        int[][] arr = new int[N][2];
+        int[] answer = new int[N];
+        for(int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = stoi(st.nextToken());
+            arr[i][1] = stoi(st.nextToken());
+            answer[i] = 1;
+        }
+        for(int i = 0; i < N - 1; i++) {
+            for(int j = i + 1; j < N; j++) {
+                if(arr[i][0] > arr[j][0] && arr[i][1] > arr[j][1]) answer[j]++;
+                else if(arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) answer[i]++;
             }
         }
-        for(int i = 0; i < n; i++) {
-            int rank = 1;
-            for(int j = 0; j < n; j++) {
-                if(i == j) {
-                    continue;
-                }
-                if(arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
-                    rank++;
-                }
-            }
-            System.out.print(rank + " ");
+        for(int i = 0; i < N; i++) {
+            sb.append(answer[i] + " ");
         }
-        
+        System.out.println(sb.toString().trim());
+    }
+
+    static int stoi(String s) {
+        return Integer.parseInt(s);
     }
 }
