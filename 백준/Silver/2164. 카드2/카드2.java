2fdefa709d76b895c4bab835bc500fe613d7c55f
yunjichoi <cyjcode99@gmail.com>
2024-06-03 21:50:42 +0900

    [Silver IV] Title: 카드2, Time: 212 ms, Memory: 17688 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,21 +1,12 @@
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        Queue<Integer> queue = new LinkedList<>();
-        for(int i = 1; i <= n; i++) {
-            queue.add(i);
-        }
-        while(queue.size() != 1) {
-            queue.remove();
-            queue.add(queue.peek());
-            queue.remove();
-        }
-        System.out.println(queue.peek());
+        int N = sc.nextInt();
+        int i = 1;
+        while(i < N) i <<= 1;
+        System.out.println((N << 1) - i);
         sc.close();
     }
 }
