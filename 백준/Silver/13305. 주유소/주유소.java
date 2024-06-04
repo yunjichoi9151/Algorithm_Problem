3eaa6a07ca94694d0fb69d6627a608e6f988dcf8
yunjichoi <cyjcode99@gmail.com>
2024-06-04 21:50:04 +0900

    [Silver III] Title: 주유소, Time: 328 ms, Memory: 29468 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = stoi(br.readLine());
+        int[] cost = new int[N];
+        int[] dis = new int[N - 1];
+        int before = Integer.MAX_VALUE;
+        int sum = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N - 1; i++) {
+            dis[i] = stoi(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            cost[i] = stoi(st.nextToken());
+            if(before < cost[i]) {
+                cost[i] = before;
+            } else if(before > cost[i]) {
+                before = cost[i];
+            }
+            if(i != N - 1) sum += cost[i] * dis[i];
+        }
+        System.out.println(sum);
+    }
+
+    static int stoi(String s) {
+        return Integer.parseInt(s);
+    }
+}
