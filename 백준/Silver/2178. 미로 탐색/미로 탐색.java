c3e8dce61691d35d792f3ee93ec2aac3771fac92
yunjichoi <cyjcode99@gmail.com>
2024-06-12 01:56:10 +0900

    [Silver I] Title: 미로 탐색, Time: 136 ms, Memory: 14712 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -2,54 +2,47 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M;
-    static int[][] map;
-    static boolean[][] visited;
+    static class Node {
+        int x, y, cnt;
+        public Node(int x, int y, int cnt) {
+            this.x = x;
+            this.y = y;
+            this.cnt = cnt;
+        }
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        map = new int[N][M];
-        visited = new boolean[N][M];
+        int N = stoi(st.nextToken());
+        int M = stoi(st.nextToken());
+        char[][] map = new char[N][M];
+        boolean[][] visited = new boolean[N][M];
         for(int i = 0; i < N; i++) {
-            String s = br.readLine();
-            for(int j = 0; j < M; j++) {
-                map[i][j] = s.charAt(j) - '0';
-            }
+            map[i] = br.readLine().toCharArray();
         }
-        BFS(0, 0);
-        System.out.println(map[N - 1][M - 1]);
-    }
-
-    public static void BFS(int x, int y) {
-        Queue<Node> que = new LinkedList<Node>();
-        que.add(new Node(x, y));
-        visited[x][y] = true;
-        int[] dx = {0, 1, 0, -1};
-        int[] dy = {1, 0, -1, 0};
+        Queue<Node> que = new LinkedList<>();
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+        int answer = Integer.MAX_VALUE;
+        que.add(new Node(0, 0, 1));
+        visited[0][0] = true;
         while(!que.isEmpty()) {
             Node node = que.poll();
+            if(node.x == N - 1 && node.y == M - 1) {
+                answer = Math.min(answer, node.cnt);
+            }
             for(int i = 0; i < 4; i++) {
                 int nx = node.x + dx[i];
                 int ny = node.y + dy[i];
-                if(nx < 0 || ny < 0 || nx >= N || ny >= M
-                    || visited[nx][ny] || map[nx][ny] == 0) {
-                    continue;
-                }
-                que.add(new Node(nx, ny));
+                if(nx < 0 || ny < 0 || nx >= N || ny >= M || map[nx][ny] != '1' || visited[nx][ny]) continue;
+                que.add(new Node(nx, ny, node.cnt + 1));
                 visited[nx][ny] = true;
-                map[nx][ny] = map[node.x][node.y] + 1;
             }
         }
+        System.out.println(answer);
     }
-}
 
-class Node {
-    int x;
-    int y;
-    public Node(int x, int y) {
-        this.x = x;
-        this.y = y;
+    static int stoi(String S) {
+        return Integer.parseInt(S);
     }
 }
