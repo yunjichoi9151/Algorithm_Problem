c7fa3c5ffd165c9d76ffcbe748436eed320e1d50
yunjichoi <cyjcode99@gmail.com>
2024-06-15 01:45:38 +0900

    [Gold V] Title: 마법사 상어와 비바라기, Time: 232 ms, Memory: 31772 KB -BaekjoonHub

--- 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.java
+++ 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.java
@@ -2,12 +2,6 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M;
-    static int[][] map;
-    static boolean[][] visited;
-    static int[] dx = {0, -1, -1, -1, 0, 1, 1, 1};
-    static int[] dy = {-1, -1, 0, 1, 1, 1, 0, -1};
-    static Queue<Node> que = new LinkedList<Node>();
     static class Node {
         int x, y;
         public Node(int x, int y) {
@@ -18,27 +12,55 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        map = new int[N][N];
-        visited = new boolean[N][N];
+        int N = stoi(st.nextToken());
+        int M = stoi(st.nextToken());
+        int[][] map = new int[N][N];
+        boolean[][] visited;
+        int[] dx = {0, -1, -1, -1, 0, 1, 1, 1};
+        int[] dy = {-1, -1, 0, 1, 1, 1, 0, -1};
+        Queue<Node> que = new LinkedList<>();
         for(int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j < N; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+                map[i][j] = stoi(st.nextToken());
             }
         }
-        que.add(new Node(N - 1, 0));
-        que.add(new Node(N - 1, 1));
-        que.add(new Node(N - 2, 0));
-        que.add(new Node(N - 2, 1));
-        for(int i = 0; i < M; i++) {
+        for(int i = N - 2; i <= N - 1; i++)
+            for(int j = 0; j <= 1; j++)
+                que.add(new Node(i, j));
+        for(int k = 0; k < M; k++) {
+            visited = new boolean[N][N];
             st = new StringTokenizer(br.readLine());
-            int d = Integer.parseInt(st.nextToken()) - 1;
-            int s = Integer.parseInt(st.nextToken());
-            step12(d, s);
-            step34();
-            step5();
+            int D = stoi(st.nextToken()) - 1;
+            int S = stoi(st.nextToken());
+            int size = que.size();
+            for(int i = 0; i < size; i++) {
+                Node node = que.poll();
+                int nx = (N + node.x + dx[D] * (S % N)) % N;
+                int ny = (N + node.y + dy[D] * (S % N)) % N;
+                que.add(new Node(nx, ny));
+                visited[nx][ny] = true;
+                map[nx][ny]++;
+            }
+            while(!que.isEmpty()) {
+                Node node = que.poll();
+                int cnt = 0;
+                for(int i = 1; i <= 7; i += 2) {
+                    int nx = node.x + dx[i];
+                    int ny = node.y + dy[i];
+                    if(nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+                    if(map[nx][ny] != 0) cnt++;
+                }
+                map[node.x][node.y] += cnt;
+            }
+            for(int i = 0; i < N; i++) {
+                for(int j = 0; j < N; j++) {
+                    if(map[i][j] >= 2 && !visited[i][j]) {
+                        que.add(new Node(i, j));
+                        map[i][j] -= 2;
+                    }
+                }
+            }
         }
         int answer = 0;
         for(int i = 0; i < N; i++) {
@@ -48,36 +70,8 @@ public class Main {
         }
         System.out.println(answer);
     }
-    static void step12(int d, int s) {
-        for(Node node : que) {
-            node.x = (N + node.x + dx[d] * (s % N)) % N;
-            node.y = (N + node.y + dy[d] * (s % N)) % N;
-            map[node.x][node.y]++;
-        }
-    }
-    static void step34() {
-        while(!que.isEmpty()) {
-            Node node = que.poll();
-            int cnt = 0;
-            visited[node.x][node.y] = true;
-            for(int i = 1; i <= 7; i += 2) {
-                int nx = node.x + dx[i];
-                int ny = node.y + dy[i];
-                if(nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
-                if(map[nx][ny] > 0) cnt++;
-            }
-            map[node.x][node.y] += cnt;
-        }
-    }
-    static void step5() {
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < N; j++) {
-                if(map[i][j] >= 2 && !visited[i][j]) {
-                    que.add(new Node(i, j));
-                    map[i][j] -= 2;
-                }
-            }
-        }
-        visited = new boolean[N][N];
+
+    static int stoi(String S) {
+        return Integer.parseInt(S);
     }
 }
