6d03acda6b4a43df03569d0fe3c445ff90a76b6c
yunjichoi <cyjcode99@gmail.com>
2024-06-21 18:59:59 +0900

    [Gold V] Title: 로봇 청소기, Time: 136 ms, Memory: 14460 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -1,42 +1,77 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int[][] map;
-    static int[] dr = { -1, 0, 1, 0 };
-    static int[] dc = { 0, 1, 0, -1 };
-    static int ans;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(), m = sc.nextInt();
-        int r = sc.nextInt(), c = sc.nextInt(), d = sc.nextInt();
-        map = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++)
-                map[i][j] = sc.nextInt();
+    static class Node {
+        int x, y, d;
+        public Node(int x, int y, int d) {
+            this.x = x;
+            this.y = y;
+            this.d = d;
         }
-        solve(r, c, d);
-        System.out.println(ans);
-        sc.close();
     }
-
-    static void solve(int r, int c, int d) {
-        if(map[r][c] == 0) {
-            map[r][c] = -1;
-            ans++;
-        } else if(map[r][c] == 1) {
-            return;
-        }
-        int nd = d;
-        for (int i = 0; i < 4; i++) {
-            nd = (nd + 3) % 4;
-            int nr = r + dr[nd];
-            int nc = c + dc[nd];
-            if (map[nr][nc] == 0) {
-                solve(nr, nc, nd);
-                return;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = stoi(st.nextToken());
+        int M = stoi(st.nextToken());
+        int[][] map = new int[N][M];
+        int[] robot = new int[2];
+        st = new StringTokenizer(br.readLine());
+        robot[0] = stoi(st.nextToken());
+        robot[1] = stoi(st.nextToken());
+        int D = stoi(st.nextToken());
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < M; j++) {
+                map[i][j] = stoi(st.nextToken());
             }
         }
-        solve(r - dr[d], c - dc[d], d);
-        return;
+        Queue<Node> que = new LinkedList<>();
+        que.add(new Node(robot[0], robot[1], D));
+        int answer = 0;
+        while(!que.isEmpty()) {
+            Node node = que.poll();
+            if(map[node.x][node.y] == 0) {
+                answer++;
+                map[node.x][node.y] = 2;
+            }
+            boolean isClean = true;
+            for(int i = 0; i < 4; i++) {
+                int nx = node.x + dx[i];
+                int ny = node.y + dy[i];
+                if(map[nx][ny] == 0) {
+                    isClean = false;
+                    break;
+                }
+            }
+            if(isClean) {
+                int td = (node.d + 2) % 4;
+                int nx = node.x + dx[td];
+                int ny = node.y + dy[td];
+                if(nx < 0 || ny < 0 || nx >= N || ny >= M || map[nx][ny] == 1) {
+                    break;
+                } else {
+                    que.add(new Node(nx, ny, node.d));
+                    continue;
+                }
+            } else {
+                int td = (node.d + 3) % 4;
+                int nx = node.x + dx[td];
+                int ny = node.y + dy[td];
+                if(nx < 0 || ny < 0 || nx >= N || ny >= M || map[nx][ny] != 0) {
+                    que.add(new Node(node.x, node.y, td));
+                } else {
+                    que.add(new Node(nx, ny, td));
+                }
+            }
+        }
+        System.out.println(answer);
+    }
+
+    static int stoi(String S) {
+        return Integer.parseInt(S);
     }
 }
