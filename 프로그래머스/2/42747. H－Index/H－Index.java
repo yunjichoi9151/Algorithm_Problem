de114aaccf5853de654d8e738556913590ab6d25
yunjichoi <cyjcode99@gmail.com>
2024-07-07 23:39:33 +0900

    [level 2] Title: H-Index, Time: 0.37 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        Arrays.sort(citations);
+        for(int i = 0; i < citations.length; i++) {
+            int h = citations.length - i;
+            if(citations[i] >= h) {
+                return h;
+            }
+        }
+        return 0;
+    }
+}
