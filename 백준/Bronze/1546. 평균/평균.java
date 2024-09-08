ffd9780fdc9032eb0338e46b70ad4ee597374981
yunjichoi <cyjcode99@gmail.com>
2024-09-08 10:10:16 +0900

    [Bronze I] Title: 평균, Time: 104 ms, Memory: 14572 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -4,22 +4,16 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = stoi(br.readLine());
-        int[] arr = new int[N];
-        int max = 0;
+        int N = Integer.parseInt(br.readLine());
+        double[] arr = new double[N];
+        double max = 0;
         double sum = 0;
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 0; i < N; i++) {
-            arr[i] = stoi(st.nextToken());
+            arr[i] = Double.parseDouble(st.nextToken());
+            sum += (double) arr[i];
             max = Math.max(max, arr[i]);
         }
-        for(int i = 0; i < N; i++) {
-            sum += (double)arr[i] / max * 100;
-        }
-        System.out.println(sum / N);
-    }
-
-    static int stoi(String S) {
-        return Integer.parseInt(S);
+        System.out.println(sum * 100 / max / N);
     }
 }
