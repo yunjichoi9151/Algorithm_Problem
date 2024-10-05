5f366df38479f0080c8a2bd6eacbbadfe5906afb
yunjichoi <cyjcode99@gmail.com>
2024-10-05 23:56:05 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 116 ms, Memory: 15896 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,21 +1,19 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.next();
-        int[] arr = new int[26];
-        for(int i = 0; i < 26; i++) {
-            arr[i] = -1;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        char[] arr1 = br.readLine().toCharArray();
+        int[] arr2 = new int[26];
+        for(int i = 0; i < 26; i++) arr2[i] = -1;
+        for (int i = 0; i < arr1.length; i++) {
+            int n = arr1[i] - 'a';
+            if (arr2[n] == -1)
+                arr2[n] = i;
         }
-        for(int i = 0; i < s.length(); i++) {
-            if(arr[s.charAt(i) - 'a'] == -1) {
-                arr[s.charAt(i) - 'a'] = i;
-            }
-        }
-        for(int i = 0; i < 26; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        sc.close();
+        for (int i = 0; i < 26; i++)
+            sb.append(arr2[i] + " ");
+        System.out.println(sb.toString().trim());
     }
 }
