bb69621b69c85fac2920a8b2be74107a7cb72d4a
yunjichoi <cyjcode99@gmail.com>
2024-10-11 01:17:05 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 116 ms, Memory: 15856 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -1,14 +1,15 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println(1 - sc.nextInt());
-        System.out.println(1 - sc.nextInt());
-        System.out.println(2 - sc.nextInt());
-        System.out.println(2 - sc.nextInt());
-        System.out.println(2 - sc.nextInt());
-        System.out.println(8 - sc.nextInt());
-        sc.close();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int[] arr = { 1, 1, 2, 2, 2, 8 };
+        for (int i = 0; i < 6; i++) {
+            sb.append(arr[i] - Integer.parseInt(st.nextToken()) + " ");
+        }
+        System.out.println(sb.toString().trim());
     }
 }
