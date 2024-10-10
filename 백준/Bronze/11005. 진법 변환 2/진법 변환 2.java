407d098a4be5e54b037b4b61f41d9564fbd261a7
yunjichoi <cyjcode99@gmail.com>
2024-10-11 02:26:35 +0900

    [Bronze I] Title: 진법 변환 2, Time: 100 ms, Memory: 14384 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int S = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        System.out.println(Integer.toString(S, N).toUpperCase());
+    }
+}
