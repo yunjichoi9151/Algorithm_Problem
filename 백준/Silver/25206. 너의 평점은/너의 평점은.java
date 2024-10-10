d94aa8c1295a060dc22b2a6f2e1d92fb31e2ad1b
yunjichoi <cyjcode99@gmail.com>
2024-10-11 01:54:19 +0900

    [Silver V] Title: 너의 평점은, Time: 100 ms, Memory: 14436 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.java
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.java
@@ -1,39 +1,41 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Double sum1 = 0.0;
-        Double sum2 = 0.0;
-        for(int i = 0; i < 20; i++) {
-            String s = sc.next();
-            Double n1 = sc.nextDouble();
-            String g = sc.next();
-            if(g.equals("P")) {
-                continue;
-            } else {
-                sum1 += n1;
-                if(g.equals("A+")) {
-                    sum2 += n1 * 4.5;
-                } else if(g.equals("A0")) {
-                    sum2 += n1 * 4.0;
-                } else if(g.equals("B+")) {
-                    sum2 += n1 * 3.5;
-                } else if(g.equals("B0")) {
-                    sum2 += n1 * 3.0;
-                } else if(g.equals("C+")) {
-                    sum2 += n1 * 2.5;
-                } else if(g.equals("C0")) {
-                    sum2 += n1 * 2.0;
-                } else if(g.equals("D+")) {
-                    sum2 += n1 * 1.5;
-                } else if(g.equals("D0")) {
-                    sum2 += n1 * 1.0;
-                } else if(g.equals("F")) {
-                    sum2 += n1 * 0;
-                }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        double all = 0;
+        double sum = 0;
+        for (int i = 0; i < 20; i++) {
+            st = new StringTokenizer(br.readLine());
+            String del = st.nextToken();
+            double num = Double.parseDouble(st.nextToken());
+            double score = ret(st.nextToken());
+            if (score != -1) {
+                all += num;
+                sum += num * score;
             }
         }
-        System.out.println(sum2 / sum1);
+        System.out.println(sum != 0 ? sum / all : 0);
+    }
+
+    static double ret(String S) {
+        double n = 0;
+        if (S.charAt(0) == 'A')
+            n += 4;
+        else if (S.charAt(0) == 'B')
+            n += 3;
+        else if (S.charAt(0) == 'C')
+            n += 2;
+        else if (S.charAt(0) == 'D')
+            n += 1;
+        else if(S.charAt(0) == 'F')
+            return 0;
+        else
+            return -1;
+        if (S.charAt(1) == '+')
+            n += 0.5;
+        return n;
     }
 }
