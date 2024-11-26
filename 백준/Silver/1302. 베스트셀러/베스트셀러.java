9d44fc7a06a0c7692f762104673350487e10647e
yunjichoi <cyjcode99@gmail.com>
2024-11-26 12:43:51 +0900

    [Silver IV] Title: 베스트셀러, Time: 108 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Silver/1302. 베스트셀러/베스트셀러.java
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.java
@@ -1,26 +1,23 @@
-import java.util.Scanner;
-import java.util.TreeMap;
-import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		String ans = "";
-		int max = 0;
-		TreeMap<String, Integer> map = new TreeMap<>();
-		for(int i = 0; i < n; i++) {
-			String s = sc.next();
-			map.put(s, map.getOrDefault(s, 0) + 1);
-		}
-		ArrayList<String> keySet = new ArrayList<>(map.keySet());
-		for(String key : keySet) {
-			if(map.get(key) > max) {
-				ans = key;
-				max = map.get(key);
-			}
-		}
-		System.out.println(ans);
-		sc.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        HashMap<String, Integer> map = new HashMap<>();
+        for(int i = 0; i < N; i++) {
+            String S = br.readLine();
+            map.put(S, map.getOrDefault(S, 0) + 1);
+        }
+        List<String> keySet = new ArrayList<>(map.keySet());
+        Collections.sort(keySet, (o1, o2) -> {
+            int comp = map.get(o2).compareTo(map.get(o1));
+            if (comp == 0) {
+                return o1.compareTo(o2);
+            }
+            return comp;
+        });
+        System.out.println(keySet.get(0));
+    }
 }
