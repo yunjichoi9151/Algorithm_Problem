303e80ad5c7b73f63b29d29c9f85cec8c186ce80
yunjichoi <cyjcode99@gmail.com>
2024-12-25 18:43:58 +0900

    [level 2] Title: 최솟값 만들기, Time: 2.40 ms, Memory: 54.3 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -2,11 +2,11 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] A, int[] B) {
-        int answer = 0;
         Arrays.sort(A);
         Arrays.sort(B);
+        int answer = 0;
         for(int i = 0; i < A.length; i++) {
-            answer += A[i] * B[B.length - i - 1];
+            answer += A[i] * B[A.length - 1 - i];
         }
         return answer;
     }
