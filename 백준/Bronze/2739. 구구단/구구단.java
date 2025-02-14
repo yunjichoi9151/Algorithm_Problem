fe34a48766308ee48ed4ca722f56c2f6d5f11174
yunjichoi <cyjcode99@gmail.com>
2025-02-14 16:55:47 +0900

    [Bronze V] Title: 구구단, Time: 132 ms, Memory: 16300 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,12 +1,13 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
         for(int i = 1; i <= 9; i++) {
-            System.out.println(n + " * " + i + " = " + (n * i));
+            sb.append(N + " * " + i + " = " + (N * i) + "\n");
         }
-        sc.close();
+        System.out.println(sb.toString().trim());
     }
 }
