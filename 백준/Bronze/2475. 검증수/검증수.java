2ef34d3c3f9251b8d937e48b7cb248e86d728272
yunjichoi <cyjcode99@gmail.com>
2025-02-14 16:52:10 +0900

    [Bronze V] Title: 검증수, Time: 108 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.java
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -1,13 +1,14 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int sum = 0;
         for(int i = 0; i < 5; i++) {
-            sum += Math.pow(sc.nextInt(), 2);
+            sum += Math.pow(Integer.parseInt(st.nextToken()), 2);
         }
         System.out.println(sum % 10);
-        sc.close();
     }
 }
