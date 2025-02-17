0526e2cd441854a395002000d51aa36021b69abb
yunjichoi <cyjcode99@gmail.com>
2025-02-17 12:47:55 +0900

    [Bronze V] Title: 시험 성적, Time: 108 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,20 +1,13 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int score = sc.nextInt();
-        if(score >= 90) {
-            System.out.println("A");
-        } else if(score >= 80) {
-            System.out.println("B");
-        } else if(score >= 70) {
-            System.out.println("C");
-        } else if(score >= 60) {
-            System.out.println("D");
-        } else {
-            System.out.println("F");
-        }
-        sc.close();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int score = Integer.parseInt(br.readLine());
+        if(score >= 90 && score <= 100) System.out.println('A');
+        else if(score >= 80 && score <= 89) System.out.println('B');
+        else if(score >= 70 && score <= 79) System.out.println('C');
+        else if(score >= 60 && score <= 69) System.out.println('D');
+        else System.out.println('F');
     }
 }
