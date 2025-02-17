7c4c8a22613d9908cc9ca9c690e070c60b85c61b
yunjichoi <cyjcode99@gmail.com>
2025-02-17 15:50:11 +0900

    [Bronze V] Title: 사칙연산, Time: 148 ms, Memory: 16572 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.java
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -1,15 +1,12 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        System.out.println(a + b);
-        System.out.println(a - b);
-        System.out.println(a * b);
-        System.out.println(a / b);
-        System.out.println(a % b);
-        sc.close();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        System.out.println((A + B) + "\n" + (A - B) + "\n" + (A * B) + "\n" + (A / B) + "\n" + (A % B));
     }
 }
