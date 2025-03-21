3bfeab3a216d0a935749ddd3353d8b666eb08ae2
yunjichoi <cyjcode99@gmail.com>
2025-03-22 06:43:36 +0900

    [Silver I] Title: 미로 탐색, Time: 116 ms, Memory: 14660 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -15,27 +15,29 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = stoi(st.nextToken());
         int M = stoi(st.nextToken());
-        char[][] map = new char[N][M];
+        int[][] map = new int[N][M];
         boolean[][] visited = new boolean[N][M];
-        for(int i = 0; i < N; i++) {
-            map[i] = br.readLine().toCharArray();
-        }
-        Queue<Node> que = new LinkedList<>();
         int[] dx = {-1, 1, 0, 0};
         int[] dy = {0, 0, -1, 1};
-        int answer = Integer.MAX_VALUE;
+        for(int i = 0; i < N; i++) {
+            String S = br.readLine();
+            for(int j = 0; j < M; j++) {
+                map[i][j] = S.charAt(j) - '0';
+            }
+        }
+        Queue<Node> que = new LinkedList<>();
         que.add(new Node(0, 0, 1));
         visited[0][0] = true;
         while(!que.isEmpty()) {
             Node node = que.poll();
             if(node.x == N - 1 && node.y == M - 1) {
                 System.out.println(node.cnt);
-                break;
+                return;
             }
             for(int i = 0; i < 4; i++) {
                 int nx = node.x + dx[i];
                 int ny = node.y + dy[i];
-                if(nx < 0 || ny < 0 || nx >= N || ny >= M || map[nx][ny] != '1' || visited[nx][ny]) continue;
+                if(nx < 0 || ny < 0 || nx >= N || ny >= M || visited[nx][ny] || map[nx][ny] == 0) continue;
                 que.add(new Node(nx, ny, node.cnt + 1));
                 visited[nx][ny] = true;
             }
