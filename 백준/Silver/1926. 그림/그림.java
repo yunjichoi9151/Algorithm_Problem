4065cb4d9518c39b4481d88911f7886bb3cf502f
yunjichoi <cyjcode99@gmail.com>
2025-03-22 06:33:32 +0900

    [Silver I] Title: 그림, Time: 544 ms, Memory: 52416 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -2,67 +2,57 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n, m, cnt, ans;
-    static boolean[][] visited;
-    static int[][] map;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
+    static class Node {
+        int x, y;
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        map = new int[n][m];
-        visited = new boolean[n][m];
-        for(int i = 0; i < n; i++) {
+        int N = stoi(st.nextToken());
+        int M = stoi(st.nextToken());
+        int[][] map = new int[N][M];
+        boolean[][] visited = new boolean[N][M];
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+        int cnt = 0;
+        int max = 0;
+        for(int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < m; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+            for(int j = 0; j < M; j++) {
+                map[i][j] = stoi(st.nextToken());
             }
         }
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < M; j++) {
                 if(map[i][j] == 1 && !visited[i][j]) {
-                    BFS(i, j);
-                }
-            }
-        }
-        System.out.println(cnt);
-        System.out.println(ans);
-    }
-
-    public static void BFS(int x, int y) {
-        Queue<Node> que = new LinkedList<Node>();
-        que.add(new Node(x, y));
-        visited[x][y] = true;
-        int size = 1;
-        while(!que.isEmpty()) {
-            Node node = que.poll();
-            for(int i = 0; i < 4; i++) {
-                for(int j = 0; j < 4; j++) {
-                    int nx = node.x + dx[i];
-                    int ny = node.y + dy[i];
-                    if(nx < 0 || ny < 0 || nx >= n || ny >= m
-                        || visited[nx][ny] || map[nx][ny] == 0) {
-                        continue;
+                    int tmp = 0;
+                    Queue<Node> que = new LinkedList<>();
+                    que.add(new Node(i, j));
+                    visited[i][j] = true;
+                    while(!que.isEmpty()) {
+                        Node node = que.poll();
+                        tmp++;
+                        for(int k = 0; k < 4; k++) {
+                            int nx = node.x + dx[k];
+                            int ny = node.y + dy[k];
+                            if(nx < 0 || ny < 0 || nx >= N || ny >= M || visited[nx][ny] || map[nx][ny] == 0) continue;
+                            que.add(new Node(nx, ny));
+                            visited[nx][ny] = true;
+                        }
                     }
-                    que.add(new Node(nx, ny));
-                    visited[nx][ny] = true;
-                    size++;
+                    if(tmp > max) max = tmp;
+                    cnt++;
                 }
             }
         }
-        cnt++;
-        if(ans < size) ans = size;
-    }
-}
-
-class Node {
-    int x;
-    int y;
-    public Node(int x, int y) {
-        this.x = x;
-        this.y = y;
+        System.out.println(cnt + "\n" + max);
     }
 
+    static int stoi(String S) {
+        return Integer.parseInt(S);
+    }
 }
