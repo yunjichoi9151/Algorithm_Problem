b2ea3ff3930dc144df692393f6cde45ae97a6451
yunjichoi <cyjcode99@gmail.com>
2025-03-22 09:38:34 +0900

    [Silver I] Title: 안전 영역, Time: 404 ms, Memory: 60500 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,62 +1,59 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int N;
-    static int[][] map;
-    static boolean[][] visited;
-    static class Node {
-        int x;
-        int y;
-        public Node(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
+  static class Node {
+    int x, y;
+    public Node(int x, int y) {
+      this.x = x;
+      this.y = y;
     }
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        HashSet<Integer> set = new HashSet<Integer>();
-        N = Integer.parseInt(br.readLine());
-        map = new int[N][N];
-        for(int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < N; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-                set.add(map[i][j]);
-            }
-        }
-        int max = 1;
-        for(int num : set) {
-            visited = new boolean[N][N];
-            int cnt = 0;
-            for(int i = 0; i < N; i++) {
-                for(int j = 0; j < N; j++) {
-                    if(map[i][j] > num && !visited[i][j]) {
-                        BFS(i, j, num);
-                        cnt++;
-                    }
-                }
-            }
-            if(max < cnt) max = cnt;
-        }
-        System.out.println(max);
+  }
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = null;
+    int N = stoi(br.readLine());
+    int[][] map = new int[N][N];
+    int[] dx = {-1, 1, 0, 0};
+    int[] dy = {0, 0, -1, 1};
+    int max = 0;
+    int ans = 0;
+    for(int i = 0; i < N; i++) {
+      st = new StringTokenizer(br.readLine());
+      for(int j = 0; j < N; j++) {
+        map[i][j] = stoi(st.nextToken());
+        if(map[i][j] > max) max = map[i][j];
+      }
     }
-
-    static void BFS(int x, int y, int num) {
-        Queue<Node> que = new LinkedList<Node>();
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-        que.add(new Node(x, y));
-        visited[x][y] = true;
-        while(!que.isEmpty()) {
-            Node node = que.poll();
-            for(int i = 0; i < 4; i++) {
-                int nx = node.x + dx[i];
-                int ny = node.y + dy[i];
-                if(nx < 0 || ny < 0 || nx >= N || ny >= N || visited[nx][ny] || map[nx][ny] <= num) continue;
+    boolean[][][] visited = new boolean[N][N][max + 1];
+    for(int w = 0; w <= max; w++) {
+      int cnt = 0;
+      for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N; j++) {
+          if(map[i][j] > w && !visited[i][j][w]) {
+            Queue<Node> que = new LinkedList<>();
+            que.add(new Node(i, j));
+            visited[i][j][w] = true;
+            while(!que.isEmpty()) {
+              Node node = que.poll();
+              for(int k = 0; k < 4; k++) {
+                int nx = node.x + dx[k];
+                int ny = node.y + dy[k];
+                if(nx < 0 || ny < 0 || nx >= N || ny >= N || map[nx][ny] <= w || visited[nx][ny][w]) continue;
                 que.add(new Node(nx, ny));
-                visited[nx][ny] = true;
+                visited[nx][ny][w] = true;
+              }
             }
+            cnt++;
+          }
         }
+      }
+      if(cnt > ans) ans = cnt;
     }
+    System.out.println(ans);
+  }
+
+  static int stoi(String S) {
+    return Integer.parseInt(S);
+  }
 }
