37b5f33a3d84af13b44598c8295abe210bcdae80
yunjichoi <cyjcode99@gmail.com>
2025-06-05 14:01:12 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 132 ms, Memory: 15028 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -1,19 +1,18 @@
+import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
         for(int i = 0; i < N; i++) {
-            for(int j = 0; j < N - i - 1; j++) {
-                sb.append(' ');
+            for(int j = 0; j < N; j++) {
+                if(j < N - i - 1) sb.append(" ");
+                else sb.append("*");
             }
-            for(int k = 0; k <= i; k++) {
-                sb.append('*');
-            }
-            sb.append('\n');
+            sb.append("\n");
         }
-        System.out.print(sb.toString());
+        System.out.println(sb.toString().replaceAll("\\s+$", ""));
     }
 }
