01838359d46d4dbce07d557e887ddc82f1434d0e
yunjichoi <cyjcode99@gmail.com>
2025-06-05 10:26:50 +0900

    [Bronze V] Title: 윤년, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -1,13 +1,11 @@
+import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        if(N % 400 == 0 || (N % 4 == 0 && N % 100 != 0)) {
-            System.out.println(1);
-        } else {
-            System.out.println(0);
-        }
+        if(N % 400 == 0 || (N % 4 == 0 && N % 100 != 0)) System.out.println(1);
+        else System.out.println(0);
     }
 }
