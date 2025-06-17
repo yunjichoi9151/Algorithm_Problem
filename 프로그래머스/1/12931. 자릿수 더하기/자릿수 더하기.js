ee377a1c548ecf085f0f85edc21425bab491eac5
yunjichoi <cyjcode99@gmail.com>
2025-06-17 10:59:40 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -3,7 +3,8 @@ function solution(n)
     var answer = 0;
     while(n > 0) {
         answer += n % 10;
-        n = parseInt(n / 10);
+        n = Math.floor(n / 10);
     }
+
     return answer;
 }
