c343d4bb1a51a283f0c09789c5c6f0a31d952424
yunjichoi <cyjcode99@gmail.com>
2025-06-19 17:23:48 +0900

    [level 1] Title: 예산, Time: 0.32 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -0,0 +1,11 @@
+function solution(d, budget) {
+    var answer = 0;
+    d = d.sort((a, b) => a - b);
+    for(let i = 0; i < d.length; i++) {
+        if(d[i] <= budget) {
+            answer++;
+            budget -= d[i];
+        } else break;
+    }
+    return answer;
+}
