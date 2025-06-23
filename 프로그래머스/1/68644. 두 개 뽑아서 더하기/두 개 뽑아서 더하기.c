f17f62aae677a79b8e8c57e9a8e65093d3e6ec64
yunjichoi <cyjcode99@gmail.com>
2025-06-23 10:30:45 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.43 ms, Memory: 33.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// numbers_len은 배열 numbers의 길이입니다.
+int* solution(int numbers[], size_t numbers_len) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int* answer = (int*)malloc(sizeof(int));
+    return answer;
+}
