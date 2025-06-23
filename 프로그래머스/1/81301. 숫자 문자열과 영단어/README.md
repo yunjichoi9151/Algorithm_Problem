791a18bdd89636e49fe7780efd00d571499537d1
yunjichoi <cyjcode99@gmail.com>
2025-06-23 13:35:46 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.21 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/README.md
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 숫자 문자열과 영단어 - 81301 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81301?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81301) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.06 ms
+메모리: 33.3 MB, 시간: 0.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 0일 17:47:28
+2025년 06월 23일 13:35:44
 
 ### 문제 설명
 
--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,7 +1,31 @@
 function solution(s) {
-  const arr = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-  arr.forEach((word, idx) => {
-    s = s.split(word).join(idx);
-  });
-  return parseInt(s, 10);
+    var answer = '';
+    const map = new Map([
+      ["zero", '0'],
+      ["one", '1'],
+      ["two", '2'],
+      ["three", '3'],
+      ["four", '4'],
+      ["five", '5'],
+      ["six", '6'],
+      ["seven", '7'],
+      ["eight", '8'],
+      ["nine", '9'],
+    ]);
+    let start = 0;
+    while(start < s.length) {
+        if(s[start] >= '0' && s[start] <= '9') answer += s[start++]
+        else {
+            var str = '';
+            while(start < s.length) {
+                str += s[start];
+                start++;
+                if(map.has(str)) {
+                    answer += map.get(str);
+                    break;
+                }
+            }
+        }
+    }
+    return Number(answer);
 }
