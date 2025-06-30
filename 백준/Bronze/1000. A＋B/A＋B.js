a3209310812bf503a6f18ef61f648ab875c3fb1a
yunjichoi <cyjcode99@gmail.com>
2025-06-30 11:17:40 +0900

    [Bronze V] Title: A+B, Time: 96 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.js
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -1,3 +1,2 @@
-const fs = require("fs");
-const input = fs.readFileSync("/dev/stdin").toString().split(' ').map(Number);
+const input = require('fs').readFileSync('/dev/stdin').toString().split(' ').map(Number);
 console.log(input[0] + input[1]);
