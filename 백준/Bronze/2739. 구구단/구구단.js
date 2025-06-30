5421054896aedc35763a77e89db75a6159cf5556
yunjichoi <cyjcode99@gmail.com>
2025-06-30 17:38:37 +0900

    [Bronze V] Title: 구구단, Time: 88 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.js
+++ 백준/Bronze/2739. 구구단/구구단.js
@@ -1,5 +1,6 @@
-var fs = require('fs');
-var input = fs.readFileSync('/dev/stdin').toString().trim();
-for(var i = 1; i <= 9; i++) {
-  console.log(input + " * " + i + " = " + input * i);
+const input = Number(require('fs').readFileSync('/dev/stdin').toString());
+const answer = [];
+for(let i = 1; i <= 9; i++) {
+  answer[i - 1] = input + ' * ' + i + " = " + (input * i);
 }
+console.log(answer.join('\n'));
