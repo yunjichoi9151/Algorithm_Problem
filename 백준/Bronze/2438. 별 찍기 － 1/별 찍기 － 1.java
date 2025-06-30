63667ff4c6c51200ff689bbf337e125869c15cc6
yunjichoi <cyjcode99@gmail.com>
2025-06-30 17:09:21 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,16 +1,15 @@
-import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
         for(int i = 0; i < N; i++) {
             for(int j = 0; j <= i; j++) {
-                sb.append("*");
+                sb.append('*');
             }
-            sb.append("\n");
+            sb.append('\n');
         }
         System.out.println(sb.toString().trim());
     }
