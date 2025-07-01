9975aceddf85b772ca822780c4681c316b3a9d77
yunjichoi <cyjcode99@gmail.com>
2025-07-01 09:25:02 +0900

    [Bronze V] Title: 고양이, Time: 100 ms, Memory: 13896 KB -BaekjoonHub

--- 백준/Bronze/10171. 고양이/고양이.java
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -1,10 +1,5 @@
-import java.io.IOException;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-        System.out.println("\\    /\\");
-        System.out.println(" )  ( ')");
-        System.out.println("(  /  )");
-        System.out.println(" \\(__)|");
+    public static void main(String[] args) {
+        System.out.println("\\    /\\\n )  ( \')\n(  /  )\n \\(__)|");
     }
 }
