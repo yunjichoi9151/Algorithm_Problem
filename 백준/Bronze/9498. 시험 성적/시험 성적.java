a6183c86ffc9709f072a75ef519c6f4a3b00969e
yunjichoi <cyjcode99@gmail.com>
2025-07-01 09:18:35 +0900

    [Bronze V] Title: 시험 성적, Time: 104 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,14 +1,13 @@
-import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int score = Integer.parseInt(br.readLine());
-        if(score >= 90) System.out.println("A");
-        else if(score >= 80) System.out.println("B");
-        else if(score >= 70) System.out.println("C");
-        else if(score >= 60) System.out.println("D");
-        else System.out.println("F");
+        int N = Integer.parseInt(br.readLine());
+        if(N >= 90) System.out.println('A');
+        else if(N >= 80) System.out.println('B');
+        else if(N >= 70) System.out.println('C');
+        else if(N >= 60) System.out.println('D');
+        else System.out.println('F');
     }
 }
