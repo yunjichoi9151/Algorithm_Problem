478f2d2ae6693fac3e1d9c7cfc97c362f7bd5fe7
yunjichoi <cyjcode99@gmail.com>
2025-07-02 09:58:39 +0900

    [Bronze V] Title: A+B - 3, Time: 92 ms, Memory: 9376 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.js
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.js
@@ -1,8 +1,2 @@
-var fs = require('fs');
-var input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
-var t = parseInt(input[0]);
-for (var i = 1; i <= t; i++) {
-  console.log(
-    parseInt(input[i].split(' ')[0]) + parseInt(input[i].split(' ')[1])
-  );
-}
+const [T, ...cases] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+console.log(cases.map(line => line.split(' ').map(Number)).map(([a, b]) => a + b).join('\n'));
