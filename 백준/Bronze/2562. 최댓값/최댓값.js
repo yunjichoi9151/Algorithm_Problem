1a0a423191fef0a4f3f69b1e03068242995e1309
yunjichoi <cyjcode99@gmail.com>
2025-07-02 17:54:35 +0900

    [Bronze III] Title: 최댓값, Time: 92 ms, Memory: 9320 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.js
+++ 백준/Bronze/2562. 최댓값/최댓값.js
@@ -1,3 +1,9 @@
-const arr = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(Number);
-const max = Math.max(...arr);
-console.log(max + '\n' + (arr.indexOf(max) + 1)); 
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(Number);
+let [max, idx] = [-1, -1];
+for(let i = 0; i < 9; i++) {
+  if(input[i] > max) {
+    max = input[i];
+    idx = i + 1;
+  }
+}
+console.log(max + '\n' + idx);
