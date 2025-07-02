60cc46ffe27a0b984b803b33ce02419f0a003a8b
yunjichoi <cyjcode99@gmail.com>
2025-07-02 11:59:03 +0900

    [Bronze V] Title: 새싹, Time: 96 ms, Memory: 13860 KB -BaekjoonHub

--- 백준/Bronze/25083. 새싹/새싹.java
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -1,10 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("         ,r'\"7");
-        System.out.println("r`-_   ,'  ,/");
-        System.out.println(" \\. \". L_r'");
-        System.out.println("   `~\\/");
-        System.out.println("      |");
-        System.out.println("      |");
+        System.out.println("         ,r'\"7\nr`-_   ,'  ,/\n \\. \". L_r'\n   `~\\/\n      |\n      |");
     }
 }
