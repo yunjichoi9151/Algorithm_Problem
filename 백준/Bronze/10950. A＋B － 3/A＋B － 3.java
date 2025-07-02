21568e7cd9a02f9ed8f445ea58596bc9050c0096
yunjichoi <cyjcode99@gmail.com>
2025-07-02 09:44:28 +0900

    [Bronze V] Title: A+B - 3, Time: 120 ms, Memory: 14588 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -4,12 +4,12 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
         int T = stoi(br.readLine());
         for(int i = 0; i < T; i++) {
             st = new StringTokenizer(br.readLine());
-            sb.append((stoi(st.nextToken()) + stoi(st.nextToken())) + "\n");
+            sb.append(stoi(st.nextToken()) + stoi(st.nextToken()) + "\n");
         }
         System.out.println(sb.toString().trim());
     }
