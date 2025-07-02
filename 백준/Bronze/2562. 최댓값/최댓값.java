dc4a826a0ac436573398f0684dcdf3589a5e63db
yunjichoi <cyjcode99@gmail.com>
2025-07-02 17:47:07 +0900

    [Bronze III] Title: 최댓값, Time: 128 ms, Memory: 16104 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -3,15 +3,15 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int max = 0;
-        int max_idx = -1;
-        for(int i = 1; i <= 9; i++) {
-            int n = Integer.parseInt(br.readLine());
-            if(n > max) {
-                max = n;
-                max_idx = i;
+        int[] arr = new int[9];
+        int[] answer = {-1, -1};
+        for(int i = 0; i < 9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            if(answer[0] < arr[i]) {
+                answer[0] = arr[i];
+                answer[1] = i + 1;
             }
         }
-        System.out.println(max + "\n" + max_idx);
+        System.out.println(answer[0] + "\n" + answer[1]);
     }
 }
