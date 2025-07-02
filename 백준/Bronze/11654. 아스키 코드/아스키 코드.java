362a4026a94f37bc739babd48dff0b0715940cc2
yunjichoi <cyjcode99@gmail.com>
2025-07-02 11:55:22 +0900

    [Bronze V] Title: 아스키 코드, Time: 108 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Bronze/11654. 아스키 코드/아스키 코드.java
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -3,6 +3,6 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println((int)(br.readLine().charAt(0)));
+        System.out.println((int)br.readLine().charAt(0));
     }
 }
