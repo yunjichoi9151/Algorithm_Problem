75e18fe3a56b4bf992333ecc9f9480216e627b01
yunjichoi <cyjcode99@gmail.com>
2025-07-03 11:31:29 +0900

    [Bronze III] Title: ACM 호텔, Time: 120 ms, Memory: 15888 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -5,17 +5,20 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st = null;
-        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        int T = stoi(br.readLine());
         for(int i = 0; i < T; i++) {
             st = new StringTokenizer(br.readLine());
-            int H = Integer.parseInt(st.nextToken());
-            int W = Integer.parseInt(st.nextToken());
-            int N = Integer.parseInt(st.nextToken());
-            int x1 = N % H == 0 ? H : N - H * (N / H);
-            int x2 = N % H == 0 ? N / H :  N / H + 1;
-            sb.append(x1 * 100 + x2 + "\n");
+            int H = stoi(st.nextToken());
+            int W = stoi(st.nextToken());
+            int N = stoi(st.nextToken());
+            if(N % H == 0) sb.append(H * 100 + (N / H) + "\n");
+            else sb.append((N % H) * 100 + (N / H + 1) + "\n");
         }
         System.out.println(sb.toString().trim());
+    }
+
+    static int stoi(String S) {
+        return Integer.parseInt(S);
     }
 }
