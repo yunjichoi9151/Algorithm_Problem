0f7751c6ad3a433eff73a37e116c0afa7941ea2c
yunjichoi <cyjcode99@gmail.com>
2025-07-03 10:36:36 +0900

    [Bronze III] Title: 알람 시계, Time: 92 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.js
+++ 백준/Bronze/2884. 알람 시계/알람 시계.js
@@ -1,12 +1,5 @@
-var fs = require('fs');
-var input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
-var h = parseInt(input[0]);
-var m = parseInt(input[1]);
-var allTime = h * 60 + m;
-if (allTime >= 45) {
-  allTime -= 45;
-  console.log(Math.floor(allTime / 60) + ' ' + (allTime % 60));
-} else {
-  allTime += 24 * 60 - 45;
-  console.log(Math.floor(allTime / 60) + ' ' + (allTime % 60));
-}
+const [h, m] = require('fs').readFileSync('/dev/stdin').toString().split(' ').map(Number);
+let num = h * 60 + m;
+if(num < 45) num += 24 * 60;
+num -= 45;
+console.log(Math.floor(num / 60) + " " + num % 60);
