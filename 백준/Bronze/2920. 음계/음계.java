44b9bb29d91de30e820328110443da40da388522
yunjichoi <cyjcode99@gmail.com>
2025-07-07 11:18:43 +0900

    [Bronze II] Title: 음계, Time: 100 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.java
+++ 백준/Bronze/2920. 음계/음계.java
@@ -5,16 +5,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int before = Integer.parseInt(st.nextToken());
-        for(int i = 0; i < 7; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            if(num == before - 1 || num == before + 1) {
-                before = num;
-            } else {
+        int[] arr = new int[8];
+        for(int i = 0; i < 8; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        boolean isBigger = arr[0] < arr[1];
+        for(int i = 2; i < 8; i++) {
+            if((arr[i - 1] < arr[i] && isBigger) || (arr[i - 1] > arr[i] && !isBigger)) continue;
+            else {
                 System.out.println("mixed");
                 return;
             }
         }
-        System.out.println(before == 8 ? "ascending" : "descending");
+        System.out.println(isBigger ? "ascending" : "descending");
     }
 }
