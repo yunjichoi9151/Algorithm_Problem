b56782333e3d9942f77cc26ee7cb37d949022209
yunjichoi <cyjcode99@gmail.com>
2025-07-07 14:18:40 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 120 ms, Memory: 15876 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -4,17 +4,17 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        char[] arr = br.readLine().toCharArray();
-        int[] cnt = new int[26];
+        String S = br.readLine();
+        int[] arr = new int[26];
         for(int i = 0; i < 26; i++) {
-            cnt[i] = -1;
+            arr[i] = -1;
         }
-        for(int i = 0; i < arr.length; i++) {
-            int num = arr[i] - 'a';
-            if(cnt[num] == -1) cnt[num] = i;
+        for(int i = 0; i < S.length(); i++) {
+            int n = S.charAt(i) - 'a';
+            if(arr[n] == -1) arr[n] = i;
         }
         for(int i = 0; i < 26; i++) {
-            sb.append(cnt[i] + " ");
+            sb.append(arr[i] + " ");
         }
         System.out.println(sb.toString().trim());
     }
