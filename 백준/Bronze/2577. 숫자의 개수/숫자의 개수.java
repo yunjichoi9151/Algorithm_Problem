d8da79d413b218e249a5eaa77a2fc35e2338cc80
yunjichoi <cyjcode99@gmail.com>
2025-07-07 10:41:51 +0900

    [Bronze II] Title: 숫자의 개수, Time: 120 ms, Memory: 15800 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -7,14 +7,14 @@ public class Main {
         int A = Integer.parseInt(br.readLine());
         int B = Integer.parseInt(br.readLine());
         int C = Integer.parseInt(br.readLine());
-        int num = A * B * C;
-        int[] arr = new int[10];
-        while(num > 0) {
-            arr[num % 10]++;
-            num /= 10;
+        int N = A * B * C;
+        int[] cnt = new int[10];
+        while(N > 0) {
+            cnt[N % 10]++;
+            N /= 10;
         }
         for(int i = 0; i < 10; i++) {
-            sb.append(arr[i] + "\n");
+            sb.append(cnt[i] + "\n");
         }
         System.out.println(sb.toString().trim());
     }
