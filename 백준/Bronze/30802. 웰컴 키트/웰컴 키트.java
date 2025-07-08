bcc63d4abcb771a520aa4fe9b1871291190a9f1a
yunjichoi <cyjcode99@gmail.com>
2025-07-08 09:17:44 +0900

    [Bronze III] Title: 웰컴 키트, Time: 132 ms, Memory: 16096 KB -BaekjoonHub

--- 백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
@@ -4,23 +4,23 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        int N = stoi(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
         int[] arr = new int[6];
         int ans = 0;
         for(int i = 0; i < 6; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            arr[i] = stoi(st.nextToken());
         }
         st = new StringTokenizer(br.readLine());
-        int T = Integer.parseInt(st.nextToken());
-        int P = Integer.parseInt(st.nextToken());
+        int T = stoi(st.nextToken());
+        int P = stoi(st.nextToken());
         for(int i = 0; i < 6; i++) {
-            if(arr[i] % T == 0) {
-                ans += arr[i] / T;
-            } else {
-                ans += arr[i] / T + 1;
-            }
+            ans += arr[i] / T + (arr[i] % T == 0 ? 0 : 1);
         }
         System.out.println(ans + "\n" + (N / P) + " " + (N % P));
+    }
+
+    static int stoi(String S) {
+        return Integer.parseInt(S);
     }
 }
