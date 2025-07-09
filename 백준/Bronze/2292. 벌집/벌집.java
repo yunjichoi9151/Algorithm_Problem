6acd779cf3f1b7e405ab7e6a24274bc6f72d0924
yunjichoi <cyjcode99@gmail.com>
2025-07-09 14:32:43 +0900

    [Bronze II] Title: 벌집, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.java
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -4,18 +4,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        if(N == 1) System.out.println(1);
-        else {
-            int cnt = 1;
-            int sum = 1;
-            while(true) {
-                if(N <= sum + cnt * 6) {
-                    System.out.println(cnt + 1);
-                    break;
-                } else {
-                    sum += cnt++ * 6;
-                }
-            }
+        int cnt = 1;
+        int range = 1;
+        while(range < N) {
+            range += 6 * cnt;
+            cnt++;
         }
+        System.out.println(cnt);
     }
 }
