6d7cbbedfe1811521e3cef0938a1dd3e4441951a
yunjichoi <cyjcode99@gmail.com>
2025-07-09 17:16:53 +0900

    [Bronze II] Title: Hashing, Time: 100 ms, Memory: 14248 KB, Score: 50 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,22 +1,15 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int l = sc.nextInt();
-        String s = sc.next();
-        long[] arr = new long[l];
-        long sum = 0;
-        for(int i = 0; i < l; i++) {
-            arr[i] = s.charAt(i) - 'a' + 1;
-            long num = 1;
-            for(int j = 1; j <= i; j++) {
-                num *= 31;
-                num %= 1234567891;
-            }
-            sum = (sum + (arr[i] * num)) % 1234567891;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int L = Integer.parseInt(br.readLine());
+        char[] arr = br.readLine().toCharArray();
+        long ans = 0;
+        for(int i = 0; i < L; i++) {
+            int num = arr[i] - 'a' + 1;
+            ans += num * Math.pow(31, i) % 1234567891;
         }
-        System.out.println(sum);
-        sc.close();
+        System.out.println(ans);
     }
 }
