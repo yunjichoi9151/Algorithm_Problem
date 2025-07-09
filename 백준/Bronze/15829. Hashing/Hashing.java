266c3a96c202cbef24448543243185babdac980f
yunjichoi <cyjcode99@gmail.com>
2025-07-09 17:31:01 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14176 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -6,9 +6,12 @@ public class Main {
         int L = Integer.parseInt(br.readLine());
         char[] arr = br.readLine().toCharArray();
         long ans = 0;
+        long r = 1;
+        long mod = 1234567891;
         for(int i = 0; i < L; i++) {
             int num = arr[i] - 'a' + 1;
-            ans += num * Math.pow(31, i) % 1234567891;
+            ans = (ans + num * r) % mod;
+            r = (r * 31) % mod;
         }
         System.out.println(ans);
     }
