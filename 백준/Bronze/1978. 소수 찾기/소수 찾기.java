fe293a36b8ad4bab0ee457b0d6db099e49442e21
yunjichoi <cyjcode99@gmail.com>
2025-07-09 11:27:34 +0900

    [Bronze II] Title: 소수 찾기, Time: 104 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -6,17 +6,19 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int ans = 0;
-        for(int i = 0; i < N; i++) {
+        int cnt = 0;
+        for(int tc = 0; tc < N; tc++) {
             int num = Integer.parseInt(st.nextToken());
-            if(num != 1) ans++;
-            for(int j = 2; j <= num / j; j++) {
-                if(num % j == 0) {
-                    ans--;
+            if(num == 1) continue;
+            boolean isTrue = true;
+            for(int i = 2; i <= (num / i); i++) {
+                if(num % i == 0) {
+                    isTrue = false;
                     break;
                 }
             }
+            if(isTrue) cnt++;
         }
-        System.out.println(ans);
+        System.out.println(cnt);
     }
 }
