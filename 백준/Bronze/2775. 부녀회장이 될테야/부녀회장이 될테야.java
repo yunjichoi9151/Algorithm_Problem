5d8786e3fb0a2e1824465b584186f151b0eb9752
yunjichoi <cyjcode99@gmail.com>
2025-07-10 17:47:40 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 112 ms, Memory: 14436 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -5,15 +5,22 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
-        int[][] map = new int[15][14];
+        int[][] arr = new int[15][14];
         for(int i = 0; i < 15; i++) {
             for(int j = 0; j < 14; j++) {
-                if(i == 0 || j == 0) map[i][j] = j + 1;
-                else map[i][j] = map[i - 1][j] + map[i][j - 1];
+                if(j == 0) {
+                    arr[i][0] = 1;
+                } else if(i == 0) {
+                    arr[0][j] = j + 1;
+                } else {
+                    arr[i][j] = arr[i - 1][j] + arr[i][j - 1];
+                }
             }
         }
-        for(int i = 0; i < T; i++) {
-            sb.append(map[Integer.parseInt(br.readLine())][Integer.parseInt(br.readLine()) - 1] + "\n");
+        for(int tc = 0; tc < T; tc++) {
+            int K = Integer.parseInt(br.readLine());
+            int N = Integer.parseInt(br.readLine());
+            sb.append(arr[K][N - 1] + "\n");
         }
         System.out.println(sb.toString().trim());
     }
