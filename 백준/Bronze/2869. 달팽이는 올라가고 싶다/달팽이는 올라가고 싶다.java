53315bfc7ae4675a144bf7856b15d073957c3bbd
yunjichoi <cyjcode99@gmail.com>
2025-07-11 16:40:00 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 104 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -5,9 +5,9 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int A = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
-        int V = Integer.parseInt(st.nextToken());
-        System.out.println((V - B) % (A - B) == 0 ? (V - B) / (A - B) : (V - B) / (A - B) + 1);
+        double A = Double.parseDouble(st.nextToken());
+        double B = Double.parseDouble(st.nextToken());
+        double V = Double.parseDouble(st.nextToken());
+        System.out.println((int)Math.ceil((double)(V - A) / (A - B)) + 1);
     }
 }
