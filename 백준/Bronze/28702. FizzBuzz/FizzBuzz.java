d08bda271f8b9516569662e962855265116940f5
yunjichoi <cyjcode99@gmail.com>
2025-07-17 15:17:08 +0900

    [Bronze I] Title: FizzBuzz, Time: 104 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Bronze/28702. FizzBuzz/FizzBuzz.java
+++ 백준/Bronze/28702. FizzBuzz/FizzBuzz.java
@@ -3,16 +3,19 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] arr = {br.readLine(), br.readLine(), br.readLine()};
+        StringBuilder sb = new StringBuilder();
+        boolean isDone = false;
         for(int i = 0; i < 3; i++) {
-            if(!arr[i].contains("Fizz") && !arr[i].contains("Buzz")) {
-                int num = Integer.parseInt(arr[i]) + (3 - i);
-                if(num % 3 == 0 && num % 5 == 0) System.out.println("FizzBuzz");
-                else if(num % 3 == 0) System.out.println("Fizz");
-                else if(num % 5 == 0) System.out.println("Buzz");
-                else System.out.println(num);
-                break;
+            String S = br.readLine();
+            if(!isDone && !S.contains("Fizz") && !S.contains("Buzz")) {
+                isDone = true;
+                int num = Integer.parseInt(S) + (3 - i);
+                if(num % 15 == 0) sb.append("FizzBuzz");
+                else if(num % 5 == 0) sb.append("Buzz");
+                else if(num % 3 == 0) sb.append("Fizz");
+                else sb.append(num);
             }
         }
+        System.out.println(sb.toString().trim());
     }
 }
