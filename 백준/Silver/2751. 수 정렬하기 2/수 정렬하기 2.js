2c81d3c83ba71bdbe8f982df3c7cd8f80af75892
yunjichoi <cyjcode99@gmail.com>
2025-07-18 14:58:59 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1064 ms, Memory: 271664 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
@@ -1,12 +1,10 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
-
-const n = +input[0];
-const arr = new Array(n);
-
-for (let i = 0; i < n; i++) {
-  arr[i] = +input[i + 1];
+const [N, ...input] = require('fs').readFileSync('/dev/stdin').toString().split('\n').map(line => Number(line.trim()));
+const arr = new Array(2000001).fill(false);
+const answer = [];
+for(let i = 0; i < N; i++) {
+  arr[input[i] + 1000000] = true;
 }
-
-arr.sort((a, b) => a - b);
-
-process.stdout.write(arr.join('\n'));
+for(let i = 0; i <= 2000000; i++) {
+  if(arr[i]) answer.push(i - 1000000);
+}
+console.log(answer.join('\n'));
