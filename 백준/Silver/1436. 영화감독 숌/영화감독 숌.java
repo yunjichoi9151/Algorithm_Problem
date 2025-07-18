b1a5e1d5411700d8c6767ed411a947a053b780e8
yunjichoi <cyjcode99@gmail.com>
2025-07-18 11:06:14 +0900

    [Silver V] Title: 영화감독 숌, Time: 260 ms, Memory: 85568 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -1,18 +1,20 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int num = 666;
-        int cnt = 1;
-        while(cnt != N) {
-            num++;
-            if(String.valueOf(num).contains("666")) {
-                cnt++;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int ans = 666;
+        int count = 0;
+        while(true) {
+            if(String.valueOf(ans).contains("666")) {
+                count++;
+                if(count == N) {
+                    System.out.println(ans);
+                    break;
+                }
             }
+            ans++;
         }
-        System.out.println(num);
-        sc.close();
     }
 }
