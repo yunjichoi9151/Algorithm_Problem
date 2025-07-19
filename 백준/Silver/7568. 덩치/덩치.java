d96da17aac1d9d11b052e1ada077e9e451793b52
yunjichoi <cyjcode99@gmail.com>
2025-07-19 17:08:54 +0900

    [Silver V] Title: 덩치, Time: 128 ms, Memory: 15884 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.java
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -5,28 +5,21 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int N = stoi(br.readLine());
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
         int[][] arr = new int[N][2];
-        int[] answer = new int[N];
         for(int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            arr[i][0] = stoi(st.nextToken());
-            arr[i][1] = stoi(st.nextToken());
-            answer[i] = 1;
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
         }
-        for(int i = 0; i < N - 1; i++) {
-            for(int j = i + 1; j < N; j++) {
-                if(arr[i][0] > arr[j][0] && arr[i][1] > arr[j][1]) answer[j]++;
-                else if(arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) answer[i]++;
+        for(int i = 0; i < N; i++) {
+            int rank = 1;
+            for(int j = 0; j < N; j++) {
+                if(arr[j][0] > arr[i][0] && arr[j][1] > arr[i][1]) rank++;
             }
-        }
-        for(int i = 0; i < N; i++) {
-            sb.append(answer[i] + " ");
+            sb.append(rank + " ");
         }
         System.out.println(sb.toString().trim());
-    }
-
-    static int stoi(String s) {
-        return Integer.parseInt(s);
     }
 }
