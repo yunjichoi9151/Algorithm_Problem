2bc60ab7edc7ddb6ebddd2960df7143f274e3f09
yunjichoi <cyjcode99@gmail.com>
2025-07-19 17:53:09 +0900

    [Silver V] Title: 나이순 정렬, Time: 684 ms, Memory: 52580 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,37 +1,34 @@
-import java.util.Scanner;
-import java.util.Arrays;
-import java.util.Comparator;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        Person[] PersonArray = new Person[n];
-        for(int i = 0; i < n; i++)
-            PersonArray[i] = new Person(sc.nextInt(), sc.next());
-        Arrays.sort(PersonArray, new Comparator<Person>() {
-            @Override
-            public int compare(Person s1, Person s2) {
-                return s1.age - s2.age; 
-            }
-        });
-        StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < n; i++)
-			sb.append(PersonArray[i]);
-		System.out.println(sb);
-        sc.close();
-    }   
-
-    public static class Person {
+    static class User {
         int age;
         String name;
-        public Person(int age, String name) {
+        int idx;
+        public User(int age, String name, int idx) {
             this.age = age;
             this.name = name;
+            this.idx = idx;
         }
-        @Override
-		public String toString() {
-			return age + " " + name + "\n";
-		}
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        User[] list = new User[N];
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            list[i] = new User(Integer.parseInt(st.nextToken()), st.nextToken(), i);
+        }
+        Arrays.sort(list, (a, b) -> {
+            if(a.age == b.age) return a.idx - b.idx;
+            else return a.age - b.age;
+        });
+        for(int i = 0; i < N; i++) {
+            sb.append(list[i].age + " " + list[i].name + "\n");
+        }
+        System.out.println(sb.toString().trim());
     }
 }
