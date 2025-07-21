e2ffbc01b6ad514d1d32b06712f01aa9c4a2e964
yunjichoi <cyjcode99@gmail.com>
2025-07-21 13:03:08 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 108 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -1,47 +1,29 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(), m = sc.nextInt();
-        int min = 2501;
-        char[][] arr = new char[n][m];
-        char[][] arr1 = new char[8][8];
-        char[][] arr2 = new char[8][8];
-        for(int i = 0; i < n; i++) {
-            String s = sc.next();
-            for(int j = 0; j < m; j++) {
-                arr[i][j] = s.charAt(j);
-            }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        char[][] paint = new char[N][M];
+        for(int i = 0; i < N; i++) {
+            paint[i] = br.readLine().toCharArray();
         }
-        for(int i = 0; i < 8; i++) {
-            for(int j = 0; j < 8; j++) {
-                if((i + j) % 2 == 0) {
-                    arr1[i][j] = 'W';
-                    arr2[i][j] = 'B';
-                }
-                else {
-                    arr1[i][j] = 'B';
-                    arr2[i][j] = 'W';
-                }
-            }
-        }
-        for(int i = 0; i <= n - 8; i++) {
-            for(int j = 0; j <= m - 8; j++) {
-                int cnt1 = 0, cnt2 = 0;
-                for(int y = 0; y < 8; y++) {
-                    for(int x = 0; x < 8; x++) {
-                        if (!((arr[i + y][j + x] + "").equals(arr1[y][x] + ""))) {
-                            cnt1++;
-                        }
-                        if (!((arr[i + y][j + x] + "").equals(arr2[y][x] + ""))) {
-                            cnt2++;
-                        }
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i <= N - 8; i++) {
+            for(int j = 0; j <= M - 8; j++) {
+                int[] cnt = new int[2];
+                for(int x = i; x < i + 8; x++) {
+                    char now = x % 2 == 0 ? 'W' : 'B';
+                    for(int y = j; y < j + 8; y++) {
+                        if(paint[x][y] != now) cnt[0]++;
+                        else cnt[1]++;
+                        now = now == 'W' ? 'B' : 'W';
                     }
                 }
-                if(min > (cnt1 < cnt2 ? cnt1 : cnt2)) {
-                    min = (cnt1 < cnt2 ? cnt1 : cnt2);
-                }
+                min = Math.min(min, Math.min(cnt[0], cnt[1]));
             }
         }
         System.out.println(min);
