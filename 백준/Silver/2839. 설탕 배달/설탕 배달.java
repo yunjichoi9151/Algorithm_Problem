b5b5c7fe367f837938f93dc701952a175442d970
yunjichoi <cyjcode99@gmail.com>
2025-07-22 11:35:01 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -4,13 +4,11 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int num = N / 5;
-        while(num >= 0) {
-            if((N - num * 5) % 3 == 0) {
-                System.out.println(num + ((N - num * 5) / 3));
+        for(int i = N / 5; i >= 0; i--) {
+            if((N - i * 5) % 3 == 0) {
+                System.out.println(i + ((N - i * 5) / 3));
                 return;
             }
-            num--;
         }
         System.out.println(-1);
     }
