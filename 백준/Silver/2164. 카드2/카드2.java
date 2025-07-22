815713712487d766ed10559ab4aeda2172036fc8
yunjichoi <cyjcode99@gmail.com>
2025-07-22 10:58:11 +0900

    [Silver IV] Title: 카드2, Time: 152 ms, Memory: 28868 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,12 +1,18 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int i = 1;
-        while(i < N) i <<= 1;
-        System.out.println((N << 1) - i);
-        sc.close();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Deque<Integer> deque = new ArrayDeque<>();
+        for(int i = 1; i <= N; i++) {
+            deque.addLast(i);
+        }
+        while(deque.size() > 1) {
+            deque.pollFirst();
+            deque.addLast(deque.pollFirst());
+        }
+        System.out.println(deque.getFirst());
     }
 }
