5acd3d70591dfab06d0737df4cf5eff3523bdd7b
yunjichoi <cyjcode99@gmail.com>
2025-07-24 09:14:09 +0900

    [Silver IV] Title: 스택, Time: 172 ms, Memory: 20908 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,76 +1,26 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static int[] stack;
-	public static int size = 0;
- 	public static void main(String[] args) {
- 
-		Scanner sc = new Scanner(System.in);
-		StringBuilder sb = new StringBuilder();
-		
-		int n = sc.nextInt();
- 
-		stack = new int[n];
-		
-		for(int i = 0; i < n; i++) {
-			String s = sc.next();
-			switch (s) {
-			case "push":
-				push(sc.nextInt());
-				break;
-			case "pop":
-				sb.append(pop()).append('\n');
-				break;
-			case "size":
-				sb.append(size()).append('\n');
-				break;
-			case "empty":
-				sb.append(empty()).append('\n');
-				break;
-			case "top":
-				sb.append(top()).append('\n');
-				break;
-			}
-		}
-		System.out.println(sb);
-	}
- 
-	public static void push(int item) {
-		stack[size] = item;
-		size++;
-	}
-    
-	public static int pop() {
-		if(size == 0) {
-			return -1;
-		}
-		else {
-			int res = stack[size - 1];
-			stack[size - 1] = 0;
-			size--;
-			return res;
-		}
-	}
-    
-	public static int size() {
-		return size;
-	}
-    
-	public static int empty() {
-		if(size == 0) {
-			return 1;
-		}
-		else {
-			return 0;
-		}
-	}
-    
-	public static int top() {
-		if(size == 0) {
-			return -1;
-		}
-		else {
-			return stack[size - 1];
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0; i < N; i++) {
+            String S = br.readLine();
+            if(S.equals("top")) {
+                sb.append(stack.isEmpty() ? -1 : stack.peek()).append('\n');
+            } else if(S.equals("size")) {
+                sb.append(stack.size()).append("\n");
+            } else if(S.equals("empty")) {
+                sb.append(stack.isEmpty() ? 1 : 0).append("\n");
+            } else if(S.equals("pop")) {
+                sb.append(stack.isEmpty() ? -1 : stack.pop()).append("\n");
+            } else {
+                stack.push(Integer.parseInt(S.split(" ")[1]));
+            }
+        }
+        System.out.println(sb.toString().trim());
+    }
 }
