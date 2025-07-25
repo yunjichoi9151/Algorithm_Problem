d8680d5314e1af9f4022236c8fe2d77763d39a80
yunjichoi <cyjcode99@gmail.com>
2025-07-25 10:16:39 +0900

    [Silver III] Title: 프린터 큐, Time: 124 ms, Memory: 14804 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.java
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -1,37 +1,39 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        for(int tc = 1; tc <= t; tc++) {
-            int n = sc.nextInt();
-            int m = sc.nextInt();
-            int cnt = 0;
-            Queue<int[]> queue = new LinkedList<>();
-            for(int i = 0; i < n; i++) {
-                queue.add(new int[]{i, sc.nextInt()});
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
+        for(int tc = 0; tc < T; tc++) {
+            st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine());
+            Queue<int[]> que = new LinkedList<>();
+            PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+            for(int i = 0; i < N; i++) {
+                int num = Integer.parseInt(st.nextToken());
+                que.add(new int[]{i, num});
+                pq.add(num);
             }
-            while(!queue.isEmpty()) {
-                int[] poll = queue.poll();
-                boolean check = true;
-                for(int[] tmp : queue) {
-                    if(tmp[1] > poll[1]) {
-                        check = false;
-                    }
-                }
-                if(check) {
+            int cnt = 0;
+            while(!que.isEmpty()) {
+                int[] now = que.poll();
+                if(now[1] == pq.peek()) {
+                    pq.poll();
                     cnt++;
-                    if(poll[0] == m) {
+                    if(now[0] == M) {
+                        sb.append(cnt).append("\n");
                         break;
                     }
                 } else {
-                    queue.add(poll);
+                    que.add(now);
                 }
             }
-            System.out.println(cnt);
         }
+        System.out.println(sb.toString().trim());
     }
 }
