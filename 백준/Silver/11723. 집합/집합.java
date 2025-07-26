5df18a50b46190022366fc4ea571b86450d3d9e8
yunjichoi <cyjcode99@gmail.com>
2025-07-26 18:52:57 +0900

    [Silver V] Title: 집합, Time: 1028 ms, Memory: 312868 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -5,27 +5,27 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
         int M = Integer.parseInt(br.readLine());
-        boolean[] arr = new boolean[21];
-        for(int i = 0; i < M; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+        boolean[] check = new boolean[21];
+        Arrays.fill(check, false);
+        for(int tc = 0; tc < M; tc++) {
+            st = new StringTokenizer(br.readLine());
             String S = st.nextToken();
             if(S.equals("all")) {
-                for(int n = 1; n <= 20; n++) {
-                    arr[n] = true;
-                }
+                Arrays.fill(check, true);
             } else if(S.equals("empty")) {
-                arr = new boolean[21];
+                Arrays.fill(check, false);
             } else {
                 int N = Integer.parseInt(st.nextToken());
-                if(S.equals("add")) {
-                    if(!arr[N]) arr[N] = true;
+                if(S.equals("check")) {
+                    sb.append(check[N] ? 1 : 0).append("\n");
+                } else if(S.equals("add")) {
+                    check[N] = true;;
                 } else if(S.equals("remove")) {
-                    arr[N] = false;
-                } else if(S.equals("check")) {
-                    sb.append((arr[N] ? 1 : 0) + "\n");
+                    check[N] = false;
                 } else if(S.equals("toggle")) {
-                    arr[N] = !arr[N];
+                    check[N] = !check[N];
                 }
             }
         }
