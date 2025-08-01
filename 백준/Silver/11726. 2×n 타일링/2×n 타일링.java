8d816637882f6896acfcd92fb0c4517b97437f58
yunjichoi <cyjcode99@gmail.com>
2025-08-01 17:35:45 +0900

    [Silver III] Title: 2×n 타일링, Time: 108 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,16 +1,19 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] dp = new int[n + 2];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        if(N == 1) {
+            System.out.println(1);
+            return;
+        }
+        int[] dp = new int[N + 1];
         dp[1] = 1;
         dp[2] = 2;
-        for (int i = 3; i <= n; i++) {
+        for(int i = 3; i <= N; i++) {
             dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
         }
-        System.out.println(dp[n] % 10007);
-        sc.close();
+        System.out.println(dp[N]);
     }
 }
--- 백준/Silver/11726. 2×n 타일링/README.md
+++ 백준/Silver/11726. 2×n 타일링/README.md
@@ -4,11 +4,15 @@
 
 ### 성능 요약
 
-메모리: 17780 KB, 시간: 212 ms
+메모리: 14348 KB, 시간: 108 ms
 
 ### 분류
 
-다이나믹 프로그래밍(dp)
+다이나믹 프로그래밍
+
+### 제출 일자
+
+2025년 8월 1일 17:34:02
 
 ### 문제 설명
 
@@ -16,7 +20,7 @@
 
 <p>아래 그림은 2×5 크기의 직사각형을 채운 한 가지 방법의 예이다.</p>
 
-<p style="text-align: center;"><img alt="" src="" style="height:50px; width:125px"></p>
+<p style="text-align: center;"><img alt="" src="https://onlinejudgeimages.s3-ap-northeast-1.amazonaws.com/problem/11726/1.png" style="height:50px; width:125px"></p>
 
 ### 입력 
 
