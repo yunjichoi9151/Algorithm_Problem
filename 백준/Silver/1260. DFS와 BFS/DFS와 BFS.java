10eb5adf013538a603ef2e02261bf5a1494ddaed
yunjichoi <cyjcode99@gmail.com>
2025-08-04 13:08:32 +0900

    [Silver II] Title: DFS와 BFS, Time: 200 ms, Memory: 19860 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,57 +1,58 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int N, M, V;
-    static int[][] arr;
+    static int N;
     static boolean[] visited;
+    static TreeSet<Integer>[] arr;
+    static StringBuilder sb;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
         N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        V = Integer.parseInt(st.nextToken());
-        arr = new int[N + 1][N + 1];
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        arr = new TreeSet[N + 1];
+        visited = new boolean[N + 1];
+        for(int i = 1; i <= N; i++) {
+            arr[i] = new TreeSet<>();
+        }
         for(int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int n1 = Integer.parseInt(st.nextToken());
-            int n2 = Integer.parseInt(st.nextToken());
-            arr[n1][n2] = 1;
-            arr[n2][n1] = 1;
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            arr[a].add(b);
+            arr[b].add(a);
         }
+        // DFS
+        DFS(K);
+        System.out.println(sb.toString().trim());
+        // BFS
+        sb = new StringBuilder();
         visited = new boolean[N + 1];
-        dfs(V);
-        System.out.println();
-        visited = new boolean[N + 1];
-        bfs(V);
-    }
-    private static void dfs(int v) {
-        visited[v] = true;
-        System.out.print(v + " ");
-        if(v == N + 1) return;
-        for(int i = 1; i <= N; i++) {
-            if(arr[v][i] == 1 && visited[i] == false) {
-                dfs(i);
+        Queue<Integer> que = new LinkedList<>();
+        que.add(K);
+        visited[K] = true;
+        while(!que.isEmpty()) {
+            int x = que.poll();
+            sb.append(x).append(" ");
+            for(int num : arr[x]) {
+                if(!visited[num]) {
+                    que.add(num);
+                    visited[num] = true;
+                }
             }
         }
+        System.out.println(sb.toString().trim());
     }
-    private static void bfs(int v) {
-        Queue<Integer> que = new LinkedList<Integer>();
-        que.offer(v);
-        visited[v] = true;
-        System.out.print(v + " ");
-        while(!que.isEmpty()) {
-            int num = que.poll();
-            for(int i = 1; i <= N; i++) {
-                if(arr[num][i] == 1 && visited[i] == false) {
-                    visited[i] = true;
-                    System.out.print(i + " ");
-                    que.offer(i);
-                }
+
+    static void DFS(int node) {
+        visited[node] = true;
+        sb.append(node).append(" ");
+        for(int x : arr[node]) {
+            if(!visited[x]) {
+                DFS(x);
             }
         }
     }
