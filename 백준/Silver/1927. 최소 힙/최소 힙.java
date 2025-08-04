087c1351db103c982aba7bf682f03039d9d43cbb
yunjichoi <cyjcode99@gmail.com>
2025-08-04 17:15:17 +0900

    [Silver II] Title: 최소 힙, Time: 260 ms, Memory: 26904 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> que = new PriorityQueue<>();
+        for(int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if(num == 0) {
+                sb.append(que.isEmpty() ? 0 : que.poll()).append("\n");
+            } else {
+                que.add(num);
+            }
+        }
+        System.out.println(sb.toString().trim());
+    }
+}
