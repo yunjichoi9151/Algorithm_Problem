c581b16039d56f7540b3e6d6b90573658ae9f32a
yunjichoi <cyjcode99@gmail.com>
2025-08-12 13:14:48 +0900

    [Silver I] Title: 단지번호붙이기, Time: 112 ms, Memory: 14400 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -2,51 +2,50 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N;
-    static int[][] map;
-    static boolean[][] visited;
-    static ArrayList<Integer> ans;
     static class Node {
-        int x;
-        int y;
+        int x, y;
         public Node(int x, int y) {
             this.x = x;
             this.y = y;
         }
     }
+    static int N;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         N = Integer.parseInt(br.readLine());
         map = new int[N][N];
         visited = new boolean[N][N];
-        ans = new ArrayList<Integer>();
+        ArrayList<Integer> list = new ArrayList<>();
         for(int i = 0; i < N; i++) {
-            String s = br.readLine();
+            String S = br.readLine();
             for(int j = 0; j < N; j++) {
-                map[i][j] = s.charAt(j) - '0';
+                map[i][j] = Integer.parseInt(S.charAt(j) + "");
             }
         }
         for(int i = 0; i < N; i++) {
             for(int j = 0; j < N; j++) {
                 if(map[i][j] == 1 && !visited[i][j]) {
-                    ans.add(BFS(i, j));
+                    list.add(BFS(i, j));
                 }
             }
         }
-        Collections.sort(ans);
-        System.out.println(ans.size());
-        for(int n : ans) {
-            System.out.println(n);
+        Collections.sort(list);
+        for(int i = 0; i < list.size(); i++) {
+            sb.append(list.get(i)).append("\n");
         }
+        System.out.println(list.size());
+        System.out.println(sb.toString().trim());
     }
-
     static int BFS(int x, int y) {
-        Queue<Node> que = new LinkedList<Node>();
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
+        Queue<Node> que = new LinkedList<>();
         que.add(new Node(x, y));
         visited[x][y] = true;
-        int num = 1;
+        int cnt = 1;
         while(!que.isEmpty()) {
             Node node = que.poll();
             for(int i = 0; i < 4; i++) {
@@ -55,9 +54,9 @@ public class Main {
                 if(nx < 0 || ny < 0 || nx >= N || ny >= N || visited[nx][ny] || map[nx][ny] == 0) continue;
                 que.add(new Node(nx, ny));
                 visited[nx][ny] = true;
-                num++;
+                cnt++;
             }
         }
-        return num;
+        return cnt;
     }
 }
