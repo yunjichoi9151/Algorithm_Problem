c1bf01b2912d6b9b7ddc2975988a2ebe2c613d5f
yunjichoi <cyjcode99@gmail.com>
2025-08-12 11:55:16 +0900

    [Silver I] Title: 숨바꼭질, Time: 148 ms, Memory: 23536 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -2,46 +2,27 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static class Node {
-        int x, cnt;
-        public Node(int x, int cnt) {
-            this.x = x;
-            this.cnt = cnt;
-        }
-    }
-    static boolean[] check;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        Queue<Node> que = new LinkedList<>();
-        check = new boolean[100001];
-        que.add(new Node(N, 0));
-        check[N] = true;
+        int[] time = new int[100001];
+        boolean[] visited = new boolean[100001];
+        Queue<Integer> que = new LinkedList<>();
+        que.add(N);
+        visited[N] = true;
         while(!que.isEmpty()) {
-            Node node = que.poll();
-            if(node.x == K) {
-                System.out.println(node.cnt);
-                return;
-            }
-            if(check(node.x - 1)) {
-                que.add(new Node(node.x - 1, node.cnt + 1));
-                check[node.x - 1] = true;
-            }
-            if(check(node.x + 1)) {
-                que.add(new Node(node.x + 1, node.cnt + 1));
-                check[node.x + 1] = true;
-            }
-            if(check(node.x * 2)) {
-                que.add(new Node(node.x * 2, node.cnt + 1));
-                check[node.x * 2] = true;
+            int now = que.poll();
+            if(now == K) break;
+            for(int next : new int[]{now - 1, now + 1, now * 2}) {
+                if(next >= 0 && next <= 100000 && !visited[next]) {
+                    que.add(next);
+                    visited[next] = true;
+                    time[next] = time[now] + 1;
+                }
             }
         }
-    }
-
-    static boolean check(int n) {
-        if(n >= 0 && n <= 100000 && !check[n]) return true;
-        else return false;
+        System.out.println(time[K]);
     }
 }
