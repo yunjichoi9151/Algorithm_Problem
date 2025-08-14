3fe80acda6ceb687297114b25520f6adda30e93f
yunjichoi <cyjcode99@gmail.com>
2025-08-14 10:49:14 +0900

    [Gold V] Title: 회의실 배정, Time: 480 ms, Memory: 44388 KB -BaekjoonHub

---
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr, (o1, o2) -> {
+            if(o1[1] == o2[1]) {
+                return o1[0] - o2[0];
+            } else {
+                return o1[1] - o2[1];
+            }
+        });
+        int end = 0;
+        int cnt = 0;
+        for(int i = 0; i < N; i++) {
+            if(arr[i][0] >= end) {
+                end = arr[i][1];
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
