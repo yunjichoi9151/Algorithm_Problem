c66c83de17979b4a5ffa19ab5e6d4d4549583968
yunjichoi <cyjcode99@gmail.com>
2025-08-14 13:53:04 +0900

    [Gold V] Title: 적록색약, Time: 140 ms, Memory: 17352 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -2,68 +2,62 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N;
-    static char[][] map;
-    static boolean[][] visited;
     static class Node {
-        int x;
-        int y;
+        int x, y;
         public Node(int x, int y) {
             this.x = x;
             this.y = y;
         }
     }
-
+    static int N;
+    static char[][] arr;
+    static boolean[][][] visited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         N = Integer.parseInt(br.readLine());
-        map = new char[N][N];
+        arr = new char[N][N];
         for(int i = 0; i < N; i++) {
-            String s = br.readLine();
+            String S = br.readLine();
             for(int j = 0; j < N; j++) {
-                map[i][j] = s.charAt(j);
+                arr[i][j] = S.charAt(j);
             }
         }
-        visited = new boolean[N][N];
-        sb.append(cal(false) + " ");
-        visited = new boolean[N][N];
-        sb.append(cal(true));
-        System.out.println(sb.toString());
+        int[] cnt = new int[2];
+        visited = new boolean[N][N][2];
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                if(!visited[i][j][0]) {
+                    BFS(i, j, true);
+                    cnt[0]++;
+                }
+                if(!visited[i][j][1]) {
+                    BFS(i, j, false);
+                    cnt[1]++;
+                }
+            }
+        }
+        System.out.println(cnt[0] + " " + cnt[1]);
     }
 
-    static void BFS(int x, int y, boolean isTrue) {
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-        Queue<Node> que = new LinkedList<Node>();
+    static void BFS(int x, int y, boolean isNormal) {
+        Queue<Node> que = new LinkedList<>();
         que.add(new Node(x, y));
-        visited[x][y] = true;
-        char c = map[x][y];
+        visited[x][y][isNormal ? 0 : 1] = true;
         while(!que.isEmpty()) {
             Node node = que.poll();
             for(int i = 0; i < 4; i++) {
                 int nx = node.x + dx[i];
                 int ny = node.y + dy[i];
-                if(nx < 0 || ny < 0 || nx >= N || ny >= N || visited[nx][ny]) continue;
-                if((isTrue && (c == 'B' && map[nx][ny] == 'B' || c != 'B' && map[nx][ny] != 'B'))
-                    || (!isTrue && map[nx][ny] == c)) {
-                        que.add(new Node(nx, ny));
-                        visited[nx][ny] = true;
-                    }
-            }
-        }
-    }
-
-    static int cal(boolean isTrue) {
-        int cnt = 0;
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < N; j++) {
-                if(!visited[i][j]) {
-                    BFS(i, j, isTrue);
-                    cnt++;
+                if(nx < 0 || ny < 0 || nx >= N || ny >= N || visited[nx][ny][isNormal ? 0 : 1]) continue;
+                if((arr[node.x][node.y] == arr[nx][ny])
+                || (!isNormal && ((arr[node.x][node.y] == 'G' && arr[nx][ny] == 'R')
+                || (arr[node.x][node.y] == 'R' && arr[nx][ny] == 'G')))) {
+                    que.add(new Node(nx, ny));
+                    visited[nx][ny][isNormal ? 0 : 1] = true;
                 }
             }
         }
-        return cnt;
     }
 }
