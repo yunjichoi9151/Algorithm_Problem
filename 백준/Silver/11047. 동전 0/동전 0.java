bdd954b3dfe5a2b119829e0726550afa3685ac7c
yunjichoi <cyjcode99@gmail.com>
2025-08-20 17:37:20 +0900

    [Silver IV] Title: 동전 0, Time: 100 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -8,14 +8,15 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
         int[] arr = new int[N];
-        int cnt = 0;
-        for(int i = N - 1; i >= 0; i--) {
+        int ans = 0;
+        for(int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
-        for(int i = 0; i < N; i++) {
-            cnt += K / arr[i];
-            K = K % arr[i];
+        for(int i = N - 1; i >= 0; i--) {
+            ans += K / arr[i];
+            K %= arr[i];
+            if(K == 0) break;
         }
-        System.out.println(cnt);
+        System.out.println(ans);
     }
 }
