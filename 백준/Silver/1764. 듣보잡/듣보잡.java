bae7192ef7529ca75afd35f7afc2e9796879f5f4
yunjichoi <cyjcode99@gmail.com>
2025-08-20 17:23:48 +0900

    [Silver IV] Title: 듣보잡, Time: 232 ms, Memory: 26324 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -9,22 +9,20 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         HashSet<String> set = new HashSet<>();
-        ArrayList<String> list = new ArrayList<>();
-        int cnt = 0;
+        ArrayList<String> ans = new ArrayList<>();
         for(int i = 0; i < N; i++) {
             set.add(br.readLine());
         }
         for(int i = 0; i < M; i++) {
             String S = br.readLine();
             if(set.contains(S)) {
-                cnt++;
-                list.add(S);
+                ans.add(S);
             }
         }
-        Collections.sort(list);
-        System.out.println(cnt);
-        for(int i = 0; i < list.size(); i++) {
-            sb.append(list.get(i)).append("\n");
+        Collections.sort(ans);
+        sb.append(ans.size()).append("\n");
+        for(int i = 0; i < ans.size(); i++) {
+            sb.append(ans.get(i)).append("\n");
         }
         System.out.println(sb.toString().trim());
     }
