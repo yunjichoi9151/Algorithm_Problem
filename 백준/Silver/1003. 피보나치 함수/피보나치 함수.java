a2bc0361435c9dd1fb439e8fb21e2822e9dde96f
yunjichoi <cyjcode99@gmail.com>
2025-08-21 11:53:13 +0900

    [Silver III] Title: 피보나치 함수, Time: 100 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -6,15 +6,18 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
         int[][] fib = new int[41][2];
-        fib[0] = new int[]{1, 0};
-        fib[1] = new int[]{0, 1};
-        for(int i = 2; i < 41; i++) {
-            fib[i] = new int[]{fib[i - 1][0] + fib[i - 2][0], fib[i - 1][1] + fib[i - 2][1]};
+        fib[0][0] = 1;
+        fib[0][1] = 0;
+        fib[1][0] = 0;
+        fib[1][1] = 1;
+        for(int i = 2; i <= 40; i++) {
+            fib[i][0] = fib[i - 1][0] + fib[i - 2][0];
+            fib[i][1] = fib[i - 1][1] + fib[i - 2][1];
         }
-        for(int tc = 0; tc < T; tc++) {
+        for(int i = 0; i < T; i++) {
             int n = Integer.parseInt(br.readLine());
             sb.append(fib[n][0]).append(" ").append(fib[n][1]).append("\n");
         }
-        System.out.println(sb.toString().trim());
+        System.out.print(sb);
     }
 }
