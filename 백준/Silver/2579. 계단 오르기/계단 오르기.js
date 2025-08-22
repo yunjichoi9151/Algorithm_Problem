2a01f2ef1871f4a03ec4116de9d6b4e4b51ad664
yunjichoi <cyjcode99@gmail.com>
2025-08-22 13:49:44 +0900

    [Silver III] Title: 계단 오르기, Time: 92 ms, Memory: 9364 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.js
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.js
@@ -1,19 +1,13 @@
-const [N, ...input] = require("fs")
-  .readFileSync("/dev/stdin")
-  .toString()
-  .split("\n")
-  .map((line) => Number(line.trim()));
-if (N === 1) {
-  console.log(input[0]);
+const [N, ...score] = require('fs').readFileSync('/dev/stdin').toString().split('\n').map(line => Number(line.trim()));
+if(N === 1) {
+  console.log(score[0]);
   return;
 }
 const dp = new Array(N + 1).fill(0);
-dp[1] = input[0];
-dp[2] = input[0] + input[1];
-for (let i = 3; i <= N; i++) {
-  dp[i] = Math.max(
-    dp[i - 2] + input[i - 1],
-    dp[i - 3] + input[i - 2] + input[i - 1]
-  );
+dp[1] = score[0];
+dp[2] = score[0] + score[1];
+for(let i = 3; i <= N; i++) {
+  dp[i] = Math.max(dp[i - 2], dp[i - 3] + score[i - 2]) + score[i - 1];
+
 }
 console.log(dp[N]);
