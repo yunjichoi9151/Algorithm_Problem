5b1b2f72897109d20e47ea8bb3588c9a06975e69
yunjichoi <cyjcode99@gmail.com>
2025-08-26 11:06:11 +0900

    [Silver III] Title: 바이러스, Time: 92 ms, Memory: 9440 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.js
+++ 백준/Silver/2606. 바이러스/바이러스.js
@@ -1,27 +1,42 @@
-let [N, M, ...input] = require("fs")
-  .readFileSync("/dev/stdin")
-  .toString()
-  .split("\n")
-  .map((line) => line.trim());
-N = Number(N);
-M = Number(M);
-input = input.map((line) => line.split(" ").map(Number));
-const arr = Array.from({ length: N + 1 }, () => []);
-for (let i = 0; i < M; i++) {
-  const [a, b] = input[i];
-  arr[a].push(b);
-  arr[b].push(a);
+class Queue {
+  constructor() {
+    this.arr = [];
+    this.head = 0;
+  }
+
+  add(num) {
+    this.arr.push(num);
+  }
+
+  poll() {
+    return this.arr[this.head++];
+  }
+
+  isEmpty() {
+    return this.head === this.arr.length;
+  }
 }
+
+let [N, T, ...input] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(line => line.trim());
+N = Number(N);
+T = Number(T);
+input = input.map(line => line.split(' ').map(Number));
+arr = Array.from({length : N + 1}, () => []);
+for(let i = 0; i < T; i++) {
+  arr[input[i][0]].push(input[i][1]);
+  arr[input[i][1]].push(input[i][0]);
+}
+const que = new Queue();
 const visited = new Array(N + 1).fill(false);
-const queue = [1];
+que.add(1);
 visited[1] = true;
 let cnt = 0;
-while (queue.length > 0) {
-  const tmp = queue.shift();
-  for (const num of arr[tmp]) {
-    if (!visited[num]) {
-      queue.push(num);
-      visited[num] = true;
+while(!que.isEmpty()) {
+  const num = que.poll();
+  for(let tmp of arr[num]) {
+    if(!visited[tmp]) {
+      que.add(tmp);
+      visited[tmp] = true;
       cnt++;
     }
   }
