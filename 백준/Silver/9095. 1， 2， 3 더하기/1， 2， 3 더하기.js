93479e409ece112b38273b310b07f8b9275b67af
yunjichoi <cyjcode99@gmail.com>
2025-08-26 11:39:07 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 88 ms, Memory: 9320 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
@@ -1,17 +1,15 @@
-const [T, ...input] = require("fs")
-  .readFileSync("/dev/stdin")
-  .toString()
-  .split("\n")
-  .map((line) => Number(line.trim()));
-const dp = new Array(11);
+const [T, ...input] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(line => Number(line.trim()));
 const ans = [];
-dp[1] = 1;
-dp[2] = 2;
-dp[3] = 4;
-for (let i = 4; i <= 10; i++) {
-  dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+for(let tc = 0; tc < T; tc++) {
+  let N = input[tc];
+  if(N <= 2) {
+    ans.push(N);
+    continue;
+  }
+  const dp = [0, 1, 2, 4];
+  for(let i = 4; i <= N; i++) {
+    dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
+  }
+  ans.push(dp[N]);
 }
-for (let i = 0; i < T; i++) {
-  ans.push(dp[input[i]]);
-}
-console.log(ans.join("\n"));
+console.log(ans.join('\n'));
