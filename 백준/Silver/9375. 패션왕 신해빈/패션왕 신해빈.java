15c367c46e41b9ed12bdbb78098ad92fffe08ca7
yunjichoi <cyjcode99@gmail.com>
2025-08-26 11:50:33 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 100 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -5,19 +5,22 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
         int T = Integer.parseInt(br.readLine());
         for(int tc = 0; tc < T; tc++) {
             int N = Integer.parseInt(br.readLine());
             HashMap<String, Integer> map = new HashMap<>();
             for(int i = 0; i < N; i++) {
-                String[] tmp = br.readLine().split(" ");
-                map.put(tmp[1], map.getOrDefault(tmp[1], 0) + 1);
+                st = new StringTokenizer(br.readLine());
+                String name = st.nextToken();
+                String type = st.nextToken();
+                map.put(type, map.getOrDefault(type, 0) + 1);
             }
             int cnt = 1;
             for(String S : map.keySet()) {
                 cnt *= (map.get(S) + 1);
             }
-            sb.append(cnt - 1).append("\n");
+            sb.append(cnt - 1).append('\n');
         }
         System.out.println(sb.toString().trim());
     }
