c1603448d2a71120064ee9f7811915b0d6292e49
yunjichoi <cyjcode99@gmail.com>
2025-08-28 10:06:10 +0900

    [Silver III] Title: Four Squares, Time: 204 ms, Memory: 11156 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.js
+++ 백준/Silver/17626. Four Squares/Four Squares.js
@@ -1,15 +1,18 @@
-const N = Number(require("fs").readFileSync("/dev/stdin").toString().trim());
-if (N === 1) {
-  console.log(1);
+const N = Number(require('fs').readFileSync('/dev/stdin').toString().trim());
+if(N <= 3) {
+  console.log(N);
   return;
 }
-const dp = new Array(N + 1);
-dp[0] = 0;
-dp[1] = 1;
-for (let i = 2; i <= N; i++) {
-  dp[i] = i;
-  for (let j = 1; j * j <= i; j++) {
-    dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+const dp = [0, 1, 2, 3];
+for(let i = 4; i <= N; i++) {
+  let num = i;
+  for(let j = 1; j * j <= i; j++) {
+    if(j * j == i) {
+      num = 1;
+      break;
+    }
+    num = Math.min(num, dp[i - j * j] + 1);
   }
+  dp.push(num);
 }
 console.log(dp[N]);
