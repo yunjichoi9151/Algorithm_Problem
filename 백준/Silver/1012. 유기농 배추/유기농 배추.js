1ab945e0b3eb3d80062ca025033b859f9ee2b04a
yunjichoi <cyjcode99@gmail.com>
2025-08-28 11:52:10 +0900

    [Silver II] Title: 유기농 배추, Time: 192 ms, Memory: 16020 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.js
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.js
@@ -1,63 +1,57 @@
 class Queue {
   constructor() {
     this.arr = [];
+    this.head = 0;
   }
 
-  add(a, b) {
-    this.arr.push({ x: a, y: b });
+  add(num) {
+    this.arr.push(num);
   }
 
   poll() {
-    return this.arr.shift();
+    return this.arr[this.head++];
   }
 
   isEmpty() {
-    return this.arr.length === 0;
+    return this.head === this.arr.length;
   }
 }
 
-const input = require("fs")
-  .readFileSync("/dev/stdin")
-  .toString()
-  .split("\n")
-  .map((line) => line.trim());
-const T = input.splice(0, 1);
+let [T, ...input] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(line => line.trim());
+T = Number(T);
+let idx = 0;
 const ans = [];
 const dx = [-1, 1, 0, 0];
 const dy = [0, 0, -1, 1];
-for (let tc = 0; tc < T; tc++) {
-  const [M, N, K] = input.splice(0, 1)[0].split(" ").map(Number);
-  const arr = input.splice(0, K);
-  const map = new Array(N).fill(0).map(() => new Array(M).fill(false));
-  const visited = new Array(N).fill(0).map(() => new Array(M).fill(false));
-  const que = new Queue();
-  let cnt = 0;
-  for (let i = 0; i < K; i++) {
-    const [x, y] = arr[i].split(" ").map(Number);
+for(let tc = 0; tc < T; tc++) {
+  const [M, N, K] = input[idx++].split(' ').map(Number);
+  const map = Array.from({length : N}, () => new Array(M).fill(false));
+  const visited = Array.from({length : N}, () => new Array(M).fill(false));
+  for(let i = 1; i <= K; i++) {
+    const [x, y] = input[idx++].split(' ').map(Number);
     map[y][x] = true;
   }
-  for (let i = 0; i < N; i++) {
-    for (let j = 0; j < M; j++) {
-      if (map[i][j] && !visited[i][j]) {
-        que.add(i, j);
+  let cnt = 0;
+  for(let i = 0; i < N; i++) {
+    for(let j = 0; j < M; j++) {
+      if(map[i][j] && !visited[i][j]) {
+        const que = new Queue();
+        que.add({x : i, y : j});
         visited[i][j] = true;
-        cnt++;
-        while (!que.isEmpty()) {
-          const node = que.poll();
-          for (let k = 0; k < 4; k++) {
-            const nx = node.x + dx[k];
-            const ny = node.y + dy[k];
-            if (nx < 0 || ny < 0 || nx >= N || ny >= M || visited[nx][ny])
-              continue;
-            if (map[nx][ny]) {
-              que.add(nx, ny);
-              visited[nx][ny] = true;
-            }
+        while(!que.isEmpty()) {
+          const tmp = que.poll();
+          for(let k = 0; k < 4; k++) {
+            const nx = tmp.x + dx[k];
+            const ny = tmp.y + dy[k];
+            if(nx < 0 || ny < 0 || nx >= N || ny >= M || visited[nx][ny] || !map[nx][ny]) continue;
+            que.add({x : nx, y : ny});
+            visited[nx][ny] = true;
           }
         }
+        cnt++;
       }
     }
   }
   ans.push(cnt);
 }
-console.log(ans.join("\n"));
+console.log(ans.join('\n'));
