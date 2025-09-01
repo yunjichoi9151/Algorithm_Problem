43ecbe8305ce85e4f3b535f2ffd30c12426d965a
yunjichoi <cyjcode99@gmail.com>
2025-09-01 10:30:57 +0900

    [Silver II] Title: 랜선 자르기, Time: 156 ms, Memory: 17304 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -7,27 +7,22 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int K = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(st.nextToken());
-        long[] arr = new long[K];
         long left = 1;
         long right = 0;
+        int[] arr = new int[K];
         for(int i = 0; i < K; i++) {
             arr[i] = Integer.parseInt(br.readLine());
-            right = Math.max(arr[i], right);
+            if(arr[i] > right) right = arr[i];
         }
-        long result = 0;
         while(left <= right) {
             long mid = (left + right) / 2;
             long cnt = 0;
             for(int i = 0; i < K; i++) {
                 cnt += arr[i] / mid;
             }
-            if(cnt >= N) {
-                result = mid;
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
+            if(cnt >= N) left = mid + 1;
+            else right = mid - 1;
         }
-        System.out.println(result);
+        System.out.println(right);
     }
 }
