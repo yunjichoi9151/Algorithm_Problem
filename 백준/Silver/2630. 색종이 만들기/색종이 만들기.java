45f1b674969411583a93f7337b7060999cd0df5f
yunjichoi <cyjcode99@gmail.com>
2025-09-02 13:09:11 +0900

    [Silver II] Title: 색종이 만들기, Time: 136 ms, Memory: 15648 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -2,52 +2,46 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    class Node {
-        int x, y;
-        public Node(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
+    static int[][] map;
+    static int white, blue;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int N = Integer.parseInt(br.readLine());
-        int[][] map = new int[N][N];
+        map = new int[N][N];
         for(int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j < N; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        int divide = N;
-        int[] cnt = new int[2];
-        while(divide > 0) {
-            for(int startX = 0; startX < N; startX += divide) {
-                for(int startY = 0; startY < N; startY += divide) {
-                    boolean isTrue = true;
-                    outerLoop:
-                    for(int i = startX; i < startX + divide; i++) {
-                        for(int j = startY; j < startY + divide; j++) {
-                            if(map[i][j] != map[startX][startY] || map[i][j] == -1) {
-                                isTrue = false;
-                                break outerLoop;
-                            }
-                        }
-                    }
-                    if(isTrue) {
-                        cnt[map[startX][startY]]++;
-                        for(int i = startX; i < startX + divide; i++) {
-                            for(int j = startY; j < startY + divide; j++) {
-                                map[i][j] = -1;
-                            }
-                        }
-                    }
+        solve(0, 0, N);
+        System.out.println(white);
+        System.out.println(blue);
+    }
+
+    static void solve(int x, int y, int size) {
+        if(check(x, y, size)) {
+            if(map[x][y] == 0) white++;
+            else blue++;
+            return;
+        }
+        int half = size / 2;
+        solve(x, y, half);
+        solve(x, y + half, half);
+        solve(x + half, y, half);
+        solve(x + half, y + half, half);
+    }
+
+    static boolean check(int x, int y, int size) {
+        int color = map[x][y];
+        for(int i = x; i < x + size; i++) {
+            for(int j = y; j < y + size; j++) {
+                if(map[i][j] != color) {
+                    return false;
                 }
             }
-            divide /= 2;
         }
-        System.out.println(cnt[0]);
-        System.out.println(cnt[1]);
+        return true;
     }
 }
