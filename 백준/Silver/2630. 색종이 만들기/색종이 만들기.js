b831a5ee15ca92f4673c2dc8bea6511c3e886694
yunjichoi <cyjcode99@gmail.com>
2025-09-02 14:03:16 +0900

    [Silver II] Title: 색종이 만들기, Time: 152 ms, Memory: 10504 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.js
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.js
@@ -1,30 +1,29 @@
-let [N, ...input] = require('fs').readFileSync('/dev/stdin').toString().split('\n').map(line => line.trim());
+let [N, ...map] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(line => line.trim());
 N = Number(N);
-input = input.map(line => line.split(' ').map(Number));
-const ans = [0, 0];
-for(let divide = N; divide > 0; divide = Math.floor(divide / 2)) {
-  for(let startX = 0; startX < N; startX += divide) {
-    for(let startY = 0; startY < N; startY += divide) {
-      if(input[startX][startY] === -1) continue;
-      let isTrue = true;
-      outerLoop:
-      for(let i = startX; i < startX + divide; i++) {
-        for(let j = startY; j < startY + divide; j++) {
-          if(input[startX][startY] != input[i][j]) {
-            isTrue = false;
-            break outerLoop;
-          }
-        }
-      }
-      if(isTrue) {
-        ans[input[startX][startY]]++;
-        for(let i = startX; i < startX + divide; i++) {
-          for(let j = startY; j < startY + divide; j++) {
-            input[i][j] = -1;
-          }
-        }
-      }
+let ans = [0, 0];
+map = map.map(line => line.split(' ').map(Number));
+solve(0, 0, N);
+console.log(ans.join('\n'));
+
+function solve(x, y, size) {
+  if(check(x, y, size)) {
+    if(map[x][y] === 0) ans[0]++;
+    else ans[1]++;
+    return;
+  }
+  const half = size / 2;
+  solve(x, y, half);
+  solve(x, y + half, half);
+  solve(x + half, y, half);
+  solve(x + half, y + half, half);
+}
+
+function check(x, y, size) {
+  const type = map[x][y];
+  for(let i = x; i < x + size; i++) {
+    for(let j = y; j < y + size; j++) {
+      if(map[i][j] !== type) return false;
     }
   }
+  return true;
 }
-console.log(ans.join('\n'));
