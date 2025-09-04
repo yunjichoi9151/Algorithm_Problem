aaba06b7ab1722326474dfc9766c8e537cbd1763
yunjichoi <cyjcode99@gmail.com>
2025-09-04 11:08:10 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 568 ms, Memory: 141948 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -2,41 +2,41 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static ArrayList<Integer>[] list;
-    static boolean[] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        list = new ArrayList[N + 1];
-        visited = new boolean[N + 1];
-        int ans = 0;
+        ArrayList<Integer>[] arr = new ArrayList[N + 1];
+        boolean[] visited = new boolean[N + 1];
         for(int i = 1; i <= N; i++) {
-            list[i] = new ArrayList<Integer>();
+            arr[i] = new ArrayList<Integer>();
         }
         for(int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
-            list[u].add(v);
-            list[v].add(u);
+            arr[u].add(v);
+            arr[v].add(u);
         }
-        for(int i = 1; i <= N; i++) {
-            if(!visited[i]) {
-                DFS(i);
-                ans++;
+        Queue<Integer> que = new LinkedList<>();
+        int cnt = 0;
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i]) {
+                que.add(i);
+                visited[i] = true;
+                cnt++;
+                while (!que.isEmpty()) {
+                    int node = que.poll();
+                    for (int num : arr[node]) {
+                        if (!visited[num]) {
+                            visited[num] = true;
+                            que.add(num);
+                        }
+                    }
+                }
             }
         }
-        System.out.println(ans);
-    }
-
-    static void DFS(int now) {
-        visited[now] = true;
-        for(int i : list[now]) {
-            if(!visited[i]) {
-                DFS(i);
-            }
-        }
+        System.out.println(cnt);
     }
 }
