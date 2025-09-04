57911ec3883f87c09e2d7632e9f80c1a84cbec21
yunjichoi <cyjcode99@gmail.com>
2025-09-04 11:14:45 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 904 ms, Memory: 136548 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.js
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.js
@@ -1,28 +1,45 @@
-const [[N, M], ...input] = require("fs")
-  .readFileSync("/dev/stdin")
-  .toString()
-  .split("\n")
-  .map((line) => line.trim().split(" ").map(Number));
-const list = Array.from({ length: N + 1 }, () => []);
-const visited = new Array(N + 1).fill(false);
-let ans = 0;
-for (let i = 0; i < M; i++) {
-  list[input[i][0]].push(input[i][1]);
-  list[input[i][1]].push(input[i][0]);
-}
-for (let i = 1; i <= N; i++) {
-  if (!visited[i]) {
-    DFS(i);
-    ans++;
+class Queue {
+  constructor() {
+    this.arr = [];
+    this.head = 0;
+  }
+
+  add(num) {
+    this.arr.push(num);
+  }
+
+  poll() {
+    return this.arr[this.head++];
+  }
+
+  isEmpty() {
+    return this.head === this.arr.length;
   }
 }
-console.log(ans);
 
-function DFS(num) {
-  visited[num] = true;
-  for (let i of list[num]) {
-    if (!visited[i]) {
-      DFS(i);
+const [[N, M], ...input] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(line => line.trim().split(' ').map(Number));
+const arr = Array.from({length : N + 1}, () => []);
+for(let i = 0; i < M; i++) {
+  arr[input[i][0]].push(input[i][1]);
+  arr[input[i][1]].push(input[i][0]);
+}
+const que = new Queue();
+const visited = new Array(N + 1).fill(false);
+let cnt = 0;
+for(let i = 1; i <= N; i++) {
+  if(!visited[i]) {
+    que.add(i);
+    visited[i] = true;
+    cnt++;
+    while(!que.isEmpty()) {
+      const node = que.poll();
+      for(let num of arr[node]) {
+        if(!visited[num]) {
+          visited[num] = true;
+          que.add(num);
+        }
+      }
     }
   }
 }
+console.log(cnt);
