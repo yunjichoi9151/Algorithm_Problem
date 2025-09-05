92498aa15632384dfd3231b7a823f6344670dae5
yunjichoi <cyjcode99@gmail.com>
2025-09-05 15:45:20 +0900

    [Silver II] Title: 과일 탕후루, Time: 388 ms, Memory: 36256 KB -BaekjoonHub

--- 백준/Silver/30804. 과일 탕후루/과일 탕후루.java
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.java
@@ -6,23 +6,22 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
+        HashMap<Integer, Integer> map = new HashMap<>();
         int[] arr = new int[N];
         for(int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        int max = 0;
-        int left = 0, right = 0;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        while (right < N) {
+        int left = 0, ans = 0;
+        for(int right = 0; right < N; right++) {
             map.put(arr[right], map.getOrDefault(arr[right], 0) + 1);
-            while (map.size() > 2) {
-                map.put(arr[left], map.get(arr[left]) - 1);
-                if (map.get(arr[left]) == 0) map.remove(arr[left]);
-                left++;
+            while(map.size() > 2) {
+                int tmp = arr[left++];
+                int tmp_cnt = map.get(tmp) - 1;
+                if(tmp_cnt == 0) map.remove(tmp);
+                else map.put(tmp, tmp_cnt);
             }
-            max = Math.max(max, right - left + 1);
-            right++;
+            ans = Math.max(ans, right - left + 1);
         }
-        System.out.println(max);
+        System.out.println(ans);
     }
 }
