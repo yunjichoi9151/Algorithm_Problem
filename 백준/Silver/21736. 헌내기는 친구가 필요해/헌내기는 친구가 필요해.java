e6412d0a44e673f63e6555ade0fafea3f2728bc7
yunjichoi <cyjcode99@gmail.com>
2025-09-05 12:48:50 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 296 ms, Memory: 37408 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
@@ -15,35 +15,31 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         char[][] map = new char[N][M];
+        int[] dx = {-1, 0, 0, 1};
+        int[] dy = {0, 1, -1, 0};
+        Queue<Node> que = new LinkedList<>();
         boolean[][] visited = new boolean[N][M];
-        Node now = new Node(0, 0);
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
+        int ans = 0;
         for(int i = 0; i < N; i++) {
-            String S = br.readLine();
+            map[i] = br.readLine().toCharArray();
             for(int j = 0; j < M; j++) {
-                map[i][j] = S.charAt(j);
                 if(map[i][j] == 'I') {
-                    now.x = i;
-                    now.y = j;
+                    que.add(new Node(i, j));
+                    visited[i][j] = true;
                 }
             }
         }
-        Queue<Node> que = new LinkedList<>();
-        que.add(now);
-        visited[now.x][now.y] = true;
-        int cnt = 0;
         while(!que.isEmpty()) {
             Node node = que.poll();
             for(int i = 0; i < 4; i++) {
                 int nx = node.x + dx[i];
                 int ny = node.y + dy[i];
                 if(nx < 0 || ny < 0 || nx >= N || ny >= M || visited[nx][ny] || map[nx][ny] == 'X') continue;
-                if(map[nx][ny] == 'P') cnt++;
-                visited[nx][ny] = true;
                 que.add(new Node(nx, ny));
+                visited[nx][ny] = true;
+                if(map[nx][ny] == 'P') ans++;
             }
         }
-        System.out.println(cnt == 0 ? "TT" : cnt);
+        System.out.println(ans == 0 ? "TT" : ans);
     }
 }
