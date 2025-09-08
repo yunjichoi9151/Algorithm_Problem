2a6ed816c5ac7d2a4927eefaaff30580ce37ae02
yunjichoi <cyjcode99@gmail.com>
2025-09-08 11:16:50 +0900

    [Silver I] Title: 숨바꼭질, Time: 132 ms, Memory: 18840 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -2,27 +2,40 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static class Node {
+        int x, time;
+        public Node(int x, int time) {
+            this.x = x;
+            this.time = time;
+        }
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        int[] time = new int[100001];
+        Queue<Node> que = new LinkedList<>();
         boolean[] visited = new boolean[100001];
-        Queue<Integer> que = new LinkedList<>();
-        que.add(N);
+        que.add(new Node(N, 0));
         visited[N] = true;
         while(!que.isEmpty()) {
-            int now = que.poll();
-            if(now == K) break;
-            for(int next : new int[]{now - 1, now + 1, now * 2}) {
-                if(next >= 0 && next <= 100000 && !visited[next]) {
-                    que.add(next);
-                    visited[next] = true;
-                    time[next] = time[now] + 1;
-                }
+            Node node = que.poll();
+            if(node.x == K) {
+                System.out.println(node.time);
+                return;
+            }
+            if(node.x * 2 <= 100000 && !visited[node.x * 2]) {
+                que.add(new Node(node.x * 2, node.time + 1));
+                visited[node.x * 2] = true;
+            } 
+            if(node.x - 1 >= 0 && !visited[node.x - 1]) {
+                que.add(new Node(node.x - 1, node.time + 1));
+                visited[node.x - 1] = true;
+            } 
+            if(node.x + 1 <= 100000 && !visited[node.x + 1]) {
+                que.add(new Node(node.x + 1, node.time + 1));
+                visited[node.x + 1] = true;
             }
         }
-        System.out.println(time[K]);
     }
 }
