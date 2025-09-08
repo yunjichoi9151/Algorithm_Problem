084a909210430440a6732a26a7ec534983fdf577
yunjichoi <cyjcode99@gmail.com>
2025-09-08 11:50:28 +0900

    [Silver I] Title: 단지번호붙이기, Time: 112 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -10,51 +10,49 @@ public class Main {
         }
     }
     static int N;
-    static int[][] map;
+    static char[][] map;
     static boolean[][] visited;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         N = Integer.parseInt(br.readLine());
-        map = new int[N][N];
+        map = new char[N][N];
         visited = new boolean[N][N];
         ArrayList<Integer> list = new ArrayList<>();
         for(int i = 0; i < N; i++) {
-            String S = br.readLine();
-            for(int j = 0; j < N; j++) {
-                map[i][j] = Integer.parseInt(S.charAt(j) + "");
-            }
+            map[i] = br.readLine().toCharArray();
         }
         for(int i = 0; i < N; i++) {
             for(int j = 0; j < N; j++) {
-                if(map[i][j] == 1 && !visited[i][j]) {
+                if(map[i][j] == '1' && !visited[i][j]) {
                     list.add(BFS(i, j));
                 }
             }
         }
+        sb.append(list.size()).append("\n");
         Collections.sort(list);
-        for(int i = 0; i < list.size(); i++) {
-            sb.append(list.get(i)).append("\n");
+        for(int num : list) {
+            sb.append(num).append("\n");
         }
-        System.out.println(list.size());
-        System.out.println(sb.toString().trim());
+        System.out.print(sb);
     }
+
     static int BFS(int x, int y) {
         Queue<Node> que = new LinkedList<>();
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+        int cnt = 0;
         que.add(new Node(x, y));
         visited[x][y] = true;
-        int cnt = 1;
         while(!que.isEmpty()) {
             Node node = que.poll();
+            cnt++;
             for(int i = 0; i < 4; i++) {
                 int nx = node.x + dx[i];
                 int ny = node.y + dy[i];
-                if(nx < 0 || ny < 0 || nx >= N || ny >= N || visited[nx][ny] || map[nx][ny] == 0) continue;
+                if(nx < 0 || ny < 0 || nx >= N || ny >= N || visited[nx][ny] || map[nx][ny] == '0') continue;
                 que.add(new Node(nx, ny));
                 visited[nx][ny] = true;
-                cnt++;
             }
         }
         return cnt;
