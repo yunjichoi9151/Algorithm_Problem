b38a738e5bafbceafb36b0719dcd235501c16dd5
yunjichoi <cyjcode99@gmail.com>
2025-09-08 11:25:35 +0900

    [Silver I] Title: 숨바꼭질, Time: 192 ms, Memory: 20488 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
@@ -1,35 +1,43 @@
 class Queue {
   constructor() {
     this.arr = [];
+    this.head = 0;
   }
 
-  add(n) {
-    this.arr.push(n);
+  add(num) {
+    this.arr.push(num);
   }
 
   poll() {
-    return this.arr.shift();
+    return this.arr[this.head++];
   }
 
   isEmpty() {
-    return this.arr.length === 0;
+    return this.arr.length === this.head;
   }
 }
 
 const [N, K] = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ').map(Number);
-const time = new Array(100001).fill(0);
 const que = new Queue();
 const visited = new Array(100001).fill(false);
-que.add(N);
+que.add({x : N, time : 0});
 visited[N] = true;
 while(!que.isEmpty()) {
-  const now = que.poll();
-  for(let i of [now - 1, now + 1, now * 2]) {
-    if(i >= 0 && i <= 100000 && !visited[i]) {
-      visited[i] = true;
-      que.add(i);
-      time[i] = time[now] + 1;
-    }
+  const node = que.poll();
+  if(node.x === K) {
+    console.log(node.time);
+    return;
+  }
+  if(node.x * 2 <= 100000 && !visited[node.x * 2]) {
+    que.add({x : node.x * 2, time : node.time + 1});
+    visited[node.x * 2] = true;
+  }
+  if(node.x - 1 >= 0 && !visited[node.x - 1]) {
+    que.add({x : node.x - 1, time : node.time + 1});
+    visited[node.x - 1] = true;
+  }
+  if(node.x + 1 <= 100000 && !visited[node.x + 1]) {
+    que.add({x : node.x + 1, time : node.time + 1});
+    visited[node.x + 1] = true;
   }
 }
-console.log(time[K]);
