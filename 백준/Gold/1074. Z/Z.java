720914ffccbff562f6fb6aeca852f3f1241210b7
yunjichoi <cyjcode99@gmail.com>
2025-09-10 15:26:56 +0900

    [Gold V] Title: Z, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.java
+++ 백준/Gold/1074. Z/Z.java
@@ -2,21 +2,20 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] dx = {0, 0, 1, 1};
-    static int[] dy = {0, 1, 0, 1};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int r = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
-        int half = (int)Math.pow(2, N) / 2;
         int ans = 0;
-        while(half > 0) {
-            ans += ((r < half ? 0 : 2) + (c < half ? 0 : 1)) * (half * half);
-            if(r >= half) r -= half;
-            if(c >= half) c -= half;
-            half /= 2;
+        while (N > 0) {
+            int half = 1 << (N - 1);
+            int quad = (r >= half ? 2 : 0) + (c >= half ? 1 : 0);
+            ans += 1L * quad * half * half;
+            if (r >= half) r -= half;
+            if (c >= half) c -= half;
+            N--;
         }
         System.out.println(ans);
     }
