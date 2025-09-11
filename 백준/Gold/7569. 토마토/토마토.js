db05b805f7c5a6e8e9d0b73db60be0c45aa1c8bd
yunjichoi <cyjcode99@gmail.com>
2025-09-11 10:45:11 +0900

    [Gold V] Title: 토마토, Time: 620 ms, Memory: 129484 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.js
+++ 백준/Gold/7569. 토마토/토마토.js
@@ -13,49 +13,45 @@ class Queue {
   }
 
   isEmpty() {
-    return this.head >= this.arr.length;
+    return this.arr.length === this.head;
   }
 }
-
-const [[M, N, H], ...input] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(line => line.trim().split(' ').map(Number));
+const [[M, N, H], ...arr] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(line => line.trim().split(' ').map(Number));
+let need = 0;
 const map = [];
-for(let i = 0; i < H * N; i += N) {
-  map.push(input.slice(i, i + N));
+for(let h = 0; h < H; h++) {
+  map.push(arr.slice(h * N, (h + 1) * N));
 }
-let sum = M * N * H;
-let tomato = 0;
 const que = new Queue();
-const dx = [0, 0, 0, 0, -1, 1];
-const dy = [0, 0, -1, 1, 0, 0];
-const dz = [-1, 1, 0, 0, 0, 0];
-for(let z = 0; z < H; z++) {
-  for(let y = 0; y < N; y++) {
-    for(let x = 0; x < M; x++) {
-      if(map[z][y][x] === 1) {
-        que.add({x : x, y : y, z : z, cnt : 0});
-        tomato++;
-      } else if(map[z][y][x] === -1) {
-        sum--;
-      }
+const dh = [-1, 1, 0, 0, 0, 0];
+const dx = [0, 0, -1, 1, 0, 0];
+const dy = [0, 0, 0, 0, -1, 1];
+for(let h = 0; h < H; h++) {
+  for(let i = 0; i < N; i++) {
+    for(let j = 0; j < M; j++) {
+      if(map[h][i][j] === 0) need++;
+      else if(map[h][i][j] === 1) que.add({h : h, x : i, y : j, cnt : 0});
     }
   }
 }
-if(sum === tomato) {
-  console.log(0)
+if(need === 0) {
+  console.log(0);
   return;
 }
-let day;
 while(!que.isEmpty()) {
   const node = que.poll();
-  day = node.cnt;
   for(let i = 0; i < 6; i++) {
+    const nh = node.h + dh[i];
     const nx = node.x + dx[i];
     const ny = node.y + dy[i];
-    const nz = node.z + dz[i];
-    if(nx < 0 || ny < 0 || nz < 0 || nx >= M || ny >= N || nz >= H || map[nz][ny][nx] === 1 || map[nz][ny][nx] === -1) continue;
-    que.add({x : nx, y : ny, z : nz, cnt : node.cnt + 1});
-    map[nz][ny][nx] = 1;
-    tomato++;
+    if(nh < 0 || nx < 0 || ny < 0 || nh >= H || nx >= N || ny >= M || map[nh][nx][ny] != 0) continue;
+    que.add({h : nh, x : nx, y : ny, cnt : node.cnt + 1});
+    map[nh][nx][ny] = 1;
+    need--;
+    if(need === 0) {
+      console.log(node.cnt + 1);
+      return;
+    }
   }
 }
-console.log(sum === tomato ? day : -1);
+console.log(-1);
