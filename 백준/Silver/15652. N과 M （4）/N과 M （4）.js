9b53c49858149357cc17264925a2465bdb7d6934
yunjichoi <cyjcode99@gmail.com>
2025-09-16 13:34:27 +0900

    [Silver III] Title: N과 M (4), Time: 152 ms, Memory: 13124 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.js
@@ -0,0 +1,15 @@
+const [N, M] = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ').map(Number);
+const arr = new Array(M);
+const ans = [];
+DFS(0, 1);
+console.log(ans.map(line => line.join(' ')).join('\n'));
+function DFS(depth, start) {
+  if(depth === M) {
+    ans.push([...arr]);
+    return;
+  }
+  for(let i = start; i <= N; i++) {
+    arr[depth] = i;
+    DFS(depth + 1, i);
+  }
+}
