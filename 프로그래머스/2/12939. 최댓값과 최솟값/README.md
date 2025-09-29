6e04afe1a37697dcc60ac5982ff896b53043b9db
yunjichoi <cyjcode99@gmail.com>
2025-09-29 11:50:45 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 10.79 ms, Memory: 89.8 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/README.md
+++ 프로그래머스/2/12939. 최댓값과 최솟값/README.md
@@ -1,10 +1,10 @@
 # [level 2] 최댓값과 최솟값 - 12939 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12939?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12939?language=java) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.08 ms
+메모리: 89.8 MB, 시간: 10.79 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 25일 15:57:48
+2025년 09월 29일 11:50:43
 
 ### 문제 설명
 
--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -3,10 +3,10 @@ class Solution {
         String[] arr = s.split(" ");
         int max = Integer.MIN_VALUE;
         int min = Integer.MAX_VALUE;
-        for(String t : arr) {
-            int num = Integer.parseInt(t);
-            max = Math.max(num, max);
-            min = Math.min(num, min);
+        for(String tmp : arr) {
+            int num = Integer.parseInt(tmp);
+            if(max < num) max = num;
+            if(min > num) min = num;
         }
         return min + " " + max;
     }
