dafc332121e47bdecd13aa00fc20a1a7cb4c6ea3
yunjichoi <cyjcode99@gmail.com>
2025-10-02 11:01:25 +0900

    [Silver III] Title: N과 M (8), Time: 156 ms, Memory: 20032 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static int[] arr;
+    static ArrayList<Integer> list = new ArrayList<>();
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        DFS(0, 0);
+        System.out.print(sb);
+    }
+    
+    static void DFS(int start, int depth) {
+        if(depth == M) {
+            for(int i = 0; i < M; i++) {
+                sb.append(list.get(i));
+                if(i != M - 1) sb.append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for(int i = start; i < N; i++) {
+            list.add(arr[i]);
+            DFS(i, depth + 1);
+            list.remove(list.size() - 1);
+        }
+    }
+}
