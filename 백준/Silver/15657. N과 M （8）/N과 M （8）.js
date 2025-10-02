21ef9f7c4ca79851d0967b958eddef44b437a944
yunjichoi <cyjcode99@gmail.com>
2025-10-02 11:10:35 +0900

    [Silver III] Title: N과 M (8), Time: 160 ms, Memory: 13932 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.js
@@ -0,0 +1,17 @@
+const [[N, M], input] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(line => line.trim().split(' ').map(Number));
+const arr = input.sort((a, b) => a - b);
+const list = [];
+const ans = [];
+DFS(0, 0);
+console.log(ans.map(line => line.join(' ')).join('\n'));
+function DFS(start, depth) {
+  if(depth === M) {
+    ans.push([...list]);
+    return;
+  }
+  for(let i = start; i < N; i++) {
+    list.push(arr[i]);
+    DFS(i, depth + 1);
+    list.pop();
+  }
+}
