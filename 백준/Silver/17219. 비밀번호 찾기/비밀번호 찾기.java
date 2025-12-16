925a73cc8e93dbc0f5b56ff617d15024a1e1070b
yunjichoi <cyjcode99@gmail.com>
2025-12-16 09:15:32 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 528 ms, Memory: 58064 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -8,7 +8,7 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        HashMap<String, String> map = new HashMap<>();
+        Map<String, String> map = new HashMap<>();
         for(int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             map.put(st.nextToken(), st.nextToken());
@@ -16,6 +16,6 @@ public class Main {
         for(int i = 0; i < M; i++) {
             sb.append(map.get(br.readLine())).append("\n");
         }
-        System.out.println(sb.toString().trim());
+        System.out.print(sb);
     }
 }
